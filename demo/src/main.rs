7a4197a06f420a5b9bfbf6a8f49dad479c47ab62
Cliff L. Biffle <cliff@oxide.computer>
2020-04-21 14:02:56 -0700

    demo: ping-pong between two tasks.

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -16,7 +16,7 @@ use kern::app::{App, RegionDesc, TaskDesc, RegionAttributes, TaskFlags};
 #[repr(C)]
 struct Descriptors {
     app: App,
-    task: TaskDesc,
+    task: [TaskDesc; 2],
     region: RegionDesc,
 }
 
@@ -28,16 +28,25 @@ fn main() -> ! {
         app: App {
             magic: kern::app::CURRENT_APP_MAGIC,
             region_count: 1,
-            task_count: 1,
+            task_count: 2,
             zeroed_expansion_space: [0; 20],
         },
-        task: TaskDesc {
-            entry_point: spin as usize as u32,
-            flags: TaskFlags::START_AT_BOOT,
-            initial_stack: 0x10010000,
-            priority: kern::app::Priority(0),
-            regions: [0, 0, 0, 0, 0, 0, 0, 0],
-        },
+        task: [
+            TaskDesc {
+                entry_point: sender as usize as u32,
+                flags: TaskFlags::START_AT_BOOT,
+                initial_stack: 0x10010000,
+                priority: kern::app::Priority(1),
+                regions: [0, 0, 0, 0, 0, 0, 0, 0],
+            },
+            TaskDesc {
+                entry_point: rxer as usize as u32,
+                flags: TaskFlags::START_AT_BOOT,
+                initial_stack: 0x10008000,
+                priority: kern::app::Priority(0),
+                regions: [0, 0, 0, 0, 0, 0, 0, 0],
+            },
+        ],
         region: RegionDesc {
             base: 0,
             size: !0,
@@ -55,16 +64,74 @@ fn main() -> ! {
     }
 }
 
-fn spin() -> ! {
+/// Loops sending an empty message.
+fn sender() -> ! {
     loop {
         unsafe {
-        asm! {
-            "svc #0"
-            :
-            : "{r11}"(0)
-            :
-            : "volatile"
-        }
+            asm! {
+                "svc #0"
+                :
+                : "{r4}"(1 << 16),
+                  "{r5}"(0),
+                  "{r6}"(0),
+                  "{r7}"(0),
+                  "{r8}"(0),
+                  "{r9}"(0),
+                  "{r10}"(0),
+                  "{r11}"(0)
+                :
+                : "volatile"
+            }
         }
     }
 }
+
+/// Loops receiving and responding to messages.
+fn rxer() -> ! {
+    let mut rx_buf = [0u8; 0];
+    loop {
+        // Receive message
+        let mut sender: u32;
+        #[allow(unused_variables)]
+        let mut operation: u32;
+        #[allow(unused_variables)]
+        let mut message_len: usize;
+        #[allow(unused_variables)]
+        let mut response_capacity: usize;
+        #[allow(unused_variables)]
+        let mut lease_count: usize;
+
+        #[allow(unused_assignments)]
+        unsafe {
+            asm! {
+                "svc #0"
+                : "={r4}"(sender),
+                  "={r5}"(operation),
+                  "={r6}"(message_len),
+                  "={r7}"(response_capacity),
+                  "={r8}"(lease_count)
+                : "{r4}"(rx_buf.as_mut_ptr())
+                  "{r5}"(rx_buf.len())
+                  "{r11}"(1)
+                :
+                : "volatile"
+            }
+        }
+        // Unblock sender
+        let response_code: u32 = 0;
+        unsafe {
+            asm! {
+                "svc #0"
+                :
+                : "{r4}"(sender)
+                  "{r5}"(response_code)
+                  "{r6}"(rx_buf.as_mut_ptr())
+                  "{r7}"(rx_buf.len())
+                  "{r11}"(2)
+                :
+                : "volatile"
+            }
+        }
+    }
+}
+
