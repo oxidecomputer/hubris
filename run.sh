0f982dceaba49d9d86c70c561c8cb8e81ff6bed9
Cliff L. Biffle <cliff@oxide.computer>
2020-04-29 08:56:21 -0700

    run.sh: fix objcopy versions that don't detect srec

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 mkdir -p target/packager
 cargo run --bin packager -- "$@" demo/app.toml target/packager
-arm-none-eabi-objcopy -O elf32-littlearm target/packager/combined.{srec,elf}
+arm-none-eabi-objcopy -Isrec -O elf32-littlearm target/packager/combined.{srec,elf}
 arm-none-eabi-gdb -q \
   -x target/packager/script.gdb \
   -x openocd.gdb \
