4eceaa91db3acdef4c5122f99925d83081b598bf
Cliff L. Biffle <cliff@oxide.computer>
2020-04-29 16:06:01 -0700

    Spam a UART from the demo.
    As of this commit, after a successful load, you should see text from
    USART2 at 115200 bps on PA2/3.
    
    The UART driver in this commit suuuucks because we don't have interrupts
    yet: it's transmit-only and blocks the entire system between characters.

---
+++ drv/stm32f4-usart/src/main.rs
@@ -0,0 +1,146 @@
+//! A driver for the STM32F4 U(S)ART.
+//!
+//! # IPC protocol
+//!
+//! ## `write` (1)
+//!
+//! Sends the contents of lease #0. Returns when completed.
+
+#![no_std]
+#![no_main]
+
+// you can put a breakpoint on `rust_begin_unwind` to catch panics
+extern crate panic_halt;
+
+use stm32f4::stm32f407 as device;
+use zerocopy::AsBytes;
+use userlib::*;
+
+#[cfg(not(feature = "standalone"))]
+const RCC: Task = Task::rcc_driver;
+
+// For standalone mode -- this won't work, but then, neither will a task without
+// a kernel.
+#[cfg(feature = "standalone")]
+const RCC: Task = SELF;
+
+const OP_WRITE: u32 = 1;
+
+#[repr(u32)]
+enum ResponseCode {
+    Success = 0,
+    BadOp = 1,
+    BadArg = 2,
+}
+
+#[export_name = "main"]
+fn main() -> ! {
+    // Turn the actual peripheral on so that we can interact with it.
+    turn_on_usart();
+
+    // From thin air, pluck a pointer to the USART register block.
+    //
+    // Safety: this is needlessly unsafe in the API. The USART is essentially a
+    // static, and we access it through a & reference so aliasing is not a
+    // concern. Were it literally a static, we could just reference it.
+    let usart = unsafe { &*device::USART2::ptr() };
+
+    // The UART has clock and is out of reset, but isn't actually on until we:
+    usart.cr1.write(|w| w.ue().enabled());
+    // Work out our baud rate divisor.
+    const CLOCK_HZ: u32 = 16_000_000;
+    const BAUDRATE: u32 = 115_200;
+    const CYCLES_PER_BIT: u32 = (CLOCK_HZ + (BAUDRATE / 2)) / BAUDRATE;
+    usart.brr.write(|w| w.div_mantissa().bits((CYCLES_PER_BIT >> 4) as u16)
+        .div_fraction().bits(CYCLES_PER_BIT as u8 & 0xF));
+
+    // Enable the transmitter.
+    usart.cr1.modify(|_, w| w.te().enabled());
+
+    turn_on_gpioa();
+
+    // TODO: the fact that we interact with GPIOA directly here is an expedient
+    // hack, but control of the GPIOs should probably be centralized somewhere.
+    let gpioa = unsafe { &*device::GPIOA::ptr() };
+
+    // Mux the USART onto the output pins. We're using PA2/3, where USART2 is
+    // selected by Alternate Function 7.
+    gpioa.moder.modify(|_, w| {
+        w.moder2().alternate()
+            .moder3().alternate()
+    });
+    gpioa.afrl.modify(|_, w| {
+        w.afrl2().af7()
+            .afrl3().af7()
+    });
+
+    usart.dr.write(|w| w.dr().bits(b'!' as u16));
+
+    // Field messages.
+    let mask = 0;  // we don't use notifications.
+    'msgloop:
+    loop {
+        let msginfo = sys_recv(&mut [], mask);
+        match msginfo.operation {
+            OP_WRITE => {
+                if msginfo.lease_count != 1 {
+                    sys_reply(msginfo.sender, ResponseCode::BadArg as u32, &[]);
+                    continue;
+                }
+
+                let (rc, atts, len) = sys_borrow_info(msginfo.sender, 0);
+                if rc != 0 || atts & 1 == 0 {
+                    sys_reply(msginfo.sender, ResponseCode::BadArg as u32, &[]);
+                    continue;
+                }
+
+                for i in 0..len {
+                    let mut byte = 0u8;
+                    let (rc, len) = sys_borrow_read(msginfo.sender, 0, i, byte.as_bytes_mut());
+                    if rc != 0 || len != 1 {
+                        sys_reply(msginfo.sender, ResponseCode::BadArg as u32, &[]);
+                        continue 'msgloop;
+                    }
+
+                    while !usart.sr.read().txe().bit() {
+                        // Spin until the transmitter becomes available
+                    }
+
+                    // Stuff byte into transmitter.
+                    usart.dr.write(|w| w.dr().bits(u16::from(byte)));
+                }
+
+                sys_reply(msginfo.sender, ResponseCode::Success as u32, &[]);
+            }
+            _ => sys_reply(msginfo.sender, ResponseCode::BadOp as u32, &[]),
+        }
+    }
+}
+
+fn turn_on_usart() {
+    let rcc_driver = TaskId::for_index_and_gen(RCC as usize, 0);
+
+    const ENABLE_CLOCK: u16 = 1;
+    let pnum = 113; // see bits in APB1ENR
+    let (code, _) = userlib::sys_send(rcc_driver, ENABLE_CLOCK, pnum.as_bytes(), &mut [], &[]);
+    assert_eq!(code, 0);
+
+    const LEAVE_RESET: u16 = 4;
+    let (code, _) = userlib::sys_send(rcc_driver, LEAVE_RESET, pnum.as_bytes(), &mut [], &[]);
+    assert_eq!(code, 0);
+}
+
+fn turn_on_gpioa() {
+    let rcc_driver = TaskId::for_index_and_gen(RCC as usize, 0);
+
+    const ENABLE_CLOCK: u16 = 1;
+    let pnum = 0; // see bits in AHB1ENR
+    let (code, _) = userlib::sys_send(rcc_driver, ENABLE_CLOCK, pnum.as_bytes(), &mut [], &[]);
+    assert_eq!(code, 0);
+
+    const LEAVE_RESET: u16 = 4;
+    let (code, _) = userlib::sys_send(rcc_driver, LEAVE_RESET, pnum.as_bytes(), &mut [], &[]);
+    assert_eq!(code, 0);
+}
+
+
--- task-ping/src/main.rs
+++ task-ping/src/main.rs
@@ -13,12 +13,19 @@ const PEER: Task = SELF;
 #[cfg(not(feature = "standalone"))]
 const PEER: Task = Task::pong;
 
+#[cfg(feature = "standalone")]
+const UART: Task = SELF;
+
+#[cfg(not(feature = "standalone"))]
+const UART: Task = Task::usart_driver;
+
 #[export_name = "main"]
 fn main() -> ! {
     let peer = TaskId::for_index_and_gen(PEER as usize, 0);
     const PING_OP: u16 = 1;
     let mut response = [0; 16];
     loop {
+        uart_send(b"Ping!\r\n");
         // Signal that we're entering send:
         set_led();
 
@@ -37,4 +44,14 @@ fn set_led() {
         &*stm32f4::stm32f407::GPIOD::ptr()
     };
     gpiod.bsrr.write(|w| w.bs12().set_bit());
+}
+
+fn uart_send(text: &[u8]) {
+    let peer = TaskId::for_index_and_gen(UART as usize, 0);
+
+    const OP_WRITE: u16 = 1;
+    let (code, _) = sys_send(peer, OP_WRITE, &[], &mut [], &[
+        Lease::from(text),
+    ]);
+    assert_eq!(0, code);
 }
