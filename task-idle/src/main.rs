dc6df7f2d009f97f31986b05b7a45a63822ccc29
Cliff L. Biffle <cliff@oxide.computer>
2020-04-30 09:03:21 -0700

    Add idle task so we can sleep between IRQs.

---
+++ task-idle/src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+// you can put a breakpoint on `rust_begin_unwind` to catch panics
+extern crate panic_halt;
+
+// Make sure we actually link in userlib, despite not using any of it explicitly
+// - we need it for our _start routine.
+extern crate userlib;
+
+#[export_name = "main"]
+fn main() -> ! {
+    loop {
+        // Safety: asm in general is unsafe, but this instruction is fine.
+        unsafe {
+            // Wait For Interrupt to pause the processor until an ISR arrives,
+            // which could wake some higher-priority task.
+            asm!("wfi"::::"volatile");
+        }
+    }
+}
