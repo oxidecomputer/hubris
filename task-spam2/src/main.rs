5b303dd821895599d9d1cad6fc350bd6f698b673
Laura Abbott <laura@oxide.computer>
2020-06-09 15:20:26 -0400

    Add another spam task to test i2c
    This just reads the ID register on the DAC on the LPC55

---
+++ task-spam2/src/main.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+#![feature(llvm_asm)]
+
+// Make sure we actually link in userlib, despite not using any of it explicitly
+// - we need it for our _start routine.
+use userlib::*;
+use cortex_m_semihosting::hprintln;
+
+#[cfg(feature = "standalone")]
+const I2C: Task = SELF;
+
+#[cfg(not(feature = "standalone"))]
+const I2C: Task = Task::i2c_driver;
+
+#[export_name = "main"]
+fn main() -> ! {
+
+    let addr : &[u8]= &[0x0];
+    let i2c = TaskId::for_index_and_gen(I2C as usize, Generation::default());
+    hprintln!("Starting to spam!").ok();
+    loop {
+        let mut recv : [u8; 4] = [0; 4];
+        let a : &mut [u8] = &mut recv;
+        // This is address of the WM8904 on Flexcomm 4
+        // register 0 = id register that should always return 8904 on read
+        let (code, _) = sys_send(i2c, 1, &[0x1a], &mut [], &[Lease::from(addr)]);
+        if code != 0 {
+            hprintln!("Got error code{}", code).ok();
+        } else {
+            hprintln!("Success").ok();
+        }
+        let (code, _) = sys_send(i2c, 2, &[0x1a], &mut [], &[Lease::from(a)]);
+        if code != 0 {
+            hprintln!("Got error code{}", code).ok();
+        } else {
+            hprintln!("Got buffer {:x?}", recv).ok();
+        }
+    }
+}
+
