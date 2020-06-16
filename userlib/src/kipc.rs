c8878693e1db9a5bccd45f965f774aadd3c0bd6c
Cliff L. Biffle <cliff@oxide.computer>
2020-06-16 12:56:20 -0700

    userlib: cargo fmt

--- userlib/src/kipc.rs
+++ userlib/src/kipc.rs
@@ -8,7 +8,8 @@ pub fn read_task_status(task: usize) -> abi::TaskState {
     // Coerce `task` to a known size (Rust doesn't assume that usize == u32)
     let task = task as u32;
     let mut response = [0; core::mem::size_of::<abi::TaskState>()];
-    let (rc, len) = sys_send(TaskId::KERNEL, 1, task.as_bytes(), &mut response, &[]);
+    let (rc, len) =
+        sys_send(TaskId::KERNEL, 1, task.as_bytes(), &mut response, &[]);
     assert_eq!(rc, 0);
     ssmarshal::deserialize(&response[..len])
         .map_err(|_| ())
