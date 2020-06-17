e81e77c4eca293307a1cb3cb75b6811131b9f556
Cliff L. Biffle <cliff@oxide.computer>
2020-06-17 14:16:11 -0700

    Move away from llvm_asm to new asm.
    In my opinion, this makes the constraints a _lot_ easier to read, and
    gets us things like noreturn. (Plus, it moves us off a deprecated
    feature whose days are numbered.)
    
    asm has opinions about trying to use r11. These opinions are misguided:
    https://github.com/rust-lang/rust/issues/73450
    
    However, we respect them by manually moving values in and out of r11 in
    the asm sequences.

--- kern/src/lib.rs
+++ kern/src/lib.rs
@@ -23,7 +23,7 @@
 //!    most clever algorithms used in kernels wind up requiring `unsafe`.)
 
 #![cfg_attr(target_os = "none", no_std)]
-#![feature(llvm_asm)]
+#![feature(asm)]
 #![feature(naked_functions)]
 
 #[macro_use]
--- task-ping/src/main.rs
+++ task-ping/src/main.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(llvm_asm)]
 
 use userlib::*;
 
