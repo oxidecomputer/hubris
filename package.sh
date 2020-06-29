596ae5ec05fd5e8f9be5465812289477fad819c9
Steve Klabnik <steve@steveklabnik.com>
2020-06-29 11:22:42 -0500

    move objcopy into package task

--- package.sh
+++ package.sh
@@ -3,4 +3,3 @@
 set -euo pipefail
 
 cargo xtask packager demo/app.toml
-arm-none-eabi-objcopy -Isrec -O elf32-littlearm target/packager/combined.{srec,elf}
--- package_lpc55.sh
+++ package_lpc55.sh
@@ -3,4 +3,3 @@
 set -euo pipefail
 
 cargo xtask packager lpc55/app.toml
-arm-none-eabi-objcopy -Isrec -O elf32-littlearm target/packager/combined.{srec,elf}
