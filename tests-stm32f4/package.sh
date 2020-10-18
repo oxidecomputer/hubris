440b95eb8cc0e6434276d93a6ad4c782ede9aa67
Bryan Cantrill <bryan@oxide.computer>
2020-10-17 20:23:47 -0700

    test rework: get tests working again

--- tests-stm32f4/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-mkdir -p target/packager
-cargo run -p packager --bin packager -- "$@" app.toml target/packager
-arm-none-eabi-objcopy -Isrec -O elf32-littlearm target/packager/combined.{srec,elf}
