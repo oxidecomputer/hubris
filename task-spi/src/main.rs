fea351923409df19f76ccffc854e2351687b0341
Laura Abbott <laura@oxide.computer>
2021-02-22 11:46:41 -0500

    Extend task-spi for testing
    Use a different buffer for each of RoT and SP to see the data
    go back and forth

--- task-spi/src/main.rs
+++ task-spi/src/main.rs
@@ -17,13 +17,26 @@ fn main() -> ! {
     let spi = TaskId::for_index_and_gen(SPI as usize, Generation::default());
     hprintln!("Waiting to receive SPI data").ok();
     loop {
-        let mut buf: [u8; 4] = [0xDE, 0xAD, 0xBE, 0xEF];
+        let mut recv: [u8; 4] = [0; 4];
+        let b: &mut [u8] = &mut recv;
+
+        cfg_if::cfg_if! {
+            if #[cfg(target_board = "gemini-bu-rot-1")] {
+                let buf : [u8; 4] = [0xCA, 0xFE, 0xFE, 0xED];
+            } else {
+                let buf : [u8; 4] = [0xDE, 0xAD, 0xBE, 0xEF];
+            }
+        }
+
+        let op = 3;
+        let a: &[u8] = &buf;
+        hprintln!("Starting a new call...").ok();
         let (code, _) =
-            sys_send(spi, 2, &[], &mut [], &[Lease::from(&mut buf[..])]);
+            sys_send(spi, op, &[], &mut [], &[Lease::from(a), Lease::from(b)]);
         if code != 0 {
             hprintln!("Got error code {}", code).ok();
         } else {
-            hprintln!("Got buffer {:x?}", buf).ok();
+            hprintln!("Got buffer {:x?}", recv).ok();
         }
     }
 }
