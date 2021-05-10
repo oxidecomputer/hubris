c567fdf59feb90188702c51909f90b1ef5e5a210
Luqman Aden <me@luqman.ca>
2021-05-10 15:37:27 -0700

    Prep for stm32f4 crates to also support f3.

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -14,6 +14,7 @@ extern crate panic_semihosting; // requires a debugger
 
 // We have to do this if we don't otherwise use it to ensure its vector table
 // gets linked in.
+#[cfg(feature = "stm32f4")]
 extern crate stm32f4;
 
 use cortex_m_rt::entry;
