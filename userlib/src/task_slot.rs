737f9a9fd214dd7ac64e94681127ea6f8dcc7b3d
Rick Altherr <kc8apf@kc8apf.net>
2021-10-01 15:17:53 -0700

    userlib: introduce userlib::peer_task!()
    Often tasks need to interact with other tasks within the same firmware
    image.  Up to now, this has been accomplished by `cargo xtask dist`
    dynamically generating a Task enum based on the tasks defined in the
    specified app.toml.  Each Task enum variant provides the index of the task
    within the firmware image's task table.  Tasks import this enum
    from userlib, hardcode the variant for the other task they want to
    interact with, and then call userlib::get_task_id() to ask the kernel to
    provide the TaskId for the current generation of that task.
    
    Since this enum changes based on which app.toml is used, tasks are
    becoming littered with `if #[cfg(target_board = "gemini-bu-1")]`
    blocks that provide the appropraite Task enum variant for the app.toml
    being used. Any change to a task's name or a task being used by a new
    app.toml tends to require modifying every dependent task. Worse, generic
    tasks such as user-leds end up with unique code for nearly every
    app.toml.
    
    userlib::peer_task!() removes the need for the Task enum and hardcoded
    task names by creating named placeholders for peer tasks. These
    placeholders are then enumerated in a special linker section that,
    similar to debug info, is excluded from the actual firmware image. An
    external tool can use this linker section to find the PeerTask storage
    locations in the firmware image, determine which task the placeholder
    should reference, and inject that task's index once known.  Tasks
    wanting to interact with a peer task simply call PeerTask::get_task_id()
    regardless of which task index was injected.

---
+++ userlib/src/task_slot.rs
@@ -0,0 +1,93 @@
+use abi::{Generation, TaskId};
+
+/// Placeholder for post-compilation linking of tasks.
+///
+/// Most tasks will need to interact with other tasks by sending messages to
+/// their TaskIds. In many cases, the task where the required functionality is
+/// implemented will vary based on the application. Since the exact target task
+/// may not be known at task compile time but will be known later, task slots
+/// are used to create compile-time placeholders that are filled in with a
+/// task's identifying information by a post-compile process.  These
+/// placeholders can then be converted into TaskId at runtime.
+#[repr(C)]
+pub struct TaskSlot(u16);
+
+impl TaskSlot {
+    /// A TaskSlot that has not been resolved by a later processing step.
+    ///
+    /// Calling get_task_id() on an unbound TaskSlot will panic.
+    pub const UNBOUND: Self = Self(TaskId::UNBOUND.0);
+
+    pub fn get_task_id(&self) -> TaskId {
+        // In the expected use case of a static TaskSlot instance, such an
+        // instance is considered immutable by the compiler.  The compiler may
+        // choose to exploit that immutability to constant-fold the value of
+        // self.0 (as known at compile-time).  Since the intent of TaskSlot is
+        // to modify the value of self.0 after compilation, it does not meet the
+        // compiler's definition of immutable but the compiler doesn't know that
+        // without some help.  This is similar to interior mutability a la
+        // UnsafeCell but slightly different as a TaskSlot instance _is_
+        // immutable at runtime and there is only ever one reference to self.0.
+        // Instead, we need to force the compiler to load self.0 from RAM as it
+        // couldn't know what the value is at compile time.  This is effectively
+        // the same as a volatile read where the value in storage may be changed
+        // outside the compiler and runtime.
+        let task_index = unsafe { core::ptr::read_volatile(&self.0) };
+
+        if task_index == TaskId::UNBOUND.0 {
+            panic!("Attempted to get task id of unbound TaskSlot");
+        }
+
+        let prototype =
+            TaskId::for_index_and_gen(task_index.into(), Generation::default());
+        crate::sys_refresh_task_id(prototype)
+    }
+}
+
+/// Description of a task slot in .task_slot_table ELF section.
+///
+/// Most tasks will need to interact with other tasks by sending messages to
+/// their TaskIds. In many cases, the task where the required functionality is
+/// required will vary based on the application. Since the exact target task may
+/// not be known at task compile time but will be known later, task slots are
+/// used to create compile-time placeholders that are filled in with a task's
+/// identifying information by a post-compile process.  These placeholders can
+/// then be converted into TaskId at runtime.
+///
+/// Each task slot entry consist of an ASCII name and the address of the task
+/// slot's placeholder in the task's binary. While not part of the kernel/task
+/// ABI, these entries are part of the task's ABI that is used by the build
+/// system.
+#[repr(C)]
+#[repr(packed)]
+pub struct TaskSlotTableEntry<const N: usize> {
+    taskidx_address: *const u16,
+    slot_name_len: usize,
+    slot_name: [u8; N],
+}
+
+impl<const N: usize> TaskSlotTableEntry<N> {
+    pub const fn for_task_slot(
+        slot_name: &'static [u8; N],
+        task_slot: &'static TaskSlot,
+    ) -> Self {
+        Self {
+            taskidx_address: &task_slot.0,
+            slot_name_len: slot_name.len(),
+            slot_name: *slot_name,
+        }
+    }
+}
+
+// SAFETY
+//
+// Storing a pointer in a struct causes it to not implement Sync automatically.
+// In this case, PeerTaskTableEntry can only be constructed via for_peer_task()
+// which requires &'static arguments.  Thus, the stored pointer can only be to a
+// static PeerTask.  Further, for_peer_task() is only intended to be used by
+// peer_task!() which places the PeerTaskTableEntry in a .peer_task_table linker
+// section that is treated similar to debug information in that no virtual
+// addresses are allocated to the contents and the section is not loaded into
+// the process space.  As such, instances of PeerTaskTableEntry will never exist
+// at runtime.
+unsafe impl<const N: usize> Sync for TaskSlotTableEntry<N> {}
