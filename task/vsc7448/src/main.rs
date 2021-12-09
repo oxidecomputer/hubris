944647702852974630ac3576601f139312287a90
Matt Keeter <matt@oxide.computer>
2021-12-09 15:48:31 -0500

    Update to new task slots and remove outdated comments

--- task/vsc7448/src/main.rs
+++ task/vsc7448/src/main.rs
@@ -1,11 +1,7 @@
 #![no_std]
 #![no_main]
 
-// NOTE: you will probably want to remove this when you write your actual code;
-// we need to import userlib to get this to compile, but it throws a warning
-// because we're not actually using it yet!
 use ringbuf::*;
-#[allow(unused_imports)]
 use userlib::*;
 
 use drv_spi_api::{Spi, SpiDevice, SpiError};
