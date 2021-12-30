742d9badf23ec4da5b5cfd868abd732f92c8d5e2
Jonathan Perkin <jonathan@perkin.org.uk>
2021-12-30 18:36:09 +0000

    sys: Kernel support for Cortex-M0+ / ARMv6-M.
    There are a number of changes required to support the older ARMv6-M
    architecture:
    
     * Only a limited subset of Thumb-2 instructions are supported.  This is
       most noticeable in function prologue / epilogue as only low registers
       can be pushed or popped, resulting in some extra work to shuffle
       everything through r4-r7 in the correct order.
    
     * Significantly simpler fault exception handling, with all failures going
       through the single HardFault exception.  There is currently no support
       for fine-grained FaultType and all exceptions are for better or worse
       currently treated as FaultInfo::IllegalInstruction.
    
     * No hardware divide instructions mean we can't trap on divide by zero
       errors either.
    
     * Lack of atomic compare-and-swap support means we currently have simple
       non-atomic implementations of swap and fetch_{add,sub}, though it
       should be possible to do better.
    
    The fault exception handling is definitely the area that needs the most
    work, but its current state is enough for the system to function as
    expected all the way up to the ping task.
    
    (Maintainer note: simplified some of the original routines and
    instruction sequences to reduce size and duplication.)
    
    Co-authored-by: Cliff L. Biffle <cliff@oxide.computer>

--- sys/userlib/build.rs
+++ sys/userlib/build.rs
@@ -5,6 +5,8 @@
 use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    build_util::expose_m_profile();
+
     // Do an architecture check.
     if env::var("CARGO_CFG_TARGET_OS").unwrap() != "none" {
         eprintln!("***********************************************");
