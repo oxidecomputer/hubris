89347681b95fba5b2dab5d2981407b3a28103b1b
Jonathan Perkin <jonathan@perkin.org.uk>
2021-12-30 19:06:29 +0000

    task: Add support for Cortex-M0+ / ARMv6-M.
    Implement non-atomic atomic support to get things working, this should
    be revisited later, if at least to consolidate the copies of this code.
    
    ARMv6-M does not have hardware divide instructions, so task-ping is
    limited to just the nullread fault.

--- task/hiffy/src/main.rs
+++ task/hiffy/src/main.rs
@@ -19,6 +19,32 @@ use core::sync::atomic::{AtomicU32, Ordering};
 use hif::*;
 use userlib::*;
 
+#[cfg(armv6m)]
+trait AtomicU32Ext {
+    fn swap(&self, val: u32, order: Ordering) -> u32;
+    fn fetch_add(&self, val: u32, order: Ordering) -> u32;
+    fn fetch_sub(&self, val: u32, order: Ordering) -> u32;
+}
+
+#[cfg(armv6m)]
+impl AtomicU32Ext for AtomicU32 {
+    fn swap(&self, val: u32, order: Ordering) -> u32 {
+        let rv = self.load(order);
+        self.store(val, order);
+        rv
+    }
+    fn fetch_add(&self, val: u32, order: Ordering) -> u32 {
+        let rv = self.load(order);
+        self.store(rv + val, order);
+        rv
+    }
+    fn fetch_sub(&self, val: u32, order: Ordering) -> u32 {
+        let rv = self.load(order);
+        self.store(rv - val, order);
+        rv
+    }
+}
+
 mod common;
 
 cfg_if::cfg_if! {
