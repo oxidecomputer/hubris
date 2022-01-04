b92f1f1603803ddf3492ceb231a1e366581c9286
Matt Keeter <matt@oxide.computer>
2022-01-04 15:56:22 -0500

    Add SERDES10G TX config

--- task/vsc7448/src/serdes10g.rs
+++ task/vsc7448/src/serdes10g.rs
@@ -1,31 +1,162 @@
 /// Tools for working with the 10G SERDES (sd10g65 in the SDK)
 use crate::{Vsc7448Spi, VscError};
+use userlib::hl;
+use vsc7448_pac::Vsc7448;
 
 pub struct SetupTx {
     f_pll: FrequencySetup,
-    f_pll_khz_plain: u32,
+
+    mult: SynthMultCalc,
+
+    tx_synth_hrate_ena: bool,
+    tx_synth_off_comp_ena: u32,
+    pll_lpf_cur: u32,
+    pll_lpf_res: u32,
+    pllf_ref_cnt_end: u32,
 }
 
 impl SetupTx {
-    // vtss_calc_sd10g65_setup_tx
+    /// `vtss_calc_sd10g65_setup_tx`
     pub fn new() -> Result<Self, VscError> {
-        let mut cfg_f_pll = get_frequency_setup(SerdesMode::Lan10g);
+        let mut f_pll = get_frequency_setup(SerdesMode::Lan10g);
         let mut f_pll_khz_plain =
-            ((cfg_f_pll.f_pll_khz as u64 * cfg_f_pll.ratio_num as u64)
-                / (cfg_f_pll.ratio_den as u64)) as u32;
+            ((f_pll.f_pll_khz as u64 * f_pll.ratio_num as u64)
+                / (f_pll.ratio_den as u64)) as u32;
 
-        let half_rate_mode = if f_pll_khz_plain < 2_500_000 {
+        let tx_synth_hrate_ena = if f_pll_khz_plain < 2_500_000 {
             f_pll_khz_plain *= 2;
-            cfg_f_pll.f_pll_khz *= 2;
-            1
+            f_pll.f_pll_khz *= 2;
+            true
         } else {
-            0
+            false
         };
+
+        let mult = SynthMultCalc::new(&f_pll)?;
+
+        let tx_synth_off_comp_ena =
+            if f_pll_khz_plain > 10_312_500 { 31 } else { 23 };
+
+        let (pll_lpf_cur, pll_lpf_res) = if f_pll_khz_plain > 7000000 {
+            (3, 10)
+        } else if f_pll_khz_plain > 3000000 {
+            (2, 15)
+        } else {
+            (0, 10)
+        };
+
+        let if_width = 32;
+        let pllf_ref_cnt_end = if tx_synth_hrate_ena {
+            (if_width * 64 * 1000000) / (f_pll_khz_plain >> 1)
+        } else {
+            (if_width * 64 * 1000000) / f_pll_khz_plain
+        };
+
         // ... AND MORE
-        unimplemented!()
+        Ok(Self {
+            f_pll,
+            mult,
+            tx_synth_hrate_ena,
+            tx_synth_off_comp_ena,
+            pll_lpf_cur,
+            pll_lpf_res,
+            pllf_ref_cnt_end,
+        })
     }
-    /// `jaguar2c_sd10g_tx_register_cfg`
-    pub fn apply(&self, v: &mut Vsc7448Spi) -> Result<(), VscError> {
+    /// Based on `jaguar2c_sd10g_tx_register_cfg`, but turning variables
+    /// which never change into direct register assignments (rather than
+    /// assigning them and passing them around in the config struct)
+    pub fn apply(
+        &self,
+        index: u32,
+        v: &mut Vsc7448Spi,
+    ) -> Result<(), VscError> {
+        let dev = Vsc7448::XGANA(index);
+        let tx_synth = dev.SD10G65_TX_SYNTH();
+        let ob = dev.SD10G65_OB();
+        v.modify(ob.SD10G65_SBUS_TX_CFG(), |r| r.set_sbus_bias_en(1))?;
+        v.modify(dev.SD10G65_IB().SD10G65_SBUS_RX_CFG(), |r| {
+            r.set_sbus_bias_en(1)
+        })?;
+        v.modify(dev.SD10G65_OB().SD10G65_OB_CFG0(), |r| r.set_en_ob(1))?;
+        v.modify(dev.SD10G65_TX_RCPLL().SD10G65_TX_RCPLL_CFG2(), |r| {
+            r.set_pll_ena(1)
+        })?;
+        v.modify(tx_synth.SD10G65_TX_SYNTH_CFG0(), |r| {
+            r.set_synth_ena(1);
+            r.set_synth_spare_pool(7);
+            r.set_synth_off_comp_ena(self.tx_synth_off_comp_ena);
+            r.set_synth_speed_sel(self.mult.speed_sel.into());
+            r.set_synth_fbdiv_sel(self.mult.fbdiv_sel.into());
+        })?;
+        v.modify(tx_synth.SD10G65_TX_SYNTH_CFG3(), |r| {
+            r.set_synth_freqm_0((self.mult.settings.freqm & 0xFFFFFFFF) as u32);
+        })?;
+        v.modify(tx_synth.SD10G65_TX_SYNTH_CFG4(), |r| {
+            r.set_synth_freqn_0((self.mult.settings.freqn & 0xFFFFFFFF) as u32);
+        })?;
+        v.modify(tx_synth.SD10G65_TX_SYNTH_CFG1(), |r| {
+            r.set_synth_freq_mult_byp(1);
+            r.set_synth_freq_mult(self.mult.freq_mult_byp.freq_mult.into());
+            r.set_synth_freq_mult_hi(
+                self.mult.freq_mult_byp.freq_mult_hi as u32,
+            );
+            r.set_synth_freqm_1((self.mult.settings.freqm >> 32) as u32);
+            r.set_synth_freqn_1((self.mult.settings.freqn >> 32) as u32);
+        })?;
+        v.modify(tx_synth.SD10G65_TX_SYNTH_CFG0(), |r| {
+            r.set_synth_ls_speed(self.mult.tx_ls_speed.into());
+            r.set_synth_cs_speed(self.mult.tx_cs_speed.into());
+            r.set_synth_hrate_ena(self.tx_synth_hrate_ena.into());
+            // These aren't in the datasheet, but are copied from the SDK
+            r.set_synth_ena_sync_unit(1);
+            r.set_synth_conv_ena(1);
+            r.set_synth_ds_dir(0);
+            r.set_synth_ds_speed(0);
+            r.set_synth_ls_dir(0);
+            r.set_synth_ls_ena(0); // TODO: CHECK THIS
+        })?;
+        v.modify(tx_synth.SD10G65_SSC_CFG1(), |r| {
+            r.set_sync_ctrl_fsel(35);
+        })?;
+        // TODO: check ob.SD10G65_OB_CFG0/2 to make sure they're defaults
+
+        let tx_rcpll = dev.SD10G65_TX_RCPLL();
+        v.modify(tx_rcpll.SD10G65_TX_RCPLL_CFG2(), |r| {
+            r.set_pll_vco_cur(7);
+            r.set_pll_vreg18(10);
+            r.set_pll_lpf_cur(self.pll_lpf_cur);
+            r.set_pll_lpf_res(self.pll_lpf_res);
+        })?;
+        v.modify(tx_rcpll.SD10G65_TX_RCPLL_CFG0(), |r| {
+            // These also aren't in the datasheet; values are from the SDK
+            r.set_pllf_syn_clk_ena(0);
+            r.set_pllf_loop_ctrl_ena(0);
+            r.set_pllf_loop_ena(0);
+        })?;
+        v.modify(tx_rcpll.SD10G65_TX_RCPLL_CFG1(), |r| {
+            r.set_pllf_ref_cnt_end(self.pllf_ref_cnt_end);
+        })?;
+        v.modify(tx_rcpll.SD10G65_TX_RCPLL_CFG0(), |r| {
+            r.set_pllf_oor_recal_ena(1);
+        })?;
+
+        hl::sleep_for(10);
+        v.modify(tx_rcpll.SD10G65_TX_RCPLL_CFG0(), |r| {
+            r.set_pllf_ena(1);
+            r.set_pllf_oor_recal_ena(0);
+        })?;
+
+        hl::sleep_for(2);
+
+        let stat0 = v.read(tx_rcpll.SD10G65_TX_RCPLL_STAT0())?;
+        if stat0.pllf_lock_stat() != 1 {
+            return Err(VscError::PllLockFailed);
+        }
+        let stat1 = v.read(tx_rcpll.SD10G65_TX_RCPLL_STAT1())?;
+        if stat1.pllf_fsm_stat() != 13 {
+            return Err(VscError::PllFsmFailed);
+        }
+
         Ok(())
     }
 }
@@ -69,8 +200,8 @@ fn synth_settings_calc(num_in: u64, div_in: u64) -> SynthSettingsCalc {
     };
     SynthSettingsCalc {
         freq_mult,
-        freqm: freqm,
-        freqn: freqn,
+        freqm,
+        freqn,
     }
 }
 
@@ -107,38 +238,44 @@ struct SynthMultCalc {
     rx_i2_step: u8, // selects step width for integrator2. Only used when setting up the synthesizer for a RX macro
 }
 
-/// sd10g65_synth_mult_calc
-fn mult_calc(f_pll_in: FrequencySetup) -> Result<SynthMultCalc, VscError> {
-    let num_in_tmp = (f_pll_in.f_pll_khz as u64) / (f_pll_in.ratio_num as u64);
-    let div_in_tmp = (f_pll_in.ratio_den as u64) * 2_500_000;
-    let dr_khz = num_in_tmp / (f_pll_in.ratio_den as u64); // = f_pll_khz_plain?
+impl SynthMultCalc {
+    /// sd10g65_synth_mult_calc
+    fn new(f_pll_in: &FrequencySetup) -> Result<SynthMultCalc, VscError> {
+        let num_in_tmp =
+            (f_pll_in.f_pll_khz as u64) / (f_pll_in.ratio_num as u64);
+        let div_in_tmp = (f_pll_in.ratio_den as u64) * 2_500_000;
+        let dr_khz = num_in_tmp / (f_pll_in.ratio_den as u64); // = f_pll_khz_plain?
 
-    let mut out = SynthMultCalc::default();
+        let mut out = SynthMultCalc::default();
 
-    out.settings = match dr_khz {
-        0..=1_666_666 => return Err(VscError::SerdesFrequencyTooLow(dr_khz)),
-        1_666_667..=3_333_333 => {
-            out.rx_fb_step = 3;
-            synth_settings_calc(num_in_tmp, div_in_tmp)
-        }
-        3_333_334..=6_666_666 => {
-            out.fbdiv_sel = 1;
-            out.tx_cs_speed = 1;
-            out.rx_fb_step = 2;
-            synth_settings_calc(num_in_tmp, 2 * div_in_tmp)
-        }
-        6_666_667..=13_333_333 => {
-            out.fbdiv_sel = 2;
-            out.tx_ls_speed = 1;
-            out.tx_cs_speed = 1;
-            synth_settings_calc(num_in_tmp, 4 * div_in_tmp)
-        }
-        _ => return Err(VscError::SerdesFrequencyTooHigh(dr_khz)),
-    };
-    out.speed_sel = if dr_khz < 5_000_000 { true } else { false };
-    out.freq_mult_byp = FrequencyDecoderBypass::new(out.settings.freq_mult)?;
+        out.settings = match dr_khz {
+            0..=1_666_666 => {
+                return Err(VscError::SerdesFrequencyTooLow(dr_khz))
+            }
+            1_666_667..=3_333_333 => {
+                out.rx_fb_step = 3;
+                synth_settings_calc(num_in_tmp, div_in_tmp)
+            }
+            3_333_334..=6_666_666 => {
+                out.fbdiv_sel = 1;
+                out.tx_cs_speed = 1;
+                out.rx_fb_step = 2;
+                synth_settings_calc(num_in_tmp, 2 * div_in_tmp)
+            }
+            6_666_667..=13_333_333 => {
+                out.fbdiv_sel = 2;
+                out.tx_ls_speed = 1;
+                out.tx_cs_speed = 1;
+                synth_settings_calc(num_in_tmp, 4 * div_in_tmp)
+            }
+            _ => return Err(VscError::SerdesFrequencyTooHigh(dr_khz)),
+        };
+        out.speed_sel = if dr_khz < 5_000_000 { true } else { false };
+        out.freq_mult_byp =
+            FrequencyDecoderBypass::new(out.settings.freq_mult)?;
 
-    Ok(out)
+        Ok(out)
+    }
 }
 
 #[derive(Default)]
