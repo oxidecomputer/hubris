c56e2c386061ce366e2be78517489e7620699a65
Matt Keeter <matt@oxide.computer>
2022-01-14 17:07:02 -0500

    Add slightly higher-level abstractions for DEV1G/2G5

--- drv/vsc7448/src/bsp/sidecar.rs
+++ drv/vsc7448/src/bsp/sidecar.rs
@@ -1,5 +1,5 @@
 use crate::{
-    dev::{dev10g_init_sfi, dev1g_init_sgmii, dev2g5_init_sgmii},
+    dev::{dev10g_init_sfi, dev1g_init_sgmii, Dev10g, DevGeneric},
     phy::PhyRw,
     serdes10g, serdes1g, serdes6g,
     spi::Vsc7448Spi,
@@ -53,20 +53,20 @@ impl<'a> Bsp<'a> {
         // Cubbies 0 through 7
         let serdes1g_cfg_sgmii = serdes1g::Config::new(serdes1g::Mode::Sgmii);
         for dev in 0..=7 {
-            dev1g_init_sgmii(dev, &self.vsc7448)?;
+            dev1g_init_sgmii(DevGeneric::new_1g(dev), &self.vsc7448)?;
             serdes1g_cfg_sgmii.apply(dev + 1, &self.vsc7448)?;
             // DEV1G[dev], SERDES1G[dev + 1], S[port + 1], SGMII
         }
         // Cubbies 8 through 21
         let serdes6g_cfg_sgmii = serdes6g::Config::new(serdes6g::Mode::Sgmii);
         for dev in 0..=13 {
-            dev2g5_init_sgmii(dev, &self.vsc7448)?;
+            dev1g_init_sgmii(DevGeneric::new_2g5(dev), &self.vsc7448)?;
             serdes6g_cfg_sgmii.apply(dev, &self.vsc7448)?;
             // DEV2G5[dev], SERDES6G[dev], S[port + 1], SGMII
         }
         // Cubbies 22 through 29
         for dev in 16..=23 {
-            dev2g5_init_sgmii(dev, &self.vsc7448)?;
+            dev1g_init_sgmii(DevGeneric::new_2g5(dev), &self.vsc7448)?;
             serdes6g_cfg_sgmii.apply(dev, &self.vsc7448)?;
             // DEV2G5[dev], SERDES6G[dev], S[port + 1], SGMII
         }
@@ -74,7 +74,7 @@ impl<'a> Bsp<'a> {
         let serdes10g_cfg_sgmii =
             serdes10g::Config::new(serdes10g::Mode::Sgmii)?;
         for dev in [27, 28] {
-            dev2g5_init_sgmii(dev, &self.vsc7448)?;
+            dev1g_init_sgmii(DevGeneric::new_2g5(dev), &self.vsc7448)?;
             serdes10g_cfg_sgmii.apply(dev - 25, &self.vsc7448)?;
             // DEV2G5[dev], SERDES10G[dev - 25], S[dev + 8], SGMII
         }
@@ -88,8 +88,8 @@ impl<'a> Bsp<'a> {
 
         // Let's configure the on-board PHY first
         // Relevant pins are
-        // - MIIM_SP_TO_PHY_MDC_2V5
-        // - MIIM_SP_TO_PHY_MDIO_2V5
+        // - MIIM_SP_TO_PHY_MDC_2V5 (PC1)
+        // - MIIM_SP_TO_PHY_MDIO_2V5 (PA2)
         // - MIIM_SP_TO_PHY_MDINT_2V5_L
         // - SP_TO_PHY4_COMA_MODE (PI10, internal pull-up)
         // - SP_TO_PHY4_RESET_L (PI9)
@@ -154,18 +154,18 @@ impl<'a> Bsp<'a> {
         serdes6g_cfg_qsgmii.apply(14, &self.vsc7448)?;
         serdes6g_cfg_qsgmii.apply(15, &self.vsc7448)?;
         for dev in 16..=23 {
-            dev1g_init_sgmii(dev, &self.vsc7448)?;
+            dev1g_init_sgmii(DevGeneric::new_1g(dev), &self.vsc7448)?;
         }
 
         ////////////////////////////////////////////////////////////////////////
         // DEV2G5[24], SERDES1G[0], S0, SGMII to Local SP
         serdes1g_cfg_sgmii.apply(0, &self.vsc7448)?;
-        dev1g_init_sgmii(24, &self.vsc7448)?;
+        dev1g_init_sgmii(DevGeneric::new_1g(24), &self.vsc7448)?;
 
         ////////////////////////////////////////////////////////////////////////
         // DEV10G[0], SERDES10G[0], S33, SFI to Tofino 2
         let serdes_cfg = serdes10g::Config::new(serdes10g::Mode::Lan10g)?;
-        dev10g_init_sfi(0, &serdes_cfg, &self.vsc7448)?;
+        dev10g_init_sfi(Dev10g::new(0), &serdes_cfg, &self.vsc7448)?;
 
         Ok(())
     }
