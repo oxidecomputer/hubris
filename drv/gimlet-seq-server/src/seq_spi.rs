22227c7f4c10396ad26ac3fc41ad5ced1d0e7b9a
Bryan Cantrill <bryan@oxide.computer>
2022-01-15 10:32:04 -0800

    add license

--- drv/gimlet-seq-server/src/seq_spi.rs
+++ drv/gimlet-seq-server/src/seq_spi.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! Sequencer FPGA SPI+GPIO communication driver.
 //!
 //! This uses external shared SPI and GPIO servers to drive the FPGA.
