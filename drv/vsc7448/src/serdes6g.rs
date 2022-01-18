5071a751064fd0e9e26bb6b8ecc3c81f37f301d2
Matt Keeter <matt@oxide.computer>
2022-01-18 13:11:36 -0800

    LICENSE GET

--- drv/vsc7448/src/serdes6g.rs
+++ drv/vsc7448/src/serdes6g.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::{spi::Vsc7448Spi, VscError};
 use userlib::hl;
 use vsc7448_pac::Vsc7448;
