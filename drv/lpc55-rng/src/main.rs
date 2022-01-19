52e5933aca9a5941c99b0f2beb85a2f276dc7530
Philip Tricca <flihp@twobit.org>
2022-01-19 15:10:31 -0800

    lpc55-rng: Update interface to fill an arbitrary buffer with entropy.
    The RNG hardware on the lpc55 provides a 32bit register holding the
    entropy generated by the platform hardware. The previous interface would
    read a u32 from this register and return it the caller. The rust rand
    crate recommends using a 32 byte seed for CPRNGs[1] and making 8
    syscalls to get this much entropy seems inefficient.
    
    This commit changes the lpc55-rng syscall to fill an arbitrary buffer
    provided by the caller. This new interface is defined using the IDL. To
    simplify access to the RNG we add a client library that:
    1) wraps the new syscall in the RandCore trait from the 'rand_core' crate
    2) includes a custom 'getrandom' implementation gated by the 'custom-getrandom' feature
    
    This API crate allows consumers to use the familiar 'getrand' and
    'rand::os::OsRng' interfaces to get random bytes from the rng_server
    task.
    
    [1] https://docs.rs/rand/latest/rand/trait.SeedableRng.html

--- drv/lpc55-rng/src/main.rs
+++ drv/lpc55-rng/src/main.rs
@@ -2,48 +2,82 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! Random Number Generation
+//! Driver for the LPC55 random number generator.
 //!
-//! This task will produce random u32 values for you, if you ask nicely.
-//!
-//! An example:
-//!
-//! ```ignore
-//! #[derive(AsBytes)]
-//! #[repr(C)]
-//! struct FetchRandomNumber;
-//!
-//! impl hl::Call for FetchRandomNumber {
-//!     const OP: u16 = 0;
-//!     type Response = u32;
-//!     type Err = u32;
-//! }
-//!
-//! let num = hl::send(rng, &FetchRandomNumber).expect("could not ask the rng for a number");
-//!
-//! hprintln!("got {} from the rng", num).ok();
-//! ```
+//! Use the rng-api crate to interact with this driver.
 
 #![no_std]
 #![no_main]
 
+use core::mem::size_of;
 use drv_lpc55_syscon_api::{Peripheral, Syscon};
+use drv_rng_api::RngError;
+use idol_runtime::{ClientError, RequestError};
+use rand_core::block::{BlockRng, BlockRngCore};
+use rand_core::RngCore;
 use userlib::*;
-use zerocopy::AsBytes;
 
 use lpc55_pac as device;
 
 task_slot!(SYSCON, syscon_driver);
 
-#[repr(u32)]
-enum ResponseCode {
-    BadArg = 1,
-    PoweredOff = 2,
+struct Lpc55BlockRngCore {
+    rng: &'static lpc55_pac::rng::RegisterBlock,
+    pmc: &'static lpc55_pac::pmc::RegisterBlock,
 }
 
-impl From<ResponseCode> for u32 {
-    fn from(rc: ResponseCode) -> Self {
-        rc as u32
+impl Lpc55BlockRngCore {
+    fn new() -> Self {
+        Lpc55BlockRngCore {
+            rng: unsafe { &*device::RNG::ptr() },
+            pmc: unsafe { &*device::PMC::ptr() },
+        }
+    }
+}
+
+impl BlockRngCore for Lpc55BlockRngCore {
+    type Item = u32;
+    type Results = [u32; 1];
+
+    fn generate(&mut self, results: &mut Self::Results) {
+        results[0] = self.rng.random_number.read().bits();
+    }
+}
+
+type Lpc55BlockRng = BlockRng<Lpc55BlockRngCore>;
+
+impl idl::InOrderRngImpl for Lpc55BlockRng {
+    fn fill(
+        &mut self,
+        _: &userlib::RecvMessage,
+        dest: idol_runtime::Leased<idol_runtime::W, [u8]>,
+    ) -> Result<usize, RequestError<RngError>> {
+        // if the oscilator is powered off, we won't get good RNG.
+        if self.core.pmc.pdruncfg0.read().pden_rng().is_poweredoff() {
+            return Err(RequestError::Runtime(RngError::PoweredOff));
+        }
+
+        let mut cnt = 0;
+        const STEP: usize = size_of::<u32>();
+        // fill in multiples of STEP / RNG register size
+        for _ in 0..(dest.len() / STEP) {
+            let number = self.next_u32();
+            dest.write_range(cnt..cnt + STEP, &number.to_ne_bytes()[0..STEP])
+                .map_err(|_| RequestError::Fail(ClientError::WentAway))?;
+            cnt += STEP;
+        }
+        // fill in remaining
+        let remain = dest.len() - cnt;
+        if remain > STEP {
+            panic!("RNG state machine bork");
+        }
+        if remain > 0 {
+            let ent = self.next_u32().to_ne_bytes();
+            dest.write_range(dest.len() - remain..dest.len(), &ent[..remain])
+                .map_err(|_| RequestError::Fail(ClientError::WentAway))?;
+            cnt += remain;
+        }
+        Ok(cnt)
     }
 }
 
@@ -54,29 +88,18 @@ fn main() -> ! {
 
     syscon.enable_clock(Peripheral::Rng);
 
-    let rng = unsafe { &*device::RNG::ptr() };
-    let pmc = unsafe { &*device::PMC::ptr() };
+    let rng = Lpc55BlockRngCore::new();
+    let mut rng = Lpc55BlockRng::new(rng);
 
-    let mut buffer = [0u32; 1];
+    let mut buffer = [0u8; idl::INCOMING_SIZE];
 
     loop {
-        hl::recv_without_notification(
-            buffer.as_bytes_mut(),
-            |_op: u16, msg| -> Result<(), ResponseCode> {
-                let (_msg, caller) =
-                    msg.fixed::<(), u32>().ok_or(ResponseCode::BadArg)?;
-
-                // if the oscilator is powered off, we won't get good RNG.
-                if pmc.pdruncfg0.read().pden_rng().is_poweredoff() {
-                    return Err(ResponseCode::PoweredOff);
-                }
-
-                let number = rng.random_number.read().bits();
-
-                caller.reply(number);
-
-                Ok(())
-            },
-        );
+        idol_runtime::dispatch(&mut buffer, &mut rng);
     }
+}
+
+mod idl {
+    use drv_rng_api::RngError;
+
+    include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
 }
