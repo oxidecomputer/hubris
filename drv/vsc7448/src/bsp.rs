4e62f24c91cbda155db225d61b31f7b1cb5e9c08
Matt Keeter <matt@oxide.computer>
2022-01-20 10:24:19 -0800

    Reorganize BSPs

--- drv/vsc7448/src/bsp.rs
+++ drv/vsc7448/src/bsp.rs
@@ -2,5 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// These modules are exported so that we don't have warnings about unused code,
+// but you should import Bsp instead, which is autoselected based on board.
 pub mod gemini_bu;
-pub mod sidecar;
+pub mod sidecar_1;
+
+cfg_if::cfg_if! {
+    if #[cfg(target_board = "gemini-bu-1")] {
+        pub use gemini_bu::Bsp;
+    } else if #[cfg(target_board = "sidecar-1")] {
+        pub use sidecar_1::Bsp;
+    } else {
+        compile_error!("No BSP available for this board");
+    }
+}
--- task/net/src/bsp.rs
+++ task/net/src/bsp.rs
@@ -1,10 +1,17 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// These modules are exported so that we don't have warnings about unused code,
+// but you should import Bsp instead, which is autoselected based on board.
+
 cfg_if::cfg_if! {
     if #[cfg(target_board = "nucleo-h743zi2")] {
-        mod nucleo_h743zi2;
-        pub use nucleo_h743zi2::{configure_ethernet_pins, configure_phy};
+        pub mod nucleo_h743zi2;
+        pub use nucleo_h743zi2 as Bsp;
     } else if #[cfg(target_board = "sidecar-1")] {
-        mod sidecar_1;
-        pub use sidecar_1::{configure_ethernet_pins, configure_phy};
+        pub mod sidecar_1;
+        pub use sidecar_1 as Bsp;
     } else {
         compile_error!("Board is not supported by the task/net");
     }
