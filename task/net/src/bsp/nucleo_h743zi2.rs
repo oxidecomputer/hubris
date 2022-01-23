9e87135f65f940591c7a5fe96a709fc37ff994b2
Matt Keeter <matt@oxide.computer>
2022-01-23 09:28:20 -0500

    License update
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- task/net/src/bsp/nucleo_h743zi2.rs
+++ task/net/src/bsp/nucleo_h743zi2.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use drv_stm32h7_eth as eth;
 use drv_stm32xx_sys_api::{self as sys_api, Sys};
 
