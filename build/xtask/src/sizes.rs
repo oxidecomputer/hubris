79ef632918b91a2b3b52ada2b5957caf535dc35f
Matt Keeter <matt@oxide.computer>
2022-01-26 09:57:18 -0500

    Updates per review

--- build/xtask/src/sizes.rs
+++ build/xtask/src/sizes.rs
@@ -12,12 +12,7 @@ use indexmap::IndexMap;
 use crate::Config;
 
 fn armv7m_suggest(size: u64) -> u64 {
-    // Nearest power of two
-    let mut i = 1;
-    while i < size {
-        i *= 2;
-    }
-    i
+    size.next_power_of_two()
 }
 fn armv8m_suggest(size: u64) -> u64 {
     // Nearest chunk of 32
@@ -46,95 +41,87 @@ pub fn run(cfg: &Path) -> anyhow::Result<()> {
             .map(|(name, _)| name.as_str())
     };
 
-    struct Task<'a> {
-        name: &'a str,
-        stacksize: u32,
-        requires: &'a IndexMap<String, u32>,
-    }
-    let mut tasks: Vec<Task> = toml
-        .tasks
-        .iter()
-        .map(|(name, task)| Task {
-            name,
-            stacksize: task
-                .stacksize
-                .unwrap_or_else(|| toml.stacksize.unwrap()),
-            requires: &task.requires,
-        })
-        .collect();
-    tasks.push(Task {
-        name: "kernel",
-        stacksize: toml.stacksize.unwrap(),
-        requires: &toml.kernel.requires,
-    });
-
-    let suggest = if toml.target.starts_with("thumbv7m")
-        || toml.target.starts_with("thumbv7em")
-    {
+    let suggest = if toml.target.starts_with("thumbv7") {
         armv7m_suggest
     } else if toml.target.starts_with("thumbv8m") {
         armv8m_suggest
     } else {
-        panic!("Unknown target {}", toml.target);
+        panic!("Unknown target: {}", toml.target);
     };
 
     let mut suggestions = Vec::new();
-    let mut first = false; // used to print a newline between tasks
-    for task in tasks {
-        if !first {
-            first = true;
-        } else {
-            println!();
-        }
-        let mut elf_name = dist_dir.clone();
-        elf_name.push(task.name);
-        let buffer = std::fs::read(elf_name)?;
-        let elf = match Object::parse(&buffer)? {
-            Object::Elf(elf) => elf,
-            o => bail!("Invalid Object {:?}", o),
+    let mut check_task =
+        |name: &str, stacksize: u32, requires: &IndexMap<String, u32>| {
+            let mut elf_name = dist_dir.clone();
+            elf_name.push(name);
+            let buffer = std::fs::read(elf_name)?;
+            let elf = match Object::parse(&buffer)? {
+                Object::Elf(elf) => elf,
+                o => bail!("Invalid Object {:?}", o),
+            };
+
+            let mut memory_sizes = IndexMap::new();
+            for phdr in &elf.program_headers {
+                // The PhysAddr is always a flash address, meaning we use the
+                // "on-disk" size of this section.
+                if let Some(region) = output_region(phdr.p_paddr) {
+                    *memory_sizes.entry(region).or_default() += phdr.p_filesz;
+                }
+                // If the VirtAddr disagrees with the PhysAddr, then this is a
+                // section which is relocated into RAM, so we also accumulate
+                // its MemSiz.
+                if phdr.p_paddr != phdr.p_vaddr {
+                    let region = output_region(phdr.p_vaddr).unwrap();
+                    *memory_sizes.entry(region).or_default() += phdr.p_memsz;
+                }
+            }
+            *memory_sizes.entry("ram").or_default() += stacksize as u64;
+
+            println!("{}", name);
+            let mut my_suggestions = Vec::new();
+            let mut has_asterisk = false;
+            for (mem_name, used) in memory_sizes {
+                let asterisk = name == "kernel" && mem_name == "ram";
+                has_asterisk |= asterisk;
+                if let Some(&size) = requires.get(mem_name) {
+                    let percent = used * 100 / size as u64;
+                    println!(
+                        "  {:<6} {: >5} bytes ({}%){}",
+                        format!("{}:", mem_name),
+                        used,
+                        percent,
+                        if asterisk { "*" } else { "" },
+                    );
+                    let suggestion = suggest(used);
+                    if suggestion != size as u64 && !asterisk {
+                        my_suggestions.push((mem_name, size, suggestion));
+                    }
+                } else {
+                    assert!(used == 0);
+                }
+            }
+            // TODO: remove this once PR #393 is merged
+            if has_asterisk {
+                println!("  * kernel uses spare RAM for dynamic allocation");
+            }
+            if !my_suggestions.is_empty() {
+                suggestions.push((name.to_owned(), my_suggestions));
+            }
+            Ok(())
         };
 
-        let mut memory_sizes = memories
-            .keys()
-            .map(|name| (name.as_str(), 0))
-            .collect::<IndexMap<_, _>>();
-        for phdr in &elf.program_headers {
-            if let Some(region) = output_region(phdr.p_vaddr) {
-                memory_sizes[region] += phdr.p_memsz;
-            }
-        }
-        memory_sizes["ram"] += task.stacksize as u64;
-
-        println!("{}", task.name);
-        let mut my_suggestions = Vec::new();
-        for name in memories.keys() {
-            let used = memory_sizes[name.as_str()];
-            if let Some(&size) = task.requires.get(name) {
-                let percent = used * 100 / size as u64;
-                println!(
-                    "  {:<6} {: >5} bytes ({}%)",
-                    format!("{}:", name),
-                    used,
-                    percent
-                );
-                let suggestion = suggest(used);
-                if suggestion != size as u64 {
-                    my_suggestions.push((name, size, suggestion));
-                }
-            } else {
-                assert!(used == 0);
-            }
-        }
-        if !my_suggestions.is_empty() {
-            suggestions.push((task.name, my_suggestions));
-        }
+    check_task("kernel", toml.stacksize.unwrap(), &toml.kernel.requires)?;
+    for (name, task) in &toml.tasks {
+        println!();
+        check_task(
+            &name,
+            task.stacksize.unwrap_or_else(|| toml.stacksize.unwrap()),
+            &task.requires,
+        )?;
     }
 
     if !suggestions.is_empty() {
-        let mut savings = memories
-            .keys()
-            .map(|name| (name.as_str(), 0))
-            .collect::<IndexMap<_, _>>();
+        let mut savings: IndexMap<_, u64> = IndexMap::new();
         println!("\n========== Suggested changes ==========");
         for (name, list) in suggestions {
             println!("{}:", name);
@@ -145,7 +132,7 @@ pub fn run(cfg: &Path) -> anyhow::Result<()> {
                     value,
                     prev
                 );
-                savings[mem.as_str()] += prev as u64 - value;
+                *savings.entry(mem).or_default() += prev as u64 - value;
             }
         }
         println!("\n------------ Total savings ------------");
