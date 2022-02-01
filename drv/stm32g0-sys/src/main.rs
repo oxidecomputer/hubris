2935ba28114452337c93abb15e301e4d1de8749b
Cliff L. Biffle <cliff@oxide.computer>
2022-02-01 11:36:57 -0800

    Factor common GPIO code out of stm32{g0,h7}
    This is a step toward reducing the overall high level of code
    duplication between stm32 targets. It exploits some traits that the PACs
    started generating circa 0.14, which are unfortunately not put into a
    common crate, but, hey, at least they exist now.
    
    Knocks some bytes (100+) off GPIO servers by reducing macro-driven code
    duplication and giving the compiler an easier task.
    
    This is sort of a HAL, but unlike most HALs it's very careful to allow
    static monomorphization on properties you know at compile time (like
    your SoC model target) and only use dynamic dispatch for things you vary
    at runtime (like which gpio port you're poking).
    
    Scary bits are mostly hidden in the server.rs file.

--- drv/stm32g0-sys/src/main.rs
+++ drv/stm32g0-sys/src/main.rs
@@ -16,7 +16,8 @@ use stm32g0::stm32g070 as device;
 #[cfg(feature = "g0b1")]
 use stm32g0::stm32g0b1 as device;
 
-use drv_stm32g0_sys_api::{GpioError, Port, RccError};
+use drv_stm32g0_sys_api::{GpioError, RccError};
+use drv_stm32xx_gpio_common::{server::get_gpio_regs, Port};
 use idol_runtime::RequestError;
 use userlib::*;
 
@@ -79,106 +80,6 @@ fn main() -> ! {
     loop {
         idol_runtime::dispatch(&mut buffer, &mut server);
     }
-}
-
-// These macros are required as the different GPIO ports return different types
-// for some reason (GPIOA returns gpioa, GPIO[B-F] return gpiob?!).
-macro_rules! do_configure {
-    ($reg:expr, $pins:expr, $packed_attributes:expr) => {{
-        // The GPIO config registers come in 1, 2, and 4-bit per
-        // field variants. The user-submitted mask is already
-        // correct for the 1-bit fields; we need to expand it
-        // into corresponding 2- and 4-bit masks. We use an
-        // outer perfect shuffle operation for this, which
-        // interleaves zeroes from the top 16 bits into the
-        // bottom 16.
-
-        // 1 in each targeted 1bit field.
-        let mask_1 = u32::from($pins);
-        // 0b01 in each targeted 2bit field.
-        let lsbs_2 = outer_perfect_shuffle(mask_1);
-        // 0b0001 in each targeted 4bit field for low half.
-        let lsbs_4l = outer_perfect_shuffle(lsbs_2 & 0xFFFF);
-        // Same for high half.
-        let lsbs_4h = outer_perfect_shuffle(lsbs_2 >> 16);
-
-        // Corresponding masks, with 1s in all field bits
-        // instead of just the LSB:
-        let mask_2 = lsbs_2.wrapping_mul(0b11);
-        let mask_4l = lsbs_4l.wrapping_mul(0b1111);
-        let mask_4h = lsbs_4h.wrapping_mul(0b1111);
-
-        let atts = $packed_attributes;
-
-        // MODER contains 16x 2-bit fields.
-        let moder_val = u32::from(atts & 0b11);
-        $reg.moder.write(|w| unsafe {
-            w.bits(
-                ($reg.moder.read().bits() & !mask_2)
-                    | (moder_val.wrapping_mul(lsbs_2)),
-            )
-        });
-        // OTYPER contains 16x 1-bit fields.
-        let otyper_val = u32::from((atts >> 2) & 1);
-        $reg.otyper.write(|w| unsafe {
-            w.bits(
-                ($reg.otyper.read().bits() & !mask_1)
-                    | (otyper_val.wrapping_mul(mask_1)),
-            )
-        });
-        // OSPEEDR contains 16x 2-bit fields.
-        let ospeedr_val = u32::from((atts >> 3) & 0b11);
-        $reg.ospeedr.write(|w| unsafe {
-            w.bits(
-                ($reg.ospeedr.read().bits() & !mask_2)
-                    | (ospeedr_val.wrapping_mul(lsbs_2)),
-            )
-        });
-        // PUPDR contains 16x 2-bit fields.
-        let pupdr_val = u32::from((atts >> 5) & 0b11);
-        $reg.pupdr.write(|w| unsafe {
-            w.bits(
-                ($reg.pupdr.read().bits() & !mask_2)
-                    | (pupdr_val.wrapping_mul(lsbs_2)),
-            )
-        });
-        // AFRx contains 8x 4-bit fields.
-        let af_val = u32::from((atts >> 7) & 0b1111);
-        $reg.afrl.write(|w| unsafe {
-            w.bits(
-                ($reg.afrl.read().bits() & !mask_4l)
-                    | (af_val.wrapping_mul(lsbs_4l)),
-            )
-        });
-        $reg.afrh.write(|w| unsafe {
-            w.bits(
-                ($reg.afrh.read().bits() & !mask_4h)
-                    | (af_val.wrapping_mul(lsbs_4h)),
-            )
-        });
-    }};
-}
-
-macro_rules! do_set_reset {
-    ($reg:expr, $set:expr, $reset:expr) => {
-        $reg.bsrr.write(|w| unsafe {
-            w.bits((u32::from($reset) << 16) | u32::from($set))
-        })
-    };
-}
-
-macro_rules! do_toggle {
-    ($reg:expr, $pins:expr) => {{
-        // Read current pin *output* states.
-        let state = $reg.odr.read().bits();
-        // Compute BSRR value to toggle all pins. That is, move
-        // currently set bits into reset position, and set unset
-        // bits.
-        let bsrr_all = state << 16 | state ^ 0xFFFF;
-        // Write that value, but masked as the user requested.
-        let bsrr_mask = u32::from($pins) | u32::from($pins) << 16;
-        $reg.bsrr.write(|w| unsafe { w.bits(bsrr_all & bsrr_mask) });
-    }};
 }
 
 struct ServerImpl<'a> {
@@ -257,24 +158,7 @@ impl idl::InOrderSysImpl for ServerImpl<'_> {
         pins: u16,
         packed_attributes: u16,
     ) -> Result<(), RequestError<GpioError>> {
-        match port {
-            Port::A => {
-                let gpio = unsafe { &*device::GPIOA::ptr() };
-                do_configure!(gpio, pins, packed_attributes);
-            }
-            _ => {
-                let gpio = match port {
-                    Port::B => unsafe { &*device::GPIOB::ptr() },
-                    Port::C => unsafe { &*device::GPIOC::ptr() },
-                    Port::D => unsafe { &*device::GPIOD::ptr() },
-                    #[cfg(feature = "g0b1")]
-                    Port::E => unsafe { &*device::GPIOE::ptr() },
-                    Port::F => unsafe { &*device::GPIOF::ptr() },
-                    _ => unreachable!(),
-                };
-                do_configure!(gpio, pins, packed_attributes);
-            }
-        }
+        unsafe { get_gpio_regs(port) }.configure(pins, packed_attributes);
         Ok(())
     }
 
@@ -285,24 +169,7 @@ impl idl::InOrderSysImpl for ServerImpl<'_> {
         set_pins: u16,
         reset_pins: u16,
     ) -> Result<(), RequestError<GpioError>> {
-        match port {
-            Port::A => {
-                let gpio = unsafe { &*device::GPIOA::ptr() };
-                do_set_reset!(gpio, set_pins, reset_pins);
-            }
-            _ => {
-                let gpio = match port {
-                    Port::B => unsafe { &*device::GPIOB::ptr() },
-                    Port::C => unsafe { &*device::GPIOC::ptr() },
-                    Port::D => unsafe { &*device::GPIOD::ptr() },
-                    #[cfg(feature = "g0b1")]
-                    Port::E => unsafe { &*device::GPIOE::ptr() },
-                    Port::F => unsafe { &*device::GPIOF::ptr() },
-                    _ => unreachable!(),
-                };
-                do_set_reset!(gpio, set_pins, reset_pins);
-            }
-        }
+        unsafe { get_gpio_regs(port) }.set_reset(set_pins, reset_pins);
         Ok(())
     }
 
@@ -312,24 +179,7 @@ impl idl::InOrderSysImpl for ServerImpl<'_> {
         port: Port,
         pins: u16,
     ) -> Result<(), RequestError<GpioError>> {
-        match port {
-            Port::A => {
-                let gpio = unsafe { &*device::GPIOA::ptr() };
-                do_toggle!(gpio, pins);
-            }
-            _ => {
-                let gpio = match port {
-                    Port::B => unsafe { &*device::GPIOB::ptr() },
-                    Port::C => unsafe { &*device::GPIOC::ptr() },
-                    Port::D => unsafe { &*device::GPIOD::ptr() },
-                    #[cfg(feature = "g0b1")]
-                    Port::E => unsafe { &*device::GPIOE::ptr() },
-                    Port::F => unsafe { &*device::GPIOF::ptr() },
-                    _ => unreachable!(),
-                };
-                do_toggle!(gpio, pins);
-            }
-        }
+        unsafe { get_gpio_regs(port) }.toggle(pins);
         Ok(())
     }
 
@@ -338,48 +188,10 @@ impl idl::InOrderSysImpl for ServerImpl<'_> {
         _: &RecvMessage,
         port: Port,
     ) -> Result<u16, RequestError<GpioError>> {
-        match port {
-            Port::A => {
-                let gpio = unsafe { &*device::GPIOA::ptr() };
-                Ok(gpio.idr.read().bits() as u16)
-            }
-            _ => {
-                let gpio = match port {
-                    Port::B => unsafe { &*device::GPIOB::ptr() },
-                    Port::C => unsafe { &*device::GPIOC::ptr() },
-                    Port::D => unsafe { &*device::GPIOD::ptr() },
-                    #[cfg(feature = "g0b1")]
-                    Port::E => unsafe { &*device::GPIOE::ptr() },
-                    Port::F => unsafe { &*device::GPIOF::ptr() },
-                    _ => unreachable!(),
-                };
-                Ok(gpio.idr.read().bits() as u16)
-            }
-        }
+        Ok(unsafe { get_gpio_regs(port) }.read())
     }
 }
 
-/// Interleaves bits in `input` as follows:
-///
-/// - Output bit 0 = input bit 0
-/// - Output bit 1 = input bit 15
-/// - Output bit 2 = input bit 1
-/// - Output bit 3 = input bit 16
-/// ...and so forth.
-///
-/// This is a great example of one of those bit twiddling tricks you never
-/// expected to need. Method from Hacker's Delight.
-const fn outer_perfect_shuffle(mut input: u32) -> u32 {
-    let mut tmp = (input ^ (input >> 8)) & 0x0000ff00;
-    input ^= tmp ^ (tmp << 8);
-    tmp = (input ^ (input >> 4)) & 0x00f000f0;
-    input ^= tmp ^ (tmp << 4);
-    tmp = (input ^ (input >> 2)) & 0x0c0c0c0c;
-    input ^= tmp ^ (tmp << 2);
-    tmp = (input ^ (input >> 1)) & 0x22222222;
-    input ^= tmp ^ (tmp << 1);
-    input
-}
 mod idl {
     use super::{GpioError, Port, RccError};
 
---
+++ drv/stm32xx-gpio-common/src/lib.rs
@@ -0,0 +1,176 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! GPIO-related things needed by all STM32 parts.
+
+#![no_std]
+
+use userlib::FromPrimitive;
+use zerocopy::AsBytes;
+
+#[cfg(feature = "server-support")]
+pub mod server;
+
+/// Enumerates the GPIO ports available on this chip, from the perspective of
+/// driver software. This does not mean the GPIO port is physically available on
+/// pins of the package -- we don't model package differences.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, FromPrimitive, AsBytes)]
+#[repr(u8)]
+pub enum Port {
+    A = 0,
+    B,
+    C,
+    D,
+    #[cfg(feature = "has-port-gpioe")]
+    E,
+    #[cfg(feature = "has-port-gpiof")]
+    F,
+    #[cfg(feature = "has-port-gpiog")]
+    G,
+    #[cfg(feature = "has-port-gpioh")]
+    H,
+    #[cfg(feature = "has-port-gpioi")]
+    I,
+    #[cfg(feature = "has-port-gpioj")]
+    J,
+    #[cfg(feature = "has-port-gpiok")]
+    K,
+}
+
+impl Port {
+    /// Turns a `Port` into a `PinSet` containing one pin, number `index`.
+    #[inline(always)]
+    pub const fn pin(self, index: usize) -> PinSet {
+        PinSet {
+            port: self,
+            pin_mask: 1 << index,
+        }
+    }
+}
+
+/// The STM32xx GPIO hardware lets us configure up to 16 pins on the same port
+/// at a time, and we expose this in the API. A `PinSet` describes the target of
+/// a configuration operation.
+///
+/// A `PinSet` can technically be empty (`pin_mask` of zero) but that's rarely
+/// useful.
+#[derive(Copy, Clone, Debug)]
+pub struct PinSet {
+    /// Port we're talking about.
+    pub port: Port,
+    /// Mask with 1s in affected positions, 0s in others.
+    pub pin_mask: u16,
+}
+
+impl PinSet {
+    /// Derives a `PinSet` by setting mask bit `index`.
+    #[inline(always)]
+    pub const fn and_pin(self, index: usize) -> Self {
+        Self {
+            pin_mask: self.pin_mask | 1 << index,
+            ..self
+        }
+    }
+}
+
+/// Possible modes for a GPIO pin.
+#[derive(Copy, Clone, Debug, PartialEq, FromPrimitive)]
+pub enum Mode {
+    /// Digital input. This activates a Schmitt trigger on the pin, which is
+    /// great for receiving digital signals, but can burn a lot of current if
+    /// faced with signals intermediate between 1 and 0. As a result, to treat a
+    /// pin as unused, set it to `Analog`.
+    Input = 0b00,
+    /// Software-controlled output. Values written to the corresponding bit of
+    /// the ODR register will control the pin's driver.
+    Output = 0b01,
+    /// Alternate function. This disconnects the direct GPIO driver from the pin
+    /// and instead connects it to the function mux, which in turn connects it
+    /// to a peripheral signal chosen by one of the `AFx` values written to
+    /// AFRL/AFRH.
+    Alternate = 0b10,
+    /// Analog input. This disconnects the output driver, input Schmitt trigger,
+    /// and function mux from the pin, and is the highest-impedance state. It is
+    /// _also_ useful for analog if the pin has an ADC channel attached.
+    Analog = 0b11,
+}
+
+/// Drive modes for a GPIO pin.
+#[derive(Copy, Clone, Debug, PartialEq, FromPrimitive)]
+pub enum OutputType {
+    /// The pin will be driven both high and low in `Output` and `Alternate`
+    /// modes.
+    PushPull = 0,
+    /// Turns off the pin's high side driver in `Output` and `Alternate` modes,
+    /// so that setting the pin to 0 pulls low, but 1 enters a high impedance
+    /// state.
+    OpenDrain = 1,
+}
+
+/// Drive speeds / slew rate limits for GPIO pins.
+///
+/// When in doubt, use `Low`. It's fast enough for most things and is less prone
+/// to generating reflections and EMI. Note that you need to check the datasheet
+/// for the specific part you're targeting to get the actual speeds of these
+/// drive settings. The notes below are thus vague.
+#[derive(Copy, Clone, Debug, PartialEq, FromPrimitive)]
+pub enum Speed {
+    /// Slowest and generally correct drive speed (up to, say, 10MHz or so).
+    Low = 0b00,
+    /// Somewhat faster (say, 50MHz).
+    Medium = 0b01,
+    /// Somewhat faster-er (idk like 80MHz? Go read the datasheet)
+    High = 0b10,
+    /// Go read the datasheet.
+    VeryHigh = 0b11,
+}
+
+/// Settings for the switchable weak pull resistors on GPIO pins.
+///
+/// Note that the pull resistors apply in all modes, so, you can apply these to
+/// an input, and you will want to turn them off for `Analog`.
+#[derive(Copy, Clone, Debug, PartialEq, FromPrimitive)]
+pub enum Pull {
+    /// Both resistors off.
+    None = 0b00,
+    /// Weak pull up.
+    Up = 0b01,
+    /// Weak pull down.
+    Down = 0b10,
+}
+
+/// Enumeration of alternate functions that can be stuffed into the AFRL/AFRH
+/// registers to change pin muxes. These only apply when the pin is in
+/// `Alternate` mode.
+///
+/// These are numbers and not, like, convenient human-readable peripheral names
+/// because the mapping from pin + AF to signal is very complex. See the
+/// datasheet.
+#[derive(Copy, Clone, Debug, PartialEq, FromPrimitive)]
+pub enum Alternate {
+    AF0 = 0,
+    AF1 = 1,
+    AF2 = 2,
+    AF3 = 3,
+    AF4 = 4,
+    AF5 = 5,
+    AF6 = 6,
+    AF7 = 7,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF8 = 8,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF9 = 9,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF10 = 10,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF11 = 11,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF12 = 12,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF13 = 13,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF14 = 14,
+    #[cfg(feature = "has-af8-thru-af15")]
+    AF15 = 15,
+}
