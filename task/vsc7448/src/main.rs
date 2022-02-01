570b983269969d9cc22d83122eaaadf093ea60f9
Matt Keeter <matt@oxide.computer>
2022-02-01 14:59:36 -0500

    Refactor startup and sentinel handling

--- task/vsc7448/src/main.rs
+++ task/vsc7448/src/main.rs
@@ -9,21 +9,50 @@ mod bsp;
 
 use bsp::Bsp;
 use drv_spi_api::Spi;
+use ringbuf::*;
 use userlib::*;
-use vsc7448::spi::Vsc7448Spi;
+use vsc7448::{spi::Vsc7448Spi, VscError};
 
 task_slot!(SPI, spi_driver);
 const VSC7448_SPI_DEVICE: u8 = 0;
+
+#[derive(Copy, Clone, PartialEq)]
+enum Trace {
+    None,
+    ChipInit(u64),
+    ChipInitFailed(VscError),
+    BspInit(u64),
+    BspInitFailed(VscError),
+}
+ringbuf!(Trace, 2, Trace::None);
 
 #[export_name = "main"]
 fn main() -> ! {
     let spi = Spi::from(SPI.get_task_id()).device(VSC7448_SPI_DEVICE);
     let vsc7448 = Vsc7448Spi(spi);
 
-    // `init` does a full chip reset, so we can run it multiple times
-    // (although if it fails once, it's likely to fail repeatedly)
-    match vsc7448::init(&vsc7448).and_then(|_| Bsp::new(&vsc7448)) {
-        Ok(mut bsp) => bsp.run(), // Does not terminate
-        Err(e) => panic!("Could not initialize: {:?}", e),
+    let t0 = sys_get_timer().now;
+    match vsc7448::init(&vsc7448) {
+        Ok(()) => {
+            let t1 = sys_get_timer().now;
+            ringbuf_entry!(Trace::ChipInit(t1 - t0));
+        }
+        Err(e) => {
+            ringbuf_entry!(Trace::ChipInitFailed(e));
+            panic!("Could not initialize chip: {:?}", e);
+        }
+    }
+
+    let t0 = sys_get_timer().now;
+    match Bsp::new(&vsc7448) {
+        Ok(mut bsp) => {
+            let t1 = sys_get_timer().now;
+            ringbuf_entry!(Trace::BspInit(t1 - t0));
+            bsp.run(); // Does not terminate
+        }
+        Err(e) => {
+            ringbuf_entry!(Trace::BspInitFailed(e));
+            panic!("Could not initialize BSP: {:?}", e);
+        }
     }
 }
