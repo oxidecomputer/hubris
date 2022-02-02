9b2ad80f5cc8e9f16a3e23c00722299bf180e09a
Matt Keeter <matt@oxide.computer>
2022-02-02 15:14:51 -0500

    Add VSC7448 image to Gimletlet

--- task/vsc7448/src/bsp.rs
+++ task/vsc7448/src/bsp.rs
@@ -6,7 +6,10 @@
 // but you should import Bsp instead, which is autoselected based on board.
 
 cfg_if::cfg_if! {
-    if #[cfg(target_board = "gemini-bu-1")] {
+    // We use the gemini_bu Bsp for both the Gemini BU and the Gimletlet host.
+    // In both cases, these are driving a VSC7448 dev kit on someone's desk.
+    if #[cfg(any(target_board = "gemini-bu-1",
+                 target_board = "gimletlet-2"))] {
         pub mod gemini_bu;
         pub use gemini_bu::Bsp;
     } else if #[cfg(target_board = "sidecar-1")] {
