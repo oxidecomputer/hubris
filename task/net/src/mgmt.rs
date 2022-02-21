7af8a7f8fa3b523bc7f96d866c3b89c20c947009
Matt Keeter <matt@oxide.computer>
2022-02-21 14:13:01 -0500

    Reorganize and use new pins:: in Nucleo H7

--- task/net/src/mgmt.rs
+++ task/net/src/mgmt.rs
@@ -5,74 +5,10 @@
 use crate::miim_bridge::MiimBridge;
 use drv_spi_api::SpiDevice;
 use drv_stm32h7_eth::Ethernet;
-use drv_stm32xx_sys_api::{
-    self as sys_api, Mode, OutputType, Pull, Speed, Sys,
-};
+use drv_stm32xx_sys_api::{self as sys_api, OutputType, Pull, Speed, Sys};
 use ksz8463::Ksz8463;
 use userlib::hl::sleep_for;
 use vsc85xx::Vsc85x2;
-
-/// Helper struct to configure SP's RMII pins
-pub struct RmiiPins {
-    pub refclk: sys_api::PinSet,
-    pub crs_dv: sys_api::PinSet,
-    pub tx_en: sys_api::PinSet,
-    pub txd1: sys_api::PinSet,
-    pub txd0: sys_api::PinSet,
-    pub rxd1: sys_api::PinSet,
-    pub rxd0: sys_api::PinSet,
-
-    pub af: sys_api::Alternate,
-}
-impl RmiiPins {
-    pub fn configure(&self, sys: &Sys) {
-        for p in &[
-            self.refclk,
-            self.crs_dv,
-            self.tx_en,
-            self.txd1,
-            self.txd0,
-            self.rxd1,
-            self.rxd0,
-        ] {
-            sys.gpio_configure(
-                p.port,
-                p.pin_mask,
-                Mode::Alternate,
-                OutputType::PushPull,
-                Speed::VeryHigh,
-                Pull::None,
-                self.af,
-            )
-            .unwrap();
-        }
-    }
-}
-
-/// Helper struct to configure MDIO pins
-pub struct MdioPins {
-    pub mdio: sys_api::PinSet,
-    pub mdc: sys_api::PinSet,
-
-    pub af: sys_api::Alternate,
-}
-impl MdioPins {
-    pub fn configure(&self, sys: &Sys) {
-        for p in &[self.mdio, self.mdc] {
-            // Using Speed::Low because otherwise the VSC8504 refuses to talk
-            sys.gpio_configure(
-                p.port,
-                p.pin_mask,
-                Mode::Alternate,
-                OutputType::PushPull,
-                Speed::Low,
-                Pull::None,
-                self.af,
-            )
-            .unwrap();
-        }
-    }
-}
 
 /// On some boards, the KSZ8463 reset line is tied to an RC + diode network
 /// which dramatically slows its rise and fall times.  We use this parameter
