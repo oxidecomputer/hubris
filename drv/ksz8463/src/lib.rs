5525cf04afce7d49fe01a4ef3f68d461d191c53f
Matt Keeter <matt@oxide.computer>
2022-02-23 14:46:16 -0500

    Add all KSZ863 registers

--- drv/ksz8463/src/lib.rs
+++ drv/ksz8463/src/lib.rs
@@ -6,6 +6,9 @@
 use drv_spi_api::{SpiDevice, SpiError};
 use ringbuf::*;
 
+mod registers;
+pub use registers::{MIBCounter, Register};
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum Trace {
     None,
@@ -18,7 +21,7 @@ ringbuf!(Trace, 16, Trace::None);
 /// Data from a management information base (MIB) counter on the chip,
 /// used to monitor port activity for network management.
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub enum MIBCounter {
+pub enum MIBCounterValue {
     Count(u32),
     CountOverflow(u32),
 }
@@ -49,164 +52,6 @@ pub struct MacTableEntry {
 
     /// MAC address from the table
     addr: [u8; 6],
-}
-
-/// Offsets used to access MIB counters
-/// (see Table 4-200 in the datasheet for details)
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum MIBOffset {
-    /// Rx lo-priority (default) octet count, including bad packets.
-    RxLoPriorityByte = 0x0,
-
-    /// Rx hi-priority octet count, including bad packets.
-    RxHiPriorityByte = 0x1,
-
-    /// Rx undersize packets with good CRC.
-    RxUndersizePkt = 0x2,
-
-    /// Rx fragment packets with bad CRC, symbol errors or alignment errors.
-    RxFragments = 0x3,
-
-    /// Rx oversize packets with good CRC (maximum: 2000 bytes).
-    RxOversize = 0x4,
-
-    /// Rx packets longer than 1522 bytes with either CRC errors, alignment errors, or symbol errors (depends on max packet size setting).
-    RxJabbers = 0x5,
-
-    /// Rx packets w/ invalid data symbol and legal packet size.
-    RxSymbolError = 0x6,
-
-    /// Rx packets within (64,1522) bytes w/ an integral number of bytes and a bad CRC (upper limit depends on maximum packet size setting).
-    RxCRCError = 0x7,
-
-    /// Rx packets within (64,1522) bytes w/ a non-integral number of bytes and a bad CRC (upper limit depends on maximum packet size setting).
-    RxAlignmentError = 0x8,
-
-    /// Number of MAC control frames received by a port with 88-08h in Ether- Type field.
-    RxControl8808Pkts = 0x9,
-
-    /// Number of PAUSE frames received by a port. PAUSE frame is qualified with EtherType (88-08h), DA, control opcode (00-01), data length (64B minimum), and a valid CRC.
-    RxPausePkts = 0xA,
-
-    /// Rx good broadcast packets (not including error broadcast packets or valid multicast packets).
-    RxBroadcast = 0xB,
-
-    /// Rx good multicast packets (not including MAC control frames, error multicast packets or valid broadcast packets).
-    RxMulticast = 0xC,
-
-    /// Rx good unicast packets.
-    RxUnicast = 0xD,
-
-    /// Total Rx packets (bad packets included) that were 64 octets in length.
-    Rx64Octets = 0xE,
-
-    /// Total Rx packets (bad packets included) that are between 65 and 127 octets in length.
-    Rx65to127Octets = 0xF,
-
-    /// Total Rx packets (bad packets included) that are between 128 and 255 octets in length.
-    Rx128to255Octets = 0x10,
-
-    /// Total Rx packets (bad packets included) that are between 256 and 511 octets in length.
-    Rx256to511Octets = 0x11,
-
-    /// Total Rx packets (bad packets included) that are between 512 and 1023 octets in length.
-    Rx512to1023Octets = 0x12,
-
-    /// Total Rx packets (bad packets included) that are between 1024 and 2000 octets in length (upper limit depends on max packet size setting).
-    Rx1024to2000Octets = 0x13,
-
-    /// Tx lo-priority good octet count, including PAUSE packets.
-    TxLoPriorityByte = 0x14,
-
-    /// Tx hi-priority good octet count, including PAUSE packets.
-    TxHiPriorityByte = 0x15,
-
-    /// The number of times a collision is detected later than 512 bit-times into the Tx of a packet.
-    TxLateCollision = 0x16,
-
-    /// Number of PAUSE frames transmitted by a port.
-    TxPausePkts = 0x17,
-
-    /// Tx good broadcast packets (not including error broadcast or valid multi- cast packets).
-    TxBroadcastPkts = 0x18,
-
-    /// Tx good multicast packets (not including error multicast packets or valid broadcast packets).
-    TxMulticastPkts = 0x19,
-
-    /// Tx good unicast packets.
-    TxUnicastPkts = 0x1A,
-
-    /// Tx packets by a port for which the 1st Tx attempt is delayed due to the busy medium.
-    TxDeferred = 0x1B,
-
-    /// Tx total collision, half duplex only.
-    TxTotalCollision = 0x1C,
-
-    /// A count of frames for which Tx fails due to excessive collisions.
-    TxExcessiveCollision = 0x1D,
-
-    /// Successfully Tx frames on a port for which Tx is inhibited by exactly one collision.
-    TxSingleCollision = 0x1E,
-
-    /// Successfully Tx frames on a port for which Tx is inhibited by more than one collision.
-    TxMultipleCollision = 0x1F,
-}
-
-#[derive(Copy, Clone, Debug, PartialEq)]
-#[allow(non_camel_case_types)]
-pub enum Register {
-    /// Chip ID and enable register
-    CIDER = 0x0,
-    /// Switch global control register 1
-    SGCR1 = 0x2,
-    /// Switch global control register 2
-    SGCR2 = 0x4,
-    /// Switch global control register 3
-    SGCR3 = 0x6,
-    /// Switch global control register 6
-    SGCR6 = 0xc,
-    /// Switch global control register 7
-    SGCR7 = 0xe,
-    /// MAC address register 1
-    MACAR1 = 0x10,
-    /// MAC address register 2
-    MACAR2 = 0x12,
-    /// MAC address register 3
-    MACAR3 = 0x14,
-
-    /// Indirect access data register 1
-    IADR1 = 0x026,
-    /// Indirect access data register 2
-    IADR2 = 0x028,
-    /// Indirect access data register 3
-    IADR3 = 0x02a,
-    /// Indirect access data register 4
-    IADR4 = 0x02c,
-    /// Indirect access data register 5
-    IADR5 = 0x02e,
-    /// Indirect access control register
-    IACR = 0x030,
-
-    /// PHY 1 and MII basic control register
-    P1MBCR = 0x4c,
-    /// PHY 1 and MII basic status register
-    P1MBSR = 0x4e,
-
-    /// PHY 2 and MII basic control register
-    P2MBCR = 0x58,
-    /// PHY 2 and MII basic status register
-    P2MBSR = 0x5a,
-
-    /// PHY 1 special control and status register
-    P1PHYCTRL = 0x066,
-    /// PHY 2 special control and status register
-    P2PHYCTRL = 0x06a,
-
-    /// Configuration status and serial bus mode register
-    CFGR = 0xd8,
-
-    /// DSP control 1 register
-    DSP_CNTRL_6 = 0x734,
 }
 
 pub struct Ksz8463 {
@@ -285,8 +130,8 @@ impl Ksz8463 {
     pub fn read_mib_counter(
         &self,
         port: u8,
-        offset: MIBOffset,
-    ) -> Result<MIBCounter, SpiError> {
+        offset: MIBCounter,
+    ) -> Result<MIBCounterValue, SpiError> {
         let b = match port {
             1 => 0x0,
             2 => 0x20,
@@ -316,9 +161,9 @@ impl Ksz8463 {
         let value: u32 = value & 0x3fffffff;
 
         if overflow {
-            Ok(MIBCounter::CountOverflow(value))
+            Ok(MIBCounterValue::CountOverflow(value))
         } else {
-            Ok(MIBCounter::Count(value))
+            Ok(MIBCounterValue::Count(value))
         }
     }
 
