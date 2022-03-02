42edf53bc7bba86d0c72537bdd3e11c21726872e
Matt Keeter <matt@oxide.computer>
2022-03-02 11:45:42 -0800

    Remove unused references

--- drv/vsc85xx/src/vsc8552.rs
+++ drv/vsc85xx/src/vsc8552.rs
@@ -13,12 +13,12 @@ pub struct Vsc8552Phy<'a, 'b, P>(pub &'b mut Phy<'a, P>);
 impl<'a, 'b, P> Deref for Vsc8552Phy<'a, 'b, P> {
     type Target = Phy<'a, P>;
     fn deref(&self) -> &Self::Target {
-        &self.0
+        self.0
     }
 }
 impl<'a, 'b, P> DerefMut for Vsc8552Phy<'a, 'b, P> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+        self.0
     }
 }
 
