c98a9c0e7420ca9bcfc2ec629e2777e7a5ee45d1
Matt Keeter <matt@oxide.computer>
2022-03-03 13:43:19 -0800

    More tweaks for VSC8504 bringup

--- drv/vsc85xx/src/vsc8504.rs
+++ drv/vsc85xx/src/vsc8504.rs
@@ -95,7 +95,8 @@ impl<'a, P: PhyRw> Vsc8504Phy<'a, P> {
         self.phy.check_base_port()?;
         crate::tesla::TeslaPhy { phy: &mut self.phy }.patch()?;
 
-        // Configure MAC in QSGMII mode and enable autonegotiation
+        // Configure MAC in QSGMII mode and enable autonegotiation, then force
+        // 100M mode ("Force-speed protocol transfer mode")
         self.phy.broadcast(|v| {
             v.modify(phy::GPIO::MAC_MODE_AND_FAST_LINK(), |r| {
                 r.0 = (r.0 & !(0b11 << 14)) | (0b01 << 14)
@@ -104,8 +105,14 @@ impl<'a, P: PhyRw> Vsc8504Phy<'a, P> {
                 r.set_auto_neg_ena(1);
             })?;
             v.modify(phy::EXTENDED_3::MAC_SERDES_PCS_CONTROL(), |r| {
-                r.0 |= 1 << 7;
+                r.0 |= 1 << 7; // ANEG_ENA
+                r.0 |= 1 << 13; // FORCE_ADV_ABILITY
             })?;
+            // Force 100M mode
+            v.write(
+                phy::EXTENDED_3::MAC_SERDES_CLAUSE_37_ADVERTISED_ABILITY(),
+                0x8401.into(),
+            )?;
             Ok(())
         })?;
 
