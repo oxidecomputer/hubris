oxidecomputer/hubris

commit 78fa6a0cf59e7cec7f59310df2e575b7f2c46d70

Correct LPC55 clock frequency
It turns out that the LPC55 chips are programmed to run at 48MHz
by default, not 96MHz. There was an extra divider register we were
not taking into account. Update the code to handle this and actually
check to make sure the clock settings are what we expect.

Long term we do expect to set the clock frequency in the CMPA area
to ensure this is always correct.

app/gemini-bu-rot/src/main.rs
@@ -207,8 +207,8 @@ fn main() -> ! {
             // FRO_12MHz.  This is true but only before the Boot ROM runs.  Per
             // §10.2.1, the Boot ROM will read the CMPA to determine what speed
             // to run the cores at with options for 48MHz, 96MHz, and
-            // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 96MHz FRO.
-            const CYCLES_PER_MS: u32 = 96_000;
+            // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 48MHz FRO.
+            const CYCLES_PER_MS: u32 = 48_000;
         }
     }
 

app/gimlet-rot/src/main.rs
@@ -207,8 +207,8 @@ fn main() -> ! {
             // FRO_12MHz.  This is true but only before the Boot ROM runs.  Per
             // §10.2.1, the Boot ROM will read the CMPA to determine what speed
             // to run the cores at with options for 48MHz, 96MHz, and
-            // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 96MHz FRO.
-            const CYCLES_PER_MS: u32 = 96_000;
+            // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 48MHz FRO.
+            const CYCLES_PER_MS: u32 = 48_000;
         }
     }
 

app/lpc55xpresso/src/main.rs
@@ -207,8 +207,8 @@ fn main() -> ! {
             // FRO_12MHz.  This is true but only before the Boot ROM runs.  Per
             // §10.2.1, the Boot ROM will read the CMPA to determine what speed
             // to run the cores at with options for 48MHz, 96MHz, and
-            // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 96MHz FRO.
-            const CYCLES_PER_MS: u32 = 96_000;
+            // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 48MHz FRO.
+            const CYCLES_PER_MS: u32 = 48_000;
         }
     }
 

stage0/src/main.rs
@@ -133,6 +133,32 @@ unsafe fn branch_to_image(image: Image) -> ! {
     );
 }
 
+fn check_system_freq() {
+    // corresponds to FRO 96 MHz, see 4.5.34 in user manual
+    const EXPECTED_MAINCLKSELA: u32 = 3;
+    // corresponds to Main Clock A, see 4.5.45 in user manual
+    const EXPECTED_MAINCLKSELB: u32 = 0;
+    // correspinds to divide by 2, see 4.5.50 in user manual
+    const EXPECTED_AHBCLKDIV: u32 = 1;
+
+    let syscon = unsafe { &*lpc55_pac::SYSCON::ptr() };
+
+    let a = syscon.mainclksela.read().bits();
+    let b = syscon.mainclkselb.read().bits();
+    let div = syscon.ahbclkdiv.read().bits();
+
+    // Very very ugly check! We are assuming that the system is running at
+    // 48Mh with these settings. This is a short term verification until we have
+    // all our expected infrastructure to make sure the system is running at
+    // what we expect.
+    if a != EXPECTED_MAINCLKSELA
+        && b != EXPECTED_MAINCLKSELB
+        && div != EXPECTED_AHBCLKDIV
+    {
+        panic!();
+    }
+}
+
 #[entry]
 fn main() -> ! {
     // This is the SYSCON_DIEID register on LPC55 which contains the ROM
@@ -142,6 +168,8 @@ fn main() -> ! {
     if val & 1 != ROM_VER {
         panic!()
     }
+
+    check_system_freq();
 
     let imagea = match image_header::get_image_a() {
         Some(a) => a,

drv/lpc55-romapi/src/lib.rs
@@ -508,7 +508,7 @@ pub unsafe fn flash_erase(addr: u32, len: u32) -> Result<(), FlashStatus> {
     //   XXX More validation of buffer?
     //   We expect the caller to have dropped the clocks appropriately
     let mut f: FlashConfig = Default::default();
-    f.mode_config.sys_freq_in_mhz = 100;
+    f.mode_config.sys_freq_in_mhz = 48;
 
     check_addr_len_alignment(addr, len)?;
 
@@ -548,7 +548,7 @@ pub unsafe fn flash_write(
     //   XXX More validation of buffer?
     //   XXX docs say we need to drop the clocks?
     let mut f: FlashConfig = Default::default();
-    f.mode_config.sys_freq_in_mhz = 100;
+    f.mode_config.sys_freq_in_mhz = 48;
 
     check_addr_len_alignment(addr, len)?;
 
@@ -589,7 +589,7 @@ pub unsafe fn flash_write(
  */
 pub fn validate_programmed(start: u32, len: u32) -> bool {
     let mut f: FlashConfig = Default::default();
-    f.mode_config.sys_freq_in_mhz = 100;
+    f.mode_config.sys_freq_in_mhz = 48;
 
     let ret = handle_flash_status(unsafe {
         (bootloader_tree()
@@ -635,7 +635,7 @@ pub fn get_key_code(
     key_code: &mut [u32; 13],
 ) -> Result<(), FlashStatus> {
     let mut f: FlashConfig = Default::default();
-    f.mode_config.sys_freq_in_mhz = 100;
+    f.mode_config.sys_freq_in_mhz = 48;
 
     handle_flash_status(unsafe {
         (bootloader_tree()
@@ -663,7 +663,7 @@ pub fn get_activation_code(
     ac: &mut [u32; ACTIVATION_CODE_SIZE / 4],
 ) -> Result<(), FlashStatus> {
     let mut f: FlashConfig = Default::default();
-    f.mode_config.sys_freq_in_mhz = 100;
+    f.mode_config.sys_freq_in_mhz = 48;
 
     handle_flash_status(unsafe {
         (bootloader_tree()
@@ -689,7 +689,7 @@ pub fn get_activation_code(
 
 pub fn write_keystore(key_store: &mut FfrKeyStore) -> Result<(), FlashStatus> {
     let mut f: FlashConfig = Default::default();
-    f.mode_config.sys_freq_in_mhz = 100;
+    f.mode_config.sys_freq_in_mhz = 48;
 
     handle_flash_status(unsafe {
         (bootloader_tree()
@@ -721,7 +721,7 @@ pub fn get_cmpa_data(
     assert!(len <= (data.len() as u32));
 
     let mut f: FlashConfig = Default::default();
-    f.mode_config.sys_freq_in_mhz = 100;
+    f.mode_config.sys_freq_in_mhz = 48;
 
     handle_flash_status(unsafe {
         (bootloader_tree()

commit 55b8a6ad221ab9b01d53817b19f2e13895fb7d65

Fix SWO settings
Commit 78fa6a0cf59e7cec7f59310df2e575b7f2c46d70 inadvertently
broke SWO support as SWO is not clocked directly off the CPU.
Fix this by dividing SWO to run at the same CPU speed.

app/gemini-bu-rot/src/main.rs
@@ -36,7 +36,11 @@ fn main() -> ! {
     // FRO_12MHz.  This is true but only before the Boot ROM runs.  Per
     // §10.2.1, the Boot ROM will read the CMPA to determine what speed
     // to run the cores at with options for 48MHz, 96MHz, and
-    // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 48MHz FRO.
+    // NMPA.SYSTEM_SPEED_CODE. With no extra settings the ROM uses
+    // the NMPA setting.
+    //
+    // Importantly, there is an extra divider to determine the CPU
+    // speed which divides the MAINCLKA = 96MHz by 2 to get 48MHz.
     const CYCLES_PER_MS: u32 = 48_000;
 
     unsafe {
@@ -46,6 +50,11 @@ fn main() -> ! {
         //
         let iocon = &*device::IOCON::ptr();
         iocon.pio0_10.modify(|_, w| w.func().alt6());
+
+        // SWO is clocked indepdently of the CPU. Match the CPU
+        // settings by setting the divider
+        let syscon = &*device::SYSCON::ptr();
+        syscon.traceclkdiv.modify(|_, w| w.div().bits(1));
 
         kern::startup::start_kernel(CYCLES_PER_MS)
     }

app/gimlet-rot/src/main.rs
@@ -36,7 +36,11 @@ fn main() -> ! {
     // FRO_12MHz.  This is true but only before the Boot ROM runs.  Per
     // §10.2.1, the Boot ROM will read the CMPA to determine what speed
     // to run the cores at with options for 48MHz, 96MHz, and
-    // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 48MHz FRO.
+    // NMPA.SYSTEM_SPEED_CODE. With no extra settings the ROM uses
+    // the NMPA setting.
+    //
+    // Importantly, there is an extra divider to determine the CPU
+    // speed which divides the MAINCLKA = 96MHz by 2 to get 48MHz.
     const CYCLES_PER_MS: u32 = 48_000;
 
     unsafe {
@@ -46,6 +50,11 @@ fn main() -> ! {
         //
         let iocon = &*device::IOCON::ptr();
         iocon.pio0_10.modify(|_, w| w.func().alt6());
+
+        // SWO is clocked indepdently of the CPU. Match the CPU
+        // settings by setting the divider
+        let syscon = &*device::SYSCON::ptr();
+        syscon.traceclkdiv.modify(|_, w| w.div().bits(1));
 
         kern::startup::start_kernel(CYCLES_PER_MS)
     }

app/lpc55xpresso/src/main.rs
@@ -36,7 +36,11 @@ fn main() -> ! {
     // FRO_12MHz.  This is true but only before the Boot ROM runs.  Per
     // §10.2.1, the Boot ROM will read the CMPA to determine what speed
     // to run the cores at with options for 48MHz, 96MHz, and
-    // NMPA.SYSTEM_SPEED_CODE.  Assume the latter which is 48MHz FRO.
+    // NMPA.SYSTEM_SPEED_CODE. With no extra settings the ROM uses
+    // the NMPA setting.
+    //
+    // Importantly, there is an extra divider to determine the CPU
+    // speed which divides the MAINCLKA = 96MHz by 2 to get 48MHz.
     const CYCLES_PER_MS: u32 = 48_000;
 
     unsafe {
@@ -46,6 +50,11 @@ fn main() -> ! {
         //
         let iocon = &*device::IOCON::ptr();
         iocon.pio0_10.modify(|_, w| w.func().alt6());
+
+        // SWO is clocked indepdently of the CPU. Match the CPU
+        // settings by setting the divider
+        let syscon = &*device::SYSCON::ptr();
+        syscon.traceclkdiv.modify(|_, w| w.div().bits(1));
 
         kern::startup::start_kernel(CYCLES_PER_MS)
     }