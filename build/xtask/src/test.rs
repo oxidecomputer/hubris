d950ff35a3a58f5f8beb23c6ba43f14020c66e70
Kristin Davidson <k@kxd.dev>
2022-05-03 22:43:42 -0500

    Add HUBRIS_HUMILITY_PATH env var config to flash and test xtasks

--- build/xtask/src/test.rs
+++ build/xtask/src/test.rs
@@ -2,6 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::env;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
@@ -17,7 +18,12 @@ pub fn run(verbose: bool, cfg: &Path) -> anyhow::Result<()> {
     archive.push("dist");
     archive.push(format!("build-{}.zip", &toml.name));
 
-    let mut humility = Command::new("humility");
+    let humility_path = match env::var("HUBRIS_HUMILITY_PATH") {
+        Ok(path) => path,
+        _ => "humility".to_string(),
+    };
+
+    let mut humility = Command::new(humility_path);
     humility.arg("-a").arg(archive);
 
     if verbose {
