6a39322b237aabc0a0c07c61863b03deb255f6f9
Matt Keeter <matt@oxide.computer>
2022-05-19 12:51:31 -0400

    Don't warn if a task calls into itself

--- build/xtask/src/dist.rs
+++ build/xtask/src/dist.rs
@@ -684,7 +684,7 @@ fn check_task_priorities(toml: &Config) -> Result<()> {
                 .get(callee)
                 .ok_or_else(|| anyhow!("Invalid task-slot: {}", callee))?
                 .priority;
-            if p >= task.priority {
+            if p >= task.priority && name != callee {
                 // TODO: once all priority inversions are fixed, return an
                 // error so no more can be introduced
                 let mut color = ColorSpec::new();
