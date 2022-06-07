6c620e64cc8c2f1139051fcd7364d3edf4c0ebe2
Cliff L. Biffle <cliff@oxide.computer>
2022-06-07 12:28:20 -0700

    jefe: add config for state notifications.
    There is no actual state to notify about at this time, this just does
    the build plumbing.

--- task/jefe/build.rs
+++ task/jefe/build.rs
@@ -2,6 +2,49 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-fn main() {
+use anyhow::{Context, Result};
+use serde::Deserialize;
+use std::collections::BTreeMap;
+use std::io::Write;
+
+fn main() -> Result<()> {
     build_util::expose_m_profile();
+
+    let cfg = build_util::task_maybe_config::<Config>()?.unwrap_or_default();
+    let out_dir = std::env::var("OUT_DIR")?;
+    let dest_path = std::path::Path::new(&out_dir).join("jefe_config.rs");
+    let mut out =
+        std::fs::File::create(&dest_path).context("creating jefe_config.rs")?;
+
+    let count = cfg.on_state_change.len();
+
+    let task = "hubris_num_tasks::Task";
+    writeln!(
+        out,
+        "const MAILING_LIST: [({}, usize); {}] = [",
+        task, count
+    )?;
+    for (name, rec) in cfg.on_state_change {
+        writeln!(out, "    ({}::{}, 1 << {})", task, name, rec.bit_number)?;
+    }
+    writeln!(out, "];")?;
+
+    Ok(())
+}
+
+/// Jefe task-level configuration.
+#[derive(Deserialize, Default)]
+#[serde(rename_all = "kebab-case")]
+struct Config {
+    /// Task requests to be notified on state change, as a map from task name to
+    /// `StateChange` record.
+    on_state_change: BTreeMap<String, StateChange>,
+}
+
+/// Description of something a task wants done on state change.
+#[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct StateChange {
+    /// Number of notification bit to signal (_not_ mask).
+    bit_number: u8,
 }
