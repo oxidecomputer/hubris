05a2b9611ba543d1ef04a33c89b03c339d52cbdc
Arjen Roodselaar <arjen@oxide.computer>
2022-06-07 14:38:15 -0700

    Use FPGA server to load Sidecar Mainboard bitstream, sequencer improvements (#555)
    This diff does the following:
    
    - Use the FPGA machinary to load the mainboard controller bitstream on boot
    - Add an API for the various functional units in the mainboard controller
    - Use the above to implement Tofino sequencer improvements (slightly more
      control, better visbility when faults occur)
    - Small tweaks in the Idol interface of the sequencer server to pick up recent
      Hubris improvements
    - Implement a rudimentary Sidecar mainboard I2C emulator, allowing the sequencer
      to run on a Gimletlet connected to a ECP5 EVN board

---
+++ drv/sidecar-mainboard-controller-api/src/tofino2.rs
@@ -0,0 +1,144 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use super::{Addr, Reg};
+use drv_fpga_api::{FpgaError, FpgaUserDesign, WriteOp};
+use userlib::FromPrimitive;
+use zerocopy::AsBytes;
+
+#[derive(Copy, Clone, PartialEq, FromPrimitive, AsBytes)]
+#[repr(u8)]
+pub enum TofinoSeqState {
+    Initial = 0,
+    A2 = 1,
+    A0 = 2,
+    InPowerUp = 3,
+    InPowerDown = 4,
+}
+
+#[derive(Copy, Clone, PartialEq, FromPrimitive, AsBytes)]
+#[repr(u8)]
+pub enum TofinoSeqError {
+    None = 0,
+    PowerGoodTimeout = 1,
+    PowerFault = 2,
+    PowerVrHot = 3,
+    PowerInvalidState = 4,
+    UserAbort = 5,
+    VidAckTimeout = 6,
+    ThermalAlert = 7,
+}
+
+/// VID to voltage mapping. The VID values are specified in TF2-DS2, with the
+/// actual voltage values derived experimentally after load testing the PDN.
+#[derive(Copy, Clone, PartialEq, FromPrimitive, AsBytes)]
+#[repr(u8)]
+pub enum Tofino2Vid {
+    V0P922 = 0b1111,
+    V0P893 = 0b1110,
+    V0P867 = 0b1101,
+    V0P847 = 0b1100,
+    V0P831 = 0b1011,
+    V0P815 = 0b1010,
+    V0P790 = 0b1001,
+    V0P759 = 0b1000,
+}
+
+pub struct Sequencer {
+    fpga: FpgaUserDesign,
+}
+
+#[derive(Copy, Clone, PartialEq)]
+pub struct Status {
+    state: TofinoSeqState,
+    error: TofinoSeqError,
+    vid: Option<Tofino2Vid>,
+    power: u32,
+}
+
+impl Sequencer {
+    pub fn new(task_id: userlib::TaskId) -> Self {
+        Self {
+            fpga: FpgaUserDesign::new(task_id),
+        }
+    }
+
+    fn read_masked(&self, addr: Addr, mask: u8) -> Result<u8, FpgaError> {
+        let v: u8 = self.fpga.read(addr)?;
+        Ok(v & mask)
+    }
+
+    fn write_ctrl(&self, op: WriteOp, value: u8) -> Result<(), FpgaError> {
+        self.fpga.write(op, Addr::TOFINO_SEQ_CTRL, value)
+    }
+
+    pub fn clear_error(&self) -> Result<(), FpgaError> {
+        self.write_ctrl(WriteOp::BitSet, Reg::TOFINO_SEQ_CTRL::CLEAR_ERROR)
+    }
+
+    pub fn enabled(&self) -> Result<bool, FpgaError> {
+        self.read_masked(Addr::TOFINO_SEQ_CTRL, Reg::TOFINO_SEQ_CTRL::EN)
+            .map(|v| v != 0)
+    }
+
+    pub fn set_enable(&self, enabled: bool) -> Result<(), FpgaError> {
+        let op = if enabled {
+            WriteOp::BitSet
+        } else {
+            WriteOp::BitClear
+        };
+        self.write_ctrl(op, Reg::TOFINO_SEQ_CTRL::EN)
+    }
+
+    pub fn ack_vid(&self) -> Result<(), FpgaError> {
+        self.write_ctrl(WriteOp::BitSet, Reg::TOFINO_SEQ_CTRL::ACK_VID)
+    }
+
+    pub fn state(&self) -> Result<TofinoSeqState, FpgaError> {
+        let v = self.read_masked(
+            Addr::TOFINO_SEQ_STATE,
+            Reg::TOFINO_SEQ_STATE::STATE,
+        )?;
+        TofinoSeqState::from_u8(v).ok_or(FpgaError::InvalidValue)
+    }
+
+    pub fn error(&self) -> Result<TofinoSeqError, FpgaError> {
+        let v = self.read_masked(
+            Addr::TOFINO_SEQ_ERROR,
+            Reg::TOFINO_SEQ_ERROR::ERROR,
+        )?;
+        TofinoSeqError::from_u8(v).ok_or(FpgaError::InvalidValue)
+    }
+
+    pub fn power_status(&self) -> Result<u32, FpgaError> {
+        self.fpga.read(Addr::TOFINO_POWER_ENABLE)
+    }
+
+    /// The VID is only valid once Tofino is powered up and a delay after PoR
+    /// has lapsed. If the VID is read while in this state a `Some(..)` will be
+    /// returned. Attempting to read the VID outside this window will result in
+    /// `None`. An `FpgaError` is returned if communication with the mainboard
+    /// controller failed or an invalid value was read from the register.
+    pub fn vid(&self) -> Result<Option<Tofino2Vid>, FpgaError> {
+        let v: u8 = self.fpga.read(Addr::TOFINO_POWER_VID)?;
+
+        if (v & Reg::TOFINO_POWER_VID::VID_VALID) != 0 {
+            match Tofino2Vid::from_u8(v & Reg::TOFINO_POWER_VID::VID) {
+                None => Err(FpgaError::InvalidValue),
+                some_vid => Ok(some_vid),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn status(&self) -> Result<Status, FpgaError> {
+        Ok(Status {
+            state: self.state()?,
+            error: self.error()?,
+            vid: self.vid()?,
+            power: self.power_status()?,
+        })
+    }
+}
---
+++ drv/sidecar-mainboard-i2c-emulator/src/main.rs
@@ -0,0 +1,72 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! An I2C device emulator for Sidecar Mainboard, intended to convince the
+//! sequencer task it is running on a Sidecar Mainboard rather than say a
+//! Gimletlet.
+
+#![no_std]
+#![no_main]
+
+use drv_i2c_api::*;
+use ringbuf::*;
+use userlib::*;
+
+#[derive(Copy, Clone, PartialEq)]
+enum Trace {
+    None,
+    Addr(u8),
+}
+ringbuf!(Trace, 16, Trace::None);
+
+#[export_name = "main"]
+fn main() -> ! {
+    let mut buffer = [0; 4];
+
+    loop {
+        hl::recv_without_notification(&mut buffer, |op, msg| match op {
+            Op::WriteRead | Op::WriteReadBlock => {
+                let (payload, caller) = msg
+                    .fixed_with_leases::<[u8; 4], usize>(2)
+                    .ok_or(ResponseCode::BadArg)?;
+
+                let (addr, _, _, _) = Marshal::unmarshal(payload)?;
+
+                if let Some(_) = ReservedAddress::from_u8(addr) {
+                    return Err(ResponseCode::ReservedAddress);
+                }
+
+                ringbuf_entry!(Trace::Addr(addr));
+
+                let wbuf = caller.borrow(0);
+                let winfo = wbuf.info().ok_or(ResponseCode::BadArg)?;
+
+                if !winfo.attributes.contains(LeaseAttributes::READ) {
+                    return Err(ResponseCode::BadArg);
+                }
+
+                let rbuf = caller.borrow(1);
+                let rinfo = rbuf.info().ok_or(ResponseCode::BadArg)?;
+
+                if winfo.len == 0 && rinfo.len == 0 {
+                    // We must have either a write OR a read -- while perhaps
+                    // valid to support both being zero as a way of testing an
+                    // address for a NACK, it's not a mode that we (currently)
+                    // support.
+                    return Err(ResponseCode::BadArg);
+                }
+
+                if winfo.len > 255 || rinfo.len > 255 {
+                    // For now, we don't support writing or reading more than
+                    // 255 bytes.
+                    return Err(ResponseCode::BadArg);
+                }
+
+                // Send an empty reply back to the caller.
+                caller.reply(0);
+                Ok(())
+            }
+        });
+    }
+}
