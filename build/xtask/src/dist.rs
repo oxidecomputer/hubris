0ae7b813f54be1caaad0f0749a696e0d972ce102
Matt Keeter <matt@oxide.computer>
2022-06-20 10:07:17 -0400

    Bump archive version (#613)
    Update to archive `v2` now that it's checked in Humility.

--- build/xtask/src/dist.rs
+++ build/xtask/src/dist.rs
@@ -1939,7 +1939,7 @@ impl Archive {
 
         let archive = File::create(&tmp_path)?;
         let mut inner = zip::ZipWriter::new(archive);
-        inner.set_comment("hubris build archive v1.0.0");
+        inner.set_comment("hubris build archive v2");
         Ok(Self {
             final_path,
             tmp_path,
