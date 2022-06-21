368c362510fd2921abfdd8bbfa4b9dfbd0e74d5a
Matt Keeter <matt@oxide.computer>
2022-06-21 10:35:56 -0400

    Add Sidecar port map (unused for now)

--- drv/vsc7448/src/config.rs
+++ drv/vsc7448/src/config.rs
@@ -38,9 +38,13 @@ pub struct PortConfig {
 /// The VSC7448 has 52 physical ports.  The port mode uniquely determines the
 /// port device type (1G, 2G5, etc) and device number.
 #[derive(Copy, Clone, Debug)]
-pub struct PortMap([PortMode; 52]);
+pub struct PortMap([Option<PortMode>; 53]);
 
 impl PortMap {
+    pub const fn new(p: [Option<PortMode>; 53]) -> Self {
+        Self(p)
+    }
+
     /// Decodes the configuration of the given port.
     ///
     /// # Special cases
@@ -55,72 +59,73 @@ impl PortMap {
     /// # Panics
     /// This will panic if i >= 52, or if the given port can't be configured in
     /// the requested mode.
-    pub fn port_config(&self, p: usize) -> PortConfig {
-        let mode = self.0[p];
-        match mode {
-            PortMode::Sfi => {
-                let dev_num = match p {
-                    49..=52 => p - 49,
-                    _ => panic!("Invalid SFI port {}", p),
-                };
-                PortConfig {
-                    mode,
-                    dev: (PortDev::Dev10g, dev_num),
-                    serdes: (PortSerdes::Serdes10g, dev_num),
+    pub fn port_config(&self, p: usize) -> Option<PortConfig> {
+        self.0[p].map(|mode| {
+            match mode {
+                PortMode::Sfi => {
+                    let dev_num = match p {
+                        49..=52 => p - 49,
+                        _ => panic!("Invalid SFI port {}", p),
+                    };
+                    PortConfig {
+                        mode,
+                        dev: (PortDev::Dev10g, dev_num),
+                        serdes: (PortSerdes::Serdes10g, dev_num),
+                    }
+                }
+                PortMode::Sgmii(_) => {
+                    let dev_type = match p {
+                        0..=7 => PortDev::Dev1g,
+                        8..=31 | 48..=52 => PortDev::Dev2g5,
+                        _ => panic!("Invalid SGMII port {}", p),
+                    };
+                    let dev_num = match p {
+                        0..=7 => p,
+                        8..=31 => p - 8,
+                        48..=52 => 24,
+                        _ => unreachable!(), // checked above
+                    };
+                    // Note that port 48 is a DEV2G5 but uses SERDES1G instead
+                    // of SERDES6G - this is not a mistake!
+                    let serdes_type = match p {
+                        0..=7 | 48 => PortSerdes::Serdes1g,
+                        8..=31 => PortSerdes::Serdes6g,
+                        49..=52 => PortSerdes::Serdes10g,
+                        _ => unreachable!(), // checked above
+                    };
+                    // SERDES1G_1 maps to Port 0, SERDES1G_2 to Port 1, etc
+                    // SERDES6G_0 maps to Port 8, SERDES6G_1 to Port 9, etc
+                    // (notice that there's an offset here; SERDES1G_0 is used
+                    //  by the NPI port, i.e. port 48)
+                    let serdes_num = match p {
+                        0..=7 => p + 1,
+                        8..=31 => p - 8,
+                        48 => 0,
+                        49..=52 => p - 49,
+                        _ => unreachable!(), // checked above
+                    };
+                    PortConfig {
+                        mode,
+                        dev: (dev_type, dev_num),
+                        serdes: (serdes_type, serdes_num),
+                    }
+                }
+                PortMode::Qsgmii(_) => {
+                    let (dev_type, dev_num) = match p {
+                        0..=7 => (PortDev::Dev1g, p),
+                        8..=31 => (PortDev::Dev2g5, p - 8),
+                        32..=47 => (PortDev::Dev1g, p - 24),
+                        _ => panic!("Invalid QSGMII port {}", p),
+                    };
+                    // Ports 0-3 use SERDES6G_4, 4-7 use SERDES6G_5, etc
+                    let serdes_num = (p / 4) + 4;
+                    PortConfig {
+                        mode,
+                        dev: (dev_type, dev_num),
+                        serdes: (PortSerdes::Serdes6g, serdes_num),
+                    }
                 }
             }
-            PortMode::Sgmii(_) => {
-                let dev_type = match p {
-                    0..=7 => PortDev::Dev1g,
-                    8..=31 | 48..=52 => PortDev::Dev2g5,
-                    _ => panic!("Invalid SGMII port {}", p),
-                };
-                let dev_num = match p {
-                    0..=7 => p,
-                    8..=31 => p - 8,
-                    48..=52 => 24,
-                    _ => unreachable!(), // checked above
-                };
-                // Note that port 48 is a DEV2G5 but uses SERDES1G instead of
-                // SERDES6G - this is not a mistake!
-                let serdes_type = match p {
-                    0..=7 | 48 => PortSerdes::Serdes1g,
-                    8..=31 => PortSerdes::Serdes6g,
-                    49..=52 => PortSerdes::Serdes10g,
-                    _ => unreachable!(), // checked above
-                };
-                // SERDES1G_1 maps to Port 0, SERDES1G_2 to Port 1, etc
-                // SERDES6G_0 maps to Port 8, SERDES6G_1 to Port 9, etc
-                // (notice that there's an offset here; SERDES1G_0 is used by
-                //  the NPI port, i.e. port 48)
-                let serdes_num = match p {
-                    0..=7 => p + 1,
-                    8..=31 => p - 8,
-                    48 => 0,
-                    49..=52 => p - 49,
-                    _ => unreachable!(), // checked above
-                };
-                PortConfig {
-                    mode,
-                    dev: (dev_type, dev_num),
-                    serdes: (serdes_type, serdes_num),
-                }
-            }
-            PortMode::Qsgmii(_) => {
-                let (dev_type, dev_num) = match p {
-                    0..=7 => (PortDev::Dev1g, p),
-                    8..=31 => (PortDev::Dev2g5, p - 8),
-                    32..=47 => (PortDev::Dev1g, p - 24),
-                    _ => panic!("Invalid QSGMII port {}", p),
-                };
-                // Ports 0-3 use SERDES6G_4, 4-7 use SERDES6G_5, etc
-                let serdes_num = (p / 4) + 4;
-                PortConfig {
-                    mode,
-                    dev: (dev_type, dev_num),
-                    serdes: (PortSerdes::Serdes6g, serdes_num),
-                }
-            }
-        }
+        })
     }
 }
--- task/vsc7448/src/bsp/sidecar_1.rs
+++ task/vsc7448/src/bsp/sidecar_1.rs
@@ -40,6 +40,74 @@ pub const REFCLK_SEL: vsc7448::RefClockFreq =
     vsc7448::RefClockFreq::Clk156p25MHz;
 pub const REFCLK2_SEL: Option<vsc7448::RefClockFreq> = None;
 
+mod map {
+    // Local module to avoid leaking imports
+    use vsc7448::config::{
+        PortMap,
+        PortMode::{self, *},
+        Speed::*,
+    };
+    const SGMII: Option<PortMode> = Some(Sgmii(Speed100M));
+    const QSGMII: Option<PortMode> = Some(Qsgmii(Speed100M));
+    const SFI: Option<PortMode> = Some(Sfi);
+    pub const PORT_MAP: PortMap = PortMap::new([
+        SGMII,  // 0  | DEV1G_0   | SERDES1G_1  | Cubby 0
+        SGMII,  // 1  | DEV1G_1   | SERDES1G_2  | Cubby 1
+        SGMII,  // 2  | DEV1G_2   | SERDES1G_3  | Cubby 2
+        SGMII,  // 3  | DEV1G_3   | SERDES1G_4  | Cubby 3
+        SGMII,  // 4  | DEV1G_4   | SERDES1G_5  | Cubby 4
+        SGMII,  // 5  | DEV1G_5   | SERDES1G_6  | Cubby 5
+        SGMII,  // 6  | DEV1G_6   | SERDES1G_7  | Cubby 6
+        SGMII,  // 7  | DEV1G_7   | SERDES1G_8  | Cubby 7
+        SGMII,  // 8  | DEV2G5_0  | SERDES6G_0  | Cubby 8
+        SGMII,  // 9  | DEV2G5_1  | SERDES6G_1  | Cubby 9
+        SGMII,  // 10 | DEV2G5_2  | SERDES6G_2  | Cubby 10
+        SGMII,  // 11 | DEV2G5_3  | SERDES6G_3  | Cubby 11
+        SGMII,  // 12 | DEV2G5_4  | SERDES6G_4  | Cubby 12
+        SGMII,  // 13 | DEV2G5_5  | SERDES6G_5  | Cubby 13
+        SGMII,  // 14 | DEV2G5_6  | SERDES6G_6  | Cubby 14
+        SGMII,  // 15 | DEV2G5_7  | SERDES6G_7  | Cubby 15
+        SGMII,  // 16 | DEV2G5_8  | SERDES6G_8  | Cubby 16
+        SGMII,  // 17 | DEV2G5_9  | SERDES6G_9  | Cubby 17
+        SGMII,  // 18 | DEV2G5_10 | SERDES6G_10 | Cubby 18
+        SGMII,  // 19 | DEV2G5_11 | SERDES6G_11 | Cubby 19
+        SGMII,  // 20 | DEV2G5_12 | SERDES6G_12 | Cubby 20
+        SGMII,  // 21 | DEV2G5_13 | SERDES6G_13 | Cubby 21
+        None,   // 22
+        None,   // 23
+        SGMII,  // 24 | DEV2G5_16 | SERDES6G_16 | Cubby 22
+        SGMII,  // 25 | DEV2G5_17 | SERDES6G_17 | Cubby 23
+        SGMII,  // 26 | DEV2G5_18 | SERDES6G_18 | Cubby 24
+        SGMII,  // 27 | DEV2G5_19 | SERDES6G_19 | Cubby 25
+        SGMII,  // 28 | DEV2G5_20 | SERDES6G_20 | Cubby 26
+        SGMII,  // 29 | DEV2G5_21 | SERDES6G_21 | Cubby 27
+        SGMII,  // 30 | DEV2G5_22 | SERDES6G_22 | Cubby 28
+        SGMII,  // 31 | DEV2G5_23 | SERDES6G_23 | Cubby 29
+        None,   // 32
+        None,   // 33
+        None,   // 34
+        None,   // 35
+        None,   // 36
+        None,   // 37
+        None,   // 38
+        None,   // 39
+        QSGMII, // 40 | DEV1G_16  | SERDES6G_14 | Peer SP
+        QSGMII, // 41 | DEV1G_17  | SERDES6G_14 | PSC0
+        QSGMII, // 42 | DEV1G_18  | SERDES6G_14 | PSC1
+        QSGMII, // 43 | Unused
+        QSGMII, // 44 | DEV1G_20  | SERDES6G_15 | Technician 1
+        QSGMII, // 45 | DEV1G_21  | SERDES6G_15 | Technician 2
+        QSGMII, // 46 | Unused
+        QSGMII, // 47 | Unused
+        SGMII,  // 48 | DEV2G5_24 | SERDES1G_0 | Local SP
+        SFI,    // 49 | DEV10G_0  | SERDES10G_0 | Tofino 2
+        None,   // 50 | Unused
+        SGMII,  // 51 | DEV2G5_27 | SERDES10G_2 | Cubby 30 (shadows DEV10G_2)
+        SGMII,  // 52 | DEV2G5_28 | SERDES10G_3 | Cubby 31 (shadows DEV10G_3)
+    ]);
+}
+pub use map::PORT_MAP;
+
 /// For convenience, we implement `PhyRw` on a thin wrapper around the `net`
 /// task handle.  We read and write to PHYs using RPC calls to the `net` task,
 /// which owns the ethernet peripheral containing the MDIO block.
