5f23a6694cf010788fec4fe98c7bcb7f9f580aa3
Bryan Cantrill <bryan@oxide.computer>
2022-06-21 08:04:02 -0700

    Support for Meanwell application (#620)
    This adds support for a Meanwell application definition for Gimletlet,
    which allows for a Gimletlet to be easily used to manage Meanwell
    power supplies on a benchtop. There is nothing particularly
    Meanwell-specific (or indeed even power-specific) here; this is really
    just a thin veneer on the GPIOs present on the J16 header on the
    Gimletlet, allowing them to be used to manage external power supplies
    without forcing the user to wade into the intricacies of GPIO pin
    configuration.  Note that the Meanwell application also indicates a
    different LED pattern (a quick blue LED blink, once per second) to
    make it clear from across a room that a particularly Gimletlet is
    serving to manage Meanwell supplies.

---
+++ drv/meanwell-api/src/lib.rs
@@ -0,0 +1,18 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! Client API for the MeanWell driver.
+
+#![no_std]
+
+use derive_idol_err::IdolError;
+use userlib::*;
+
+#[derive(Copy, Clone, Debug, FromPrimitive, IdolError)]
+pub enum MeanwellError {
+    NotPresent = 1,
+    GpioError = 2,
+}
+
+include!(concat!(env!("OUT_DIR"), "/client_stub.rs"));
---
+++ drv/meanwell/src/main.rs
@@ -0,0 +1,179 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//!
+//! An exceedingly simple driver to deal with the management of Meanwell power
+//! supplies from a Gimletlet on a benchtop.  This driver has nothing
+//! Meanwell-specific (or indeed even power-specific); this is really just a
+//! wrapper around the seven GPIOs present on J16 on the Gimletlet.  We also
+//! use this opportunity to indicate a different LED pattern (a quick blue LED
+//! blink, once per second) to make it clear from across a room that a
+//! particularly Gimletlet is serving to manage Meanwell supplies.
+//!
+#![no_std]
+#![no_main]
+
+use drv_meanwell_api::MeanwellError;
+use idol_runtime::NotificationHandler;
+use idol_runtime::RequestError;
+use userlib::*;
+
+use drv_stm32xx_sys_api as sys_api;
+
+struct ServerImpl {
+    deadline: u64,
+    led_on: bool,
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(any(target_board = "gimletlet-1", target_board = "gimletlet-2"))] {
+        const MEANWELL_PINS: &[sys_api::PinSet] = &[
+            sys_api::Port::B.pin(14),
+            sys_api::Port::B.pin(15),
+            sys_api::Port::D.pin(8),
+            sys_api::Port::D.pin(9),
+            sys_api::Port::D.pin(10),
+            sys_api::Port::D.pin(11),
+            sys_api::Port::D.pin(12),
+        ];
+
+        // In keeping with the Meanwell's blue indicator LED, we use only the
+        // blue LED to indicate that this is a Meanwell management Gimletlet.
+        const LED_INDEX: usize = 3;
+    } else {
+        compile_error!("unsupported Meanwell board");
+    }
+}
+
+fn set(index: usize, val: bool) -> Result<(), RequestError<MeanwellError>> {
+    use drv_stm32xx_sys_api::*;
+
+    let sys = SYS.get_task_id();
+    let sys = Sys::from(sys);
+
+    if index >= MEANWELL_PINS.len() {
+        Err(MeanwellError::NotPresent.into())
+    } else {
+        match sys.gpio_set_to(MEANWELL_PINS[index], val) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(MeanwellError::GpioError.into()),
+        }
+    }
+}
+
+fn get(index: usize) -> Result<bool, RequestError<MeanwellError>> {
+    use drv_stm32xx_sys_api::*;
+
+    let sys = SYS.get_task_id();
+    let sys = Sys::from(sys);
+
+    if index >= MEANWELL_PINS.len() {
+        Err(MeanwellError::NotPresent.into())
+    } else {
+        match sys.gpio_read(MEANWELL_PINS[index]) {
+            Ok(val) => Ok(val != 0),
+            Err(_) => Err(MeanwellError::GpioError.into()),
+        }
+    }
+}
+
+impl idl::InOrderMeanwellImpl for ServerImpl {
+    fn power_on(
+        &mut self,
+        _: &RecvMessage,
+        index: usize,
+    ) -> Result<(), RequestError<MeanwellError>> {
+        set(index, true)
+    }
+
+    fn power_off(
+        &mut self,
+        _: &RecvMessage,
+        index: usize,
+    ) -> Result<(), RequestError<MeanwellError>> {
+        set(index, false)
+    }
+
+    fn is_on(
+        &mut self,
+        _: &RecvMessage,
+        index: usize,
+    ) -> Result<bool, RequestError<MeanwellError>> {
+        get(index)
+    }
+}
+
+task_slot!(USER_LEDS, user_leds);
+task_slot!(SYS, sys);
+
+const TIMER_MASK: u32 = 1 << 0;
+const TIMER_INTERVAL_LONG: u64 = 900;
+const TIMER_INTERVAL_SHORT: u64 = 100;
+
+impl NotificationHandler for ServerImpl {
+    fn current_notification_mask(&self) -> u32 {
+        TIMER_MASK
+    }
+
+    fn handle_notification(&mut self, _bits: u32) {
+        let user_leds =
+            drv_user_leds_api::UserLeds::from(USER_LEDS.get_task_id());
+
+        if !self.led_on {
+            user_leds.led_on(LED_INDEX).unwrap();
+            self.led_on = true;
+            self.deadline += TIMER_INTERVAL_SHORT;
+        } else {
+            user_leds.led_off(LED_INDEX).unwrap();
+            self.led_on = false;
+            self.deadline += TIMER_INTERVAL_LONG;
+        }
+
+        sys_set_timer(Some(self.deadline), TIMER_MASK);
+    }
+}
+
+#[export_name = "main"]
+fn main() -> ! {
+    let deadline = sys_get_timer().now;
+
+    //
+    // This will put our timer in the past, and should immediately kick us.
+    //
+    sys_set_timer(Some(deadline), TIMER_MASK);
+
+    let mut serverimpl = ServerImpl {
+        led_on: false,
+        deadline,
+    };
+
+    use drv_stm32xx_sys_api::*;
+
+    let sys = SYS.get_task_id();
+    let sys = Sys::from(sys);
+
+    //
+    // We can safely do this even if the pins are already configured without
+    // changing their state.
+    //
+    for pin in MEANWELL_PINS {
+        sys.gpio_configure_output(
+            *pin,
+            OutputType::PushPull,
+            Speed::Low,
+            Pull::None,
+        )
+        .unwrap();
+    }
+
+    let mut incoming = [0u8; idl::INCOMING_SIZE];
+    loop {
+        idol_runtime::dispatch_n(&mut incoming, &mut serverimpl);
+    }
+}
+
+mod idl {
+    use super::MeanwellError;
+    include!(concat!(env!("OUT_DIR"), "/server_stub.rs"));
+}
