1e59e7681da2d56b228d03127d92dc38dee33af4
Matt Keeter <matt@oxide.computer>
2022-07-07 10:21:41 -0400

    Add FPGA checksum testing

--- drv/sidecar-front-io/src/controller.rs
+++ drv/sidecar-front-io/src/controller.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::Addr;
+use crate::{Addr, SIDECAR_IO_BITSTREAM_CHECKSUM};
 use drv_fpga_api::*;
 
 static COMPRESSED_BITSTREAM: &[u8] =
@@ -51,5 +51,29 @@ impl FrontIOController {
     pub fn ident_valid(&self) -> Result<(u32, bool), FpgaError> {
         let ident = u32::from_be(self.user_design.read(Addr::ID0)?);
         Ok((ident, ident == Self::EXPECTED_IDENT))
+    }
+
+    /// Loads the checksum scratchpad register and checks whether it matches
+    /// our expected checksum.
+    ///
+    /// This allows us to detect cases where the Hubris image has been updated
+    /// while the FPGA remained powered: if the FPGA bitstream in the new
+    /// Hubris image has changed, the checksum will no longer match.
+    pub fn checksum_valid(&self) -> Result<(u32, bool), FpgaError> {
+        let checksum =
+            u32::from_be(self.user_design.read(Addr::CHECKSUM_SCRATCHPAD0)?);
+        Ok((checksum, checksum == SIDECAR_IO_BITSTREAM_CHECKSUM))
+    }
+
+    /// Writes the checksum scratchpad register to our expected checksum.
+    ///
+    /// In concert with `checksum_valid`, this lets us detect cases where the
+    /// Hubris image has rebooted and the FPGA image should be updated.
+    pub fn write_checksum(&self) -> Result<(), FpgaError> {
+        self.user_design.write(
+            WriteOp::Write,
+            Addr::CHECKSUM_SCRATCHPAD0,
+            SIDECAR_IO_BITSTREAM_CHECKSUM.to_be(),
+        )
     }
 }
