89704931aaff81d990fb0333457ddfd87c29ee5e
Cliff L. Biffle <cliff@oxide.computer>
2022-08-03 15:50:27 -0700

    idle: add insomniac mode
    This avoids using WFI to enter a lower power state.

--- task/idle/src/main.rs
+++ task/idle/src/main.rs
@@ -12,8 +12,20 @@ extern crate userlib;
 #[export_name = "main"]
 fn main() -> ! {
     loop {
-        // Wait For Interrupt to pause the processor until an ISR arrives,
-        // which could wake some higher-priority task.
-        cortex_m::asm::wfi();
+        if cfg!(feature = "insomniac") {
+            // In insomniac-mode, we just spinloop to absorb idle cycles. This
+            // is useful on certain processors where entering a low-power state
+            // interrupts debugging.
+            //
+            // Note that this is an explicit nop rather than an empty block
+            // because an empty `loop {}` is technically UB and will be replaced
+            // by a trap, bringing the system to a halt with no tasks runnable.
+            // So, do not get clever and remove this.
+            cortex_m::asm::nop();
+        } else {
+            // Wait For Interrupt to pause the processor until an ISR arrives,
+            // which could wake some higher-priority task.
+            cortex_m::asm::wfi();
+        }
     }
 }
