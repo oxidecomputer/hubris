cd6d3d5240578ea296f87a7dc6b184560c72ce24
Laura Abbott <laura@oxide.computer>
2022-08-30 17:17:36 -0400

    Add board rev for fixed LPC55 chips
    We (finally) got access to LPC55 chips that match what we expect to use
    in production. Add a carrier board revision for working with those
    chips.

--- build/xtask/src/flash.rs
+++ build/xtask/src/flash.rs
@@ -134,8 +134,10 @@ pub fn config(
     chip_dir: &Path,
 ) -> anyhow::Result<Option<FlashConfig>> {
     match board {
-        "lpcxpresso55s69" | "rot-carrier-1" | "gimlet-rot-1" => {
-            let chip = if board == "lpcxpresso55s69" {
+        "lpcxpresso55s69" | "rot-carrier-1" | "rot-carrier-2"
+        | "gimlet-rot-1" => {
+            let chip = if board == "lpcxpresso55s69" || board == "rot-carrier-2"
+            {
                 "lpc55s69"
             } else {
                 "lpc55s28"
@@ -188,8 +190,8 @@ pub fn config(
 
 pub fn chip_name(board: &str) -> anyhow::Result<&'static str> {
     let b = match board {
-        "lpcxpresso55s69" => "LPC55S69JBD100",
-        "rot-carrier-1" | "gimlet-rot-1" => "LPC55S69JBD100",
+        "lpcxpresso55s69" | "rot-carrier-2" => "LPC55S69JBD100",
+        "rot-carrier-1" | "gimlet-rot-1" => "LPC55S28JBD100",
         "stm32f3-discovery" => "STM32F303VCTx",
         "stm32f4-discovery" => "STM32F407VGTx",
         "nucleo-h743zi2" => "STM32H743ZITx",
