4e9f266d3d833f350e8a8daea608448b44047dd6
Matt Keeter <matt@oxide.computer>
2022-08-31 15:47:41 -0400

    Copy a bunch of code from gimlet-hf-server

--- drv/auxflash-server/src/main.rs
+++ drv/auxflash-server/src/main.rs
@@ -7,17 +7,6 @@
 
 use userlib::*;
 
-/* Sidecar-only for now!
- *
- * SP_QSPI_RESET_L     PF5     GPIO
- * SP_QSPI_CLK         PF10    QUADSPI_CLK
- * SP_QSPI_IO0 (SI)    PF8     QUADSPI_BK1_IO0
- * SP_QSPI_IO1 (SO)    PF9     QUADSPI_BK1_IO1
- * SP_QSPI_CS_L        PG6     QUADSPI_BK1_NCS (or GPIO?)
- * SP_QSPI_IO2 (*WP)   PF7     QUADSPI_BK1_IO2
- * SP_QSPI_IO3 (*HOLD) PF6     QUADSPI_BK1_IO3
- */
-
 /* Gimlet uses SPI instead
  *
  * SP_TO_FLASH_SPI_HOLD_N  PB8     GPIO
@@ -30,10 +19,89 @@ use userlib::*;
  * (This is `local_flash` in `gimlet/rev-b.toml`)
  */
 
+#[cfg(feature = "h753")]
+use stm32h7::stm32h753 as device;
+
+use drv_stm32h7_qspi::Qspi;
+use drv_stm32xx_sys_api as sys_api;
+
+task_slot!(SYS, sys);
+
+const QSPI_IRQ: u32 = 1;
+
 #[export_name = "main"]
 fn main() -> ! {
+    let sys = sys_api::Sys::from(SYS.get_task_id());
+
+    sys.enable_clock(sys_api::Peripheral::QuadSpi);
+    sys.leave_reset(sys_api::Peripheral::QuadSpi);
+
+    let reg = unsafe { &*device::QUADSPI::ptr() };
+    let qspi = Qspi::new(reg, QSPI_IRQ);
+
+    let clock = 5; // 200MHz kernel / 5 = 40MHz clock
+    qspi.configure(clock, 24); // 2**24 = 16MiB = 128Mib
+
+    // Sidecar-only for now!
+    //
+    // This is mostly copied from `gimlet-hf-server`, with a few pin adjustments
+    //
+    // SP_QSPI_RESET_L     PF5     GPIO
+    // SP_QSPI_CLK         PF10    QUADSPI_CLK
+    // SP_QSPI_IO0 (SI)    PF8     QUADSPI_BK1_IO0
+    // SP_QSPI_IO1 (SO)    PF9     QUADSPI_BK1_IO1
+    // SP_QSPI_CS_L        PG6     QUADSPI_BK1_NCS (or GPIO?)
+    // SP_QSPI_IO2 (*WP)   PF7     QUADSPI_BK1_IO2
+    // SP_QSPI_IO3 (*HOLD) PF6     QUADSPI_BK1_IO3
+    sys.gpio_configure_alternate(
+        sys_api::Port::F.pin(6).and_pin(7).and_pin(10),
+        sys_api::OutputType::PushPull,
+        sys_api::Speed::Medium,
+        sys_api::Pull::None,
+        sys_api::Alternate::AF9,
+    )
+    .unwrap();
+    sys.gpio_configure_alternate(
+        sys_api::Port::F.pin(8).and_pin(9),
+        sys_api::OutputType::PushPull,
+        sys_api::Speed::Medium,
+        sys_api::Pull::None,
+        sys_api::Alternate::AF10,
+    )
+    .unwrap();
+    sys.gpio_configure_alternate(
+        sys_api::Port::G.pin(6),
+        sys_api::OutputType::PushPull,
+        sys_api::Speed::Medium,
+        sys_api::Pull::None,
+        sys_api::Alternate::AF10,
+    )
+    .unwrap();
+
+    let qspi_reset = sys_api::Port::F.pin(5);
+    sys.gpio_reset(qspi_reset).unwrap();
+    sys.gpio_configure_output(
+        qspi_reset,
+        sys_api::OutputType::PushPull,
+        sys_api::Speed::Low,
+        sys_api::Pull::None,
+    )
+    .unwrap();
+
+    // TODO: The best clock frequency to use can vary based on the flash
+    // part, the command used, and signal integrity limits of the board.
+
+    // Ensure hold time for reset in case we just restarted.
+    // TODO look up actual hold time requirement
+    hl::sleep_for(1);
+
+    // Release reset and let it stabilize.
+    sys.gpio_set(qspi_reset).unwrap();
+    hl::sleep_for(10);
+
+    // TODO: check the ID and make sure it's what we expect
+
     loop {
-        // NOTE: you need to put code here before running this! Otherwise LLVM
-        // will turn this into a single undefined instruction.
+        hl::sleep_for(1000);
     }
 }
