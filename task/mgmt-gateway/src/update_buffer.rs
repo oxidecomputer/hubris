91b8bc09297d97a66b61c3a93e15e0625a5bae2d
John Gallagher <john@oxidecomputer.com>
2022-09-12 11:42:15 -0400

    mgmt-gateway: add update status and IDs
    This is the companion to the larger MGS change in
    https://github.com/oxidecomputer/omicron/pull/1714. From the SP's side,
    the changes are relatively minor:
    
    1. We removed "update prepare status" in favor of the more general
       "update status"
    2. Update messages now always include an update ID, and for "update
       abort" we only abort if the ID matches our current in-progress
       update.

--- task/mgmt-gateway/src/update_buffer.rs
+++ task/mgmt-gateway/src/update_buffer.rs
@@ -3,7 +3,9 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use crate::Log;
-use gateway_messages::ResponseError;
+use gateway_messages::{
+    ResponseError, UpdateId, UpdateInProgressStatus, UpdateStatus,
+};
 use ringbuf::ringbuf_entry_root;
 use userlib::UnwrapLite;
 
@@ -18,11 +20,6 @@ pub type WriteBlockFn<T> = fn(
 /// have been successfully written.
 pub type FinalizeFn<T> = fn(user_data: &T) -> Result<(), ResponseError>;
 
-pub enum UpdateProgress {
-    Complete,
-    Incomplete,
-}
-
 /// `UpdateBuffer` provides common logic for apply updates over the management
 /// network, assuming a common pattern of:
 ///
@@ -33,9 +30,10 @@ pub enum UpdateProgress {
 ///    short block).
 /// 4. A function to call once all blocks have been written.
 pub struct UpdateBuffer<T, const BLOCK_SIZE: usize> {
-    current_update_stream_id: Option<u64>,
-    total_length: usize,
-    bytes_written: usize,
+    // We never set `status` to `UpdateStatus::Preparing`, so in all matches
+    // below we panic on that arm. If there is a long-running preparation
+    // process, our caller will handle that status report.
+    status: UpdateStatus,
     current_block: &'static mut heapless::Vec<u8, BLOCK_SIZE>,
     write_block_fn: WriteBlockFn<T>,
     finalize_fn: FinalizeFn<T>,
@@ -48,82 +46,100 @@ impl<T, const BLOCK_SIZE: usize> UpdateBuffer<T, BLOCK_SIZE> {
         finalize_fn: FinalizeFn<T>,
     ) -> Self {
         Self {
-            current_update_stream_id: None,
-            total_length: 0,
-            bytes_written: 0,
+            status: UpdateStatus::None,
             current_block: buf,
             write_block_fn,
             finalize_fn,
         }
     }
 
+    pub fn status(&self) -> UpdateStatus {
+        self.status
+    }
+
     pub fn ensure_no_update_in_progress(&self) -> Result<(), ResponseError> {
-        if self.current_update_stream_id.is_some() {
-            Err(ResponseError::UpdateInProgress {
-                bytes_received: self.bytes_written as u32,
-            })
-        } else {
-            Ok(())
+        match self.status {
+            UpdateStatus::Preparing { .. } => panic!(),
+            status @ UpdateStatus::InProgress(_) => {
+                Err(ResponseError::UpdateInProgress(status))
+            }
+            UpdateStatus::None
+            | UpdateStatus::Complete(_)
+            | UpdateStatus::Aborted(_) => Ok(()),
         }
     }
 
-    pub fn ensure_matching_stream_id(
-        &self,
-        stream_id: u64,
-    ) -> Result<(), ResponseError> {
-        match self.current_update_stream_id {
-            None => Err(ResponseError::UpdateNotPrepared),
-            Some(s) => {
-                if s == stream_id {
-                    Ok(())
-                } else {
-                    Err(ResponseError::InvalidUpdateStreamId {
-                        sp_stream_id: s,
-                    })
-                }
-            }
+    pub fn in_progress_update_id(&self) -> Option<&UpdateId> {
+        match &self.status {
+            UpdateStatus::Preparing { .. } => panic!(),
+            UpdateStatus::InProgress(status) => Some(&status.id),
+            UpdateStatus::None
+            | UpdateStatus::Complete(_)
+            | UpdateStatus::Aborted(_) => None,
         }
     }
 
     /// Panics if an update is in progress; use
     /// [`ensure_no_update_in_progress()`] first.
-    pub fn start(&mut self, stream_id: u64, total_length: usize) {
-        if self.current_update_stream_id.is_some() {
+    pub fn start(&mut self, update_id: UpdateId, total_size: u32) {
+        if self.ensure_no_update_in_progress().is_err() {
             panic!();
         }
 
-        self.current_update_stream_id = Some(stream_id);
-        self.total_length = total_length;
-        self.bytes_written = 0;
+        self.status = UpdateStatus::InProgress(UpdateInProgressStatus {
+            id: update_id,
+            bytes_received: 0,
+            total_size,
+        });
         self.current_block.clear();
     }
 
-    pub fn reset(&mut self) {
-        self.current_update_stream_id = None;
-        self.total_length = 0;
-        self.bytes_written = 0;
-        self.current_block.clear();
+    pub fn abort(&mut self) {
+        match self.status {
+            UpdateStatus::Preparing { .. } => panic!(),
+            UpdateStatus::InProgress(status) => {
+                self.status = UpdateStatus::Aborted(status.id);
+            }
+            UpdateStatus::None
+            | UpdateStatus::Complete(_)
+            | UpdateStatus::Aborted(_) => (),
+        }
     }
 
     pub fn ingest_chunk(
         &mut self,
-        stream_id: u64,
+        update_id: &UpdateId,
         user_data: &T,
         offset: u32,
         mut data: &[u8],
-    ) -> Result<UpdateProgress, ResponseError> {
-        // Reject chunks that don't match our current stream.
-        self.ensure_matching_stream_id(stream_id)?;
+    ) -> Result<(), ResponseError> {
+        // Check that we have an update currently in progress.
+        let status = match &mut self.status {
+            UpdateStatus::Preparing { .. } => panic!(),
+            UpdateStatus::InProgress(status) => status,
+            UpdateStatus::None
+            | UpdateStatus::Complete(_)
+            | UpdateStatus::Aborted(_) => {
+                return Err(ResponseError::UpdateNotPrepared);
+            }
+        };
 
-        // Reject chunks that don't match our current progress.
-        if offset as usize != self.bytes_written {
-            return Err(ResponseError::UpdateInProgress {
-                bytes_received: self.bytes_written as u32,
+        // Reject chunks that don't match our current update ID.
+        if &status.id != update_id {
+            return Err(ResponseError::InvalidUpdateId {
+                sp_update_id: status.id,
             });
         }
 
+        // Reject chunks that don't match our current progress.
+        if offset != status.bytes_received {
+            return Err(ResponseError::UpdateInProgress(
+                UpdateStatus::InProgress(*status),
+            ));
+        }
+
         // Reject chunks that would go past the total size we're expecting.
-        if self.bytes_written + data.len() > self.total_length {
+        if status.bytes_received + data.len() as u32 > status.total_size {
             return Err(ResponseError::InvalidUpdateChunk);
         }
 
@@ -132,17 +148,18 @@ impl<T, const BLOCK_SIZE: usize> UpdateBuffer<T, BLOCK_SIZE> {
             assert!(cap > 0);
             let to_copy = usize::min(cap, data.len());
 
-            let current_block_index = self.bytes_written / BLOCK_SIZE;
+            let current_block_index =
+                status.bytes_received as usize / BLOCK_SIZE;
             self.current_block
                 .extend_from_slice(&data[..to_copy])
                 .unwrap_lite();
             data = &data[to_copy..];
-            self.bytes_written += to_copy;
+            status.bytes_received += to_copy as u32;
 
             // If the block is full or this is the final block, send it to the
             // update task.
             if self.current_block.len() == self.current_block.capacity()
-                || self.bytes_written == self.total_length
+                || status.bytes_received == status.total_size
             {
                 let result = (self.write_block_fn)(
                     user_data,
@@ -155,10 +172,10 @@ impl<T, const BLOCK_SIZE: usize> UpdateBuffer<T, BLOCK_SIZE> {
                 let n = self.current_block.len();
                 self.current_block.clear();
 
-                // If writing this block failed, roll back our `bytes_written`
+                // If writing this block failed, roll back our `bytes_received`
                 // counter to the beginning of the block we just tried to write.
                 if let Err(err) = result {
-                    self.bytes_written -= n;
+                    status.bytes_received -= n as u32;
                     return Err(err);
                 }
             }
@@ -167,15 +184,16 @@ impl<T, const BLOCK_SIZE: usize> UpdateBuffer<T, BLOCK_SIZE> {
         // Finalizing the update is implicit (we finalize if we just wrote the
         // last block). Should we make it explict somehow? Maybe that comes with
         // adding auth / code signing?
-        if self.bytes_written == self.total_length {
+        if status.bytes_received == status.total_size {
             (self.finalize_fn)(user_data)?;
+            self.status = UpdateStatus::Complete(*update_id);
             ringbuf_entry_root!(Log::UpdateComplete);
-            Ok(UpdateProgress::Complete)
+            Ok(())
         } else {
             ringbuf_entry_root!(Log::UpdatePartial {
-                bytes_written: self.bytes_written
+                bytes_written: status.bytes_received
             });
-            Ok(UpdateProgress::Incomplete)
+            Ok(())
         }
     }
 }
