235f5d4ecd543d0dc13fddb0e761c483e0c8ef3a
Philip Tricca <flihp@twobit.org>
2022-09-28 17:51:24 -0700

    stage0: Introduce scope changes in the DICE artifact generation to save ram.
    This seems to be the most reliable method for getting the compiler to
    reuse stack space. The end result isn't how I'd choose to structure the
    code but I need this memory back.

--- stage0/src/dice.rs
+++ stage0/src/dice.rs
@@ -3,7 +3,8 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use crate::image_header::Image;
-use core::str::FromStr;
+use core::{convert::TryInto, str::FromStr};
+
 use dice_crate::{
     AliasCertBuilder, AliasData, AliasOkm, Cdi, CdiL1, CertData,
     CertSerialNumber, DeviceIdOkm, DeviceIdSelfCertBuilder, Handoff, RngData,
@@ -15,7 +16,7 @@ use salty::signature::Keypair;
 use sha3::{Digest, Sha3_256};
 use unwrap_lite::UnwrapLite;
 
-fn get_deviceid_keypair(cdi: &Cdi) -> Keypair {
+fn gen_deviceid_keypair(cdi: &Cdi) -> Keypair {
     let devid_okm = DeviceIdOkm::from_cdi(cdi);
 
     Keypair::from(devid_okm.as_bytes())
@@ -25,6 +26,104 @@ fn get_deviceid_keypair(cdi: &Cdi) -> Keypair {
 // https://github.com/oxidecomputer/hubris/issues/734
 fn get_serial_number() -> SerialNumber {
     SerialNumber::from_str("0123456789ab").expect("SerialNumber::from_str")
+}
+
+fn gen_cert_artifacts(
+    cert_serial_number: &mut CertSerialNumber,
+    serial_number: &SerialNumber,
+    deviceid_keypair: &Keypair,
+    handoff: &Handoff,
+) {
+    let deviceid_cert = DeviceIdSelfCertBuilder::new(
+        &cert_serial_number.next(),
+        &serial_number,
+        &deviceid_keypair.public,
+    )
+    .sign(&deviceid_keypair);
+
+    let cert_data = CertData::new(deviceid_cert);
+    handoff.store(&cert_data);
+}
+
+fn gen_alias_artifacts(
+    cdi_l1: &CdiL1,
+    cert_serial_number: &mut CertSerialNumber,
+    serial_number: &SerialNumber,
+    deviceid_keypair: &Keypair,
+    fwid: &[u8; 32],
+    handoff: &Handoff,
+) {
+    let alias_okm = AliasOkm::from_cdi(&cdi_l1);
+    let alias_keypair = Keypair::from(alias_okm.as_bytes());
+
+    let alias_cert = AliasCertBuilder::new(
+        &cert_serial_number.next(),
+        &serial_number,
+        &alias_keypair.public,
+        fwid,
+    )
+    .sign(&deviceid_keypair);
+
+    let tqdhe_okm = TrustQuorumDheOkm::from_cdi(&cdi_l1);
+    let tqdhe_keypair = Keypair::from(tqdhe_okm.as_bytes());
+
+    let tqdhe_cert = TrustQuorumDheCertBuilder::new(
+        &cert_serial_number.next(),
+        &serial_number,
+        &tqdhe_keypair.public,
+        fwid,
+    )
+    .sign(&deviceid_keypair);
+
+    let alias_data =
+        AliasData::new(alias_okm, alias_cert, tqdhe_okm, tqdhe_cert);
+
+    handoff.store(&alias_data);
+}
+
+fn gen_spmeasure_artifacts(
+    cdi_l1: &CdiL1,
+    cert_serial_number: &mut CertSerialNumber,
+    serial_number: &SerialNumber,
+    deviceid_keypair: &Keypair,
+    fwid: &[u8; 32],
+    handoff: &Handoff,
+) {
+    let spmeasure_okm = SpMeasureOkm::from_cdi(&cdi_l1);
+    let spmeasure_keypair = Keypair::from(spmeasure_okm.as_bytes());
+
+    let spmeasure_cert = SpMeasureCertBuilder::new(
+        &cert_serial_number.next(),
+        &serial_number,
+        &spmeasure_keypair.public,
+        fwid,
+    )
+    .sign(&deviceid_keypair);
+
+    let spmeasure_data = SpMeasureData::new(spmeasure_okm, spmeasure_cert);
+
+    handoff.store(&spmeasure_data);
+}
+
+fn gen_rng_artifacts(cdi_l1: &CdiL1, handoff: &Handoff) {
+    let rng_seed = RngSeed::from_cdi(cdi_l1);
+    let rng_data = RngData::new(rng_seed);
+
+    handoff.store(&rng_data);
+}
+
+fn gen_fwid(image: &Image) -> [u8; 32] {
+    // Collect hash(es) of TCB. The first TCB Component Identifier (TCI)
+    // calculated is the Hubris image. The DICE specs call this collection
+    // of TCIs the FWID. This hash is stored in keeys certified by the
+    // DeviceId. This hash should be 'updated' with relevant configuration
+    // and code as FWID for Hubris becomes known.
+    // TODO: This is a particularly naive way to calculate the FWID:
+    // https://github.com/oxidecomputer/hubris/issues/736
+    let mut fwid = Sha3_256::new();
+    fwid.update(image.as_bytes());
+
+    fwid.finalize().try_into().expect("fwid")
 }
 
 pub fn run(image: &Image) {
@@ -41,78 +140,33 @@ pub fn run(image: &Image) {
     };
 
     let dname_sn = get_serial_number();
-    let deviceid_keypair = get_deviceid_keypair(&cdi);
+    let deviceid_keypair = gen_deviceid_keypair(&cdi);
     let mut cert_sn = CertSerialNumber::default();
 
-    let deviceid_cert = DeviceIdSelfCertBuilder::new(
-        &cert_sn.next(),
-        &dname_sn,
-        &deviceid_keypair.public,
-    )
-    .sign(&deviceid_keypair);
+    gen_cert_artifacts(&mut cert_sn, &dname_sn, &deviceid_keypair, &handoff);
 
-    let certs = CertData::new(deviceid_cert);
-    handoff.store(&certs);
-
-    // Collect hash(es) of TCB. The first TCB Component Identifier (TCI)
-    // calculated is the Hubris image. The DICE specs call this collection
-    // of TCIs the FWID. This hash is stored in keeys certified by the
-    // DeviceId. This hash should be 'updated' with relevant configuration
-    // and code as FWID for Hubris becomes known.
-    // TODO: This is a particularly naive way to calculate the FWID:
-    // https://github.com/oxidecomputer/hubris/issues/736
-    let mut fwid = Sha3_256::new();
-    fwid.update(image.as_bytes());
-    let fwid = fwid.finalize();
+    let fwid = gen_fwid(image);
 
     // create CDI for layer 1 (L1) firmware (the hubris image we're booting)
-    let cdi_l1 = CdiL1::new(&cdi, fwid.as_ref());
+    let cdi_l1 = CdiL1::new(&cdi, &fwid);
 
-    // derive alias key
-    let alias_okm = AliasOkm::from_cdi(&cdi_l1);
-    let alias_keypair = Keypair::from(alias_okm.as_bytes());
-
-    let alias_cert = AliasCertBuilder::new(
-        &cert_sn.next(),
+    gen_alias_artifacts(
+        &cdi_l1,
+        &mut cert_sn,
         &dname_sn,
-        &alias_keypair.public,
-        fwid.as_ref(),
-    )
-    .sign(&deviceid_keypair);
+        &deviceid_keypair,
+        &fwid,
+        &handoff,
+    );
 
-    let tqdhe_okm = TrustQuorumDheOkm::from_cdi(&cdi_l1);
-    let tqdhe_keypair = Keypair::from(tqdhe_okm.as_bytes());
-
-    let tqdhe_cert = TrustQuorumDheCertBuilder::new(
-        &cert_sn.next(),
+    gen_spmeasure_artifacts(
+        &cdi_l1,
+        &mut cert_sn,
         &dname_sn,
-        &tqdhe_keypair.public,
-        fwid.as_ref(),
-    )
-    .sign(&deviceid_keypair);
+        &deviceid_keypair,
+        &fwid,
+        &handoff,
+    );
 
-    let alias_data =
-        AliasData::new(alias_okm, alias_cert, tqdhe_okm, tqdhe_cert);
-
-    handoff.store(&alias_data);
-
-    let spmeasure_okm = SpMeasureOkm::from_cdi(&cdi_l1);
-    let spmeasure_keypair = Keypair::from(spmeasure_okm.as_bytes());
-
-    let spmeasure_cert = SpMeasureCertBuilder::new(
-        &cert_sn.next(),
-        &dname_sn,
-        &spmeasure_keypair.public,
-        fwid.as_ref(),
-    )
-    .sign(&deviceid_keypair);
-
-    let spmeasure_data = SpMeasureData::new(spmeasure_okm, spmeasure_cert);
-
-    handoff.store(&spmeasure_data);
-
-    let rng_seed = RngSeed::from_cdi(&cdi_l1);
-    let rng_data = RngData::new(rng_seed);
-
-    handoff.store(&rng_data);
+    gen_rng_artifacts(&cdi_l1, &handoff);
 }
