2ca0c7454819ad7ae618a94f96165e96abce77cb
Matt Keeter <matt@oxide.computer>
2022-10-10 14:55:54 -0400

    Don't rebuild Sidecar images if env changes

--- drv/sidecar-mainboard-controller/build.rs
+++ drv/sidecar-mainboard-controller/build.rs
@@ -24,7 +24,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Pull the bitstream checksum from an environment variable
     // (injected by `xtask` itself as part of auxiliary flash packing)
     println!("cargo:rerun-if-env-changed=HUBRIS_AUXFLASH_CHECKSUM_FPGA");
-    let checksum = env!("HUBRIS_AUXFLASH_CHECKSUM_FPGA");
+    let checksum = std::env::var("HUBRIS_AUXFLASH_CHECKSUM_FPGA").unwrap();
     writeln!(
         &mut file,
         "\npub const SIDECAR_MAINBOARD_BITSTREAM_CHECKSUM: [u8; 32] = {};",
