dc849964bcd115e6697c21719e8ac328717c4d56
John Gallagher <john@oxidecomputer.com>
2022-10-24 12:32:02 -0400

    Cleanup from PR review

--- task/control-plane-agent/src/inventory.rs
+++ task/control-plane-agent/src/inventory.rs
@@ -10,19 +10,10 @@ use task_validate_api::DEVICES as VALIDATE_DEVICES;
 use task_validate_api::{Validate, ValidateError, ValidateOk};
 use userlib::UnwrapLite;
 
-// Most of the devices we report to MGS come from asking the `validate` task,
-// but we have a handful that we describe ourself; they're listed by
-// `OurInventory`, and we (in `Inventory` below) logically glue together
-// `OurInventory`'s devices with `VALIDATE_DEVICES`.
-mod our_inventory;
-
-use our_inventory::OurInventory;
-
 userlib::task_slot!(VALIDATE, validate);
 
 pub(crate) struct Inventory {
     validate_task: Validate,
-    ours: OurInventory,
 }
 
 impl Inventory {
@@ -31,29 +22,32 @@ impl Inventory {
 
         Self {
             validate_task: Validate::from(VALIDATE.get_task_id()),
-            ours: OurInventory::new(),
         }
     }
 
     pub(crate) fn num_devices(&self) -> usize {
-        self.ours.num_devices() + VALIDATE_DEVICES.len()
+        OUR_DEVICES.len() + VALIDATE_DEVICES.len()
     }
 
     pub(crate) fn device_description(
         &self,
         index: usize,
     ) -> DeviceDescription<'static> {
-        // If `index` is in `0..self.ours.num_devices()`, defer to it;
-        // otherwise, subtract `self.ours.num_devices()` to shift it into the
-        // range `0..VALIDATE_DEVICES.len()` and ask `validate`.
-        let index = match index.checked_sub(self.ours.num_devices()) {
-            // Subtraction failure; `index` is in `0..self.ours.num_devices()`.
-            None => return self.ours.device_description(index),
-            // Subtraction success; `index` has been shifted down to be in the
-            // range `0..VALIDATE_DEVICES.len()`.
-            Some(index) => index,
-        };
+        // If `index` is in `0..OUR_DEVICES.len()`, return that device directly;
+        // otherwise, subtract `OUR_DEVICES.len()` to shift it into the range
+        // `0..VALIDATE_DEVICES.len()` and ask `validate`.
+        if index < OUR_DEVICES.len() {
+            OUR_DEVICES[index]
+        } else {
+            let index = index - OUR_DEVICES.len();
+            self.device_description_for_validate_device(index)
+        }
+    }
 
+    fn device_description_for_validate_device(
+        &self,
+        index: usize,
+    ) -> DeviceDescription<'static> {
         let device = &VALIDATE_DEVICES[index];
 
         let presence = match self.validate_task.validate_i2c(index) {
@@ -73,14 +67,17 @@ impl Inventory {
             }
         };
 
+        // This format string is statically guaranteed to fit in `component`
+        // based on our `max_num_devices` submodule below (which only contains
+        // static assertions that ensure this format string will fit!).
         let mut component = FmtComponentId::default();
-        component
-            .write_fmt(format_args!(
-                "{}{}",
-                SpComponent::GENERIC_DEVICE_PREFIX,
-                index
-            ))
-            .unwrap_lite();
+        write!(
+            &mut component,
+            "{}{}",
+            SpComponent::GENERIC_DEVICE_PREFIX,
+            index
+        )
+        .unwrap_lite();
 
         let mut capabilities = DeviceCapabilities::empty();
         if device.num_measurement_channels > 0 {
@@ -114,6 +111,64 @@ impl fmt::Write for FmtComponentId {
         }
     }
 }
+
+// List of logical or high-level components that this task is responsible for
+// (or at least responds to in terms of MGS requests for status / update, even
+// if another task is actually responsible for lower-level details).
+//
+// TODO: Are our device names and descriptions good enough, or are there more
+//       specific names we should use? This may be answered when we expand
+//       DeviceDescription with any VPD / serial numbers.
+const OUR_DEVICES: &[DeviceDescription<'static>] = &[
+    // We always include "ourself" as a component; this is the component name
+    // MGS uses to send SP image updates.
+    DeviceDescription {
+        component: SpComponent::SP_ITSELF,
+        device: SpComponent::SP_ITSELF.const_as_str(),
+        description: "Service Processor",
+        capabilities: DeviceCapabilities::UPDATEABLE,
+        presence: DevicePresence::Present,
+    },
+    // If we have the auxflash feature enabled, report the auxflash as a
+    // component. We do not mark it as explicitly "updateable", even though it
+    // is written as a part of the SP update process. Crucially, that is a part
+    // of updating the `SP_ITSELF` component; the auxflash is not independently
+    // updateable.
+    #[cfg(feature = "auxflash")]
+    DeviceDescription {
+        component: SpComponent::SP_AUX_FLASH,
+        device: SpComponent::SP_AUX_FLASH.const_as_str(),
+        description: "Service Processor auxiliary flash",
+        capabilities: DeviceCapabilities::empty(),
+        presence: DevicePresence::Present,
+    },
+    // If we're building for gimlet, we always claim to have a host CPU.
+    //
+    // This is a lie on gimletlet (where we still build with the "gimlet"
+    // feature), but a useful one in general.
+    #[cfg(feature = "gimlet")]
+    DeviceDescription {
+        component: SpComponent::SP3_HOST_CPU,
+        device: SpComponent::SP3_HOST_CPU.const_as_str(),
+        description: "Gimlet SP3 host cpu",
+        capabilities: DeviceCapabilities::HAS_SERIAL_CONSOLE,
+        presence: DevicePresence::Present, // TODO: ok to assume always present?
+    },
+    // If we're building for gimlet, we always claim to have host boot flash.
+    //
+    // This is a lie on gimletlet (where we still build with the "gimlet"
+    // feature), and a less useful one than the host CPU (since trying to access
+    // the "host flash" will fail unless we have an adapter providing QSPI
+    // flash).
+    #[cfg(feature = "gimlet")]
+    DeviceDescription {
+        component: SpComponent::HOST_CPU_BOOT_FLASH,
+        device: SpComponent::HOST_CPU_BOOT_FLASH.const_as_str(),
+        description: "Gimlet host boot flash",
+        capabilities: DeviceCapabilities::UPDATEABLE,
+        presence: DevicePresence::Present, // TODO: ok to assume always present?
+    },
+];
 
 // We use a generic component ID of `{prefix}{index}` for all of
 // `VALIDATE_DEVICES`; here we statically assert the maximum number of devices
@@ -187,6 +242,7 @@ const fn assert_device_tlv_fits_in_one_packet(
 }
 
 const fn assert_each_device_tlv_fits_in_one_packet() {
+    // Check devices described by `validate`.
     let mut i = 0;
     loop {
         if i == VALIDATE_DEVICES.len() {
@@ -195,6 +251,19 @@ const fn assert_each_device_tlv_fits_in_one_packet() {
         assert_device_tlv_fits_in_one_packet(
             VALIDATE_DEVICES[i].device,
             VALIDATE_DEVICES[i].description,
+        );
+        i += 1;
+    }
+
+    // Check devices described by us.
+    let mut i = 0;
+    loop {
+        if i == OUR_DEVICES.len() {
+            break;
+        }
+        assert_device_tlv_fits_in_one_packet(
+            OUR_DEVICES[i].device,
+            OUR_DEVICES[i].description,
         );
         i += 1;
     }
