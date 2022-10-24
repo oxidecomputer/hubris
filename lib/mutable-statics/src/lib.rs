a8bc5fbf2e78aa8e1a9f6f9f8fe86ec034e3bf2c
Matt Keeter <matt@oxide.computer>
2022-10-24 13:52:53 -0400

    Fix missing word

--- lib/mutable-statics/src/lib.rs
+++ lib/mutable-statics/src/lib.rs
@@ -11,8 +11,9 @@
 /// &mut references to statics, which are declared within the macro.
 ///
 /// The macro accepts definitions of one or more mutable static arrays. It will
-/// arrange for them to be initialized a per-array lambda function, and return a
-/// tuple containing mutable references to each, in the order they're declared.
+/// arrange for them to be initialized by a per-array lambda function, and
+/// return a tuple containing mutable references to each, in the order they're
+/// declared.
 #[macro_export]
 macro_rules! mutable_statics {
     (
