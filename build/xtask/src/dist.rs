oxidecomputer/hubris

commit 0bf9752979fca9ab1b19f07601957343265ab649

Better warning message on kernel overflow (#740)

build/xtask/src/dist.rs
@@ -11,6 +11,7 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 use anyhow::{anyhow, bail, Context, Result};
+use atty::Stream;
 use colored::*;
 use indexmap::IndexMap;
 use path_slash::PathBufExt;
@@ -1053,6 +1054,13 @@ fn build(
     let mut cmd = build_config.cmd("rustc");
     cmd.arg("--release");
 
+    // We're capturing stderr (for diagnosis), so `cargo` won't automatically
+    // turn on color.  If *we* are a TTY, then force it on.
+    if atty::is(Stream::Stderr) {
+        cmd.arg("--color");
+        cmd.arg("always");
+    }
+
     // This works because we control the environment in which we're about
     // to invoke cargo, and never modify CARGO_TARGET in that environment.
     let cargo_out = Path::new("target").to_path_buf();
@@ -1104,9 +1112,31 @@ fn build(
     let prev_time = std::fs::metadata(&src_file).and_then(|f| f.modified());
 
     let status = cmd
-        .status()
+        .output()
         .context(format!("failed to run rustc ({:?})", cmd))?;
-    if !status.success() {
+
+    // Immediately echo `stderr` back out, using a raw write because it may
+    // contain terminal control characters
+    std::io::stderr().write_all(&status.stderr)?;
+
+    if !status.status.success() {
+        // We've got a special case here: if the kernel memory is too small,
+        // then the build will fail with a cryptic linker error.  We can't
+        // convert `status.stderr` to a `String`, because it probably contains
+        // terminal control characters, so do a raw `&[u8]` search instead.
+        if name == "kernel"
+            && memchr::memmem::find(
+                &status.stderr,
+                b"will not fit in region".as_slice(),
+            )
+            .is_some()
+        {
+            bail!(
+                "command failed, see output for details\n    \
+                         The kernel may have run out of space; try increasing \
+                         its allocation in the app's TOML file"
+            )
+        }
         bail!("command failed, see output for details");
     }
 

build/xtask/Cargo.toml
@@ -6,12 +6,14 @@ edition = "2021"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
+atty = "0.2"
 colored = "2.0"
 clap = { version = "3.0.14", features = ["derive"] }
 anyhow = "1.0.32"
 cargo_metadata = "0.12.0"
 ordered-toml = {git = "https://github.com/oxidecomputer/ordered-toml"}
 strsim = "0.10.0"
+memchr = "2.4"
 
 # for dist
 serde = { version = "1.0.114", features = ["derive"] }

commit a4e45f79dde0f9485b0431be248c5d754af58336

Use a worker thread to constantly echo stderr when building (#870)
This fixes an issue introduced in #740 where we didn't see cargo output until a
task _finished_ building, which made it less obvious where the time was being
spent.

build/xtask/src/dist.rs
@@ -5,10 +5,10 @@
 use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque};
 use std::fs::{self, File};
 use std::hash::{Hash, Hasher};
-use std::io::Write;
+use std::io::{Read, Write};
 use std::ops::Range;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 use anyhow::{anyhow, bail, Context, Result};
 use atty::Stream;
@@ -1359,22 +1359,52 @@ fn build(
 
     // File generated by the build system
     let src_file = cargo_out.join(build_config.out_path);
-    let status = cmd
-        .output()
+
+    // We want to (a) store stderr to a buffer to check for a particular error
+    // message, and (b) print data from stderr to the terminal as quickly as we
+    // can.  To enable these dueling priorities, we spawn a task with stderr
+    // going to a pipe, then use a separate thread to constantly read from that
+    // pipe.
+    let mut child = cmd
+        .stderr(Stdio::piped())
+        .spawn()
+        .context("Failed to start child process")?;
+
+    let mut child_stderr =
+        child.stderr.take().context("Failed to take stderr")?;
+    let reader_thread = std::thread::spawn(move || {
+        let mut out_bytes = vec![];
+        let mut stderr = std::io::stderr();
+        loop {
+            let mut buf = [0u8; 256];
+            let num = child_stderr.read(&mut buf).unwrap();
+            if num == 0 {
+                break;
+            }
+
+            // Immediately echo `stderr` back out, using a raw write because it
+            // may contain terminal control characters
+            stderr.write_all(&buf[0..num]).unwrap();
+            stderr.flush().unwrap();
+
+            out_bytes.extend(buf[0..num].into_iter());
+        }
+        out_bytes
+    });
+
+    let status = child
+        .wait()
         .context(format!("failed to run rustc ({:?})", cmd))?;
+    let stderr_bytes = reader_thread.join().unwrap();
 
-    // Immediately echo `stderr` back out, using a raw write because it may
-    // contain terminal control characters
-    std::io::stderr().write_all(&status.stderr)?;
-
-    if !status.status.success() {
+    if !status.success() {
         // We've got a special case here: if the kernel memory is too small,
         // then the build will fail with a cryptic linker error.  We can't
         // convert `status.stderr` to a `String`, because it probably contains
         // terminal control characters, so do a raw `&[u8]` search instead.
         if name == "kernel"
             && memchr::memmem::find(
-                &status.stderr,
+                &stderr_bytes,
                 b"will not fit in region".as_slice(),
             )
             .is_some()