63c7ce39de779f43bc7b58149e1ad56981f7ea6a
Matt Keeter <matt@oxide.computer>
2022-10-28 09:51:43 -0400

    Fix MWOCP name

--- drv/i2c-devices/src/mwocp68.rs
+++ drv/i2c-devices/src/mwocp68.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! MWOCP68-??? Murata power shelf
+//! MWOCP68-3600 Murata power shelf
 
 use crate::{CurrentSensor, Validate, VoltageSensor};
 use core::cell::Cell;
