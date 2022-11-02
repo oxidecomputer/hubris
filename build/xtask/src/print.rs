00d878b8d57e654307a540f50a3e14b802d8d664
Steve Klabnik <steve@steveklabnik.com>
2022-11-02 13:42:51 -0500

    Introduce new command, xtask print
    This command can print out information about the build. Currently, it
    only supports one option, --archive, which prints out the final build
    archive. This is useful in scripting.
    
    Example:
    
    ```
    〉cargo xtask print .\app\demo-stm32f4-discovery\app.toml --archive
        Finished dev [optimized + debuginfo] target(s) in 1.68s
         Running `target\debug\xtask.exe print .\app\demo-stm32f4-discovery\app.toml --archive`
    target\demo-stm32f4-discovery\dist\default\build-demo-stm32f4-discovery.zip
    ```
    
    Additonally, I wanted to make the errors nice for this, and so I ended
    up adding some contexts inside of the other commands, which will also
    make them slightly better. Example:
    
    ```
    〉cargo xtask print .\app\demo-stm32f4-discovery\app.tom --archive
        Finished dev [optimized + debuginfo] target(s) in 1.52s
         Running `target\debug\xtask.exe print .\app\demo-stm32f4-discovery\app.tom --archive`
    Error: could not print information about the build
    
    Caused by:
        0: could not create build configuration
        1: could not read .\app\demo-stm32f4-discovery\app.tom
        2: The system cannot find the file specified. (os error 2)
    ```

---
+++ build/xtask/src/print.rs
@@ -0,0 +1,34 @@
+use std::path::Path;
+
+use anyhow::{bail, Context, Error, Result};
+
+use crate::dist::PackageConfig;
+
+pub fn run(
+    cfg: &Path,
+    archive: bool,
+    image_name: Option<String>,
+) -> Result<()> {
+    if archive {
+        let config = PackageConfig::new(cfg, false, false)
+            .context("could not create build configuration")?;
+
+        let image_name = image_name.unwrap_or(String::from("default"));
+
+        let image_name = config
+            .toml
+            .image_names
+            .iter()
+            .find(|name| name == &&image_name)
+            .ok_or(Error::msg(format!("cannot find image {}", image_name)))?;
+
+        let final_path = config
+            .img_file(format!("build-{}.zip", config.toml.name), image_name);
+
+        println!("{}", final_path.display());
+    } else {
+        bail!("I'm not sure what to print. Currently supported: --archive");
+    }
+
+    Ok(())
+}
