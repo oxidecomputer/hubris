4c629f095d05fbd1ec4511026629351db9e07ab0
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2022-12-13 00:23:33 -0500

    Fix formatting for update script and add block comment

--- drv/update-api/hiffy-rot-flash.sh
+++ drv/update-api/hiffy-rot-flash.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+# Load an image using the humility hiffy interface for the lpc55-update-server.
+#
+# Usage Example:
+# ARCHIVE=hubris/target/rot-carrier/dist/a/build-rot-carrier.zip rot-update-tools/hiffy-rot-flash.sh rot-carrier-v2/b/final.bin ImageB
+#
+# In the above example, `ARCHIVE` is set to the current image running on the
+# RoT. In this example, slot A is currently running, so we upload to slot b via
+# a saved build in `rot-carrier-v2` and additionally give the keyword `ImageB`.
+
 set -x
 
 image=$1
@@ -15,39 +24,38 @@ humility -a $ARCHIVE hiffy -c Update.prep_image_update -a image_type=$image_type
 
 write_blocks() {
   start=$1
-	end=$2
-	block_image=$3
-	
-	echo $start
-	echo $end
+  end=$2
+  block_image=$3
+
+  echo $start
+  echo $end
 
   for ((i=$start;i<=$end;i++))
-	do
-	   humility -a $ARCHIVE hiffy -c Update.write_one_block -a block_num=$i -i <(dd if=$block_image bs=512 count=1 skip=$i)
-		 if [ "$?" -ne 0 ]
-		 then
-		    # Retry once
-				#
-				# The following errors have been observed
-				#
-				# [2022-12-01T17:07:49Z WARN  probe_rs::config::target] Using custom sequence for LPC55S69
-	      # humility: attached to 1fc9:0143:CW4N3IFK22UUX via CMSIS-DAP
-	      # humility hiffy failed: A core architecture specific error occured
-				#
-	      # Caused by:
-	      #     0: Failed to read register DRW at address 0x0000000c
-	      #     1: An error specific to the selected architecture occured
-	      #     2: Target device responded with WAIT response to request.
-	      # Error: humility failed
-		    sleep 1
-	      humility -a $ARCHIVE hiffy -c Update.write_one_block -a block_num=$i -i <(dd if=$block_image bs=512 count=1 skip=$i)
-		 fi
-		 if [ "$?" -ne 0 ]
-		 then
-		     exit 1
-		 fi
-	  
-	done
+  do
+    humility -a $ARCHIVE hiffy -c Update.write_one_block -a block_num=$i -i <(dd if=$block_image bs=512 count=1 skip=$i)
+    if [ "$?" -ne 0 ]
+    then
+      # Retry once
+      #
+      # The following errors have been observed
+      #
+      # [2022-12-01T17:07:49Z WARN  probe_rs::config::target] Using custom sequence for LPC55S69
+      # humility: attached to 1fc9:0143:CW4N3IFK22UUX via CMSIS-DAP
+      # humility hiffy failed: A core architecture specific error occured
+      #
+      # Caused by:
+      #     0: Failed to read register DRW at address 0x0000000c
+      #     1: An error specific to the selected architecture occured
+      #     2: Target device responded with WAIT response to request.
+      # Error: humility failed
+      sleep 1
+        humility -a $ARCHIVE hiffy -c Update.write_one_block -a block_num=$i -i <(dd if=$block_image bs=512 count=1 skip=$i)
+    fi
+    if [ "$?" -ne 0 ]
+    then
+      exit 1
+     fi
+  done
 
 }
 
