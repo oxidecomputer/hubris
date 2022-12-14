93b5a9aa74ae2a76cf3568fb0035d00bc876fe07
Aaron Hartwig <aaron@oxidecomputer.com>
2022-12-14 13:57:54 -0600

    Set output enable for LED controller by default (#1027)

--- drv/sidecar-front-io/src/transceivers.rs
+++ drv/sidecar-front-io/src/transceivers.rs
@@ -636,6 +636,8 @@ impl Transceivers {
                 Addr::LED_CTRL,
                 Reg::LED_CTRL::RESET,
             )?;
+
+            fpga.write(WriteOp::BitSet, Addr::LED_CTRL, Reg::LED_CTRL::OE)?;
         }
 
         Ok(())
