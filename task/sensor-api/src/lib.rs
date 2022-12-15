3540d099a58578d50076ba3470e93943179ee891
Matt Keeter <matt@oxide.computer>
2022-12-15 11:38:43 -0500

    Add timestamps to sensor readings (#1023)
    This PR adds timestamps to readings within the `sensors` task, which must be
    supplied when calling `Sensor.post` and `Sensor.nodata`
    
    In addition, sensor data is reorganized into a struct-of-arrays which stores the
    most recent reading **and** the most recent error, along with a marker
    indicating which (of the two) is most recent.  Using a struct-of-arrays saves a
    significant amount of RAM (which would otherwise be lost to padding between
    `struct` members).
    
    The existing `Sensor.get` API is unchanged to preserve compatibility with
    Humility; the PR adds `Sensor.get_reading` which returns both the value and the
    timestamp.

--- task/sensor-api/src/lib.rs
+++ task/sensor-api/src/lib.rs
@@ -8,29 +8,46 @@
 
 use derive_idol_err::IdolError;
 use drv_i2c_api::ResponseCode;
+use hubpack::SerializedSize;
+use serde::{Deserialize, Serialize};
 use userlib::*;
 
-#[derive(zerocopy::AsBytes, Copy, Clone, Debug, Eq, PartialEq)]
+#[derive(
+    zerocopy::AsBytes,
+    Copy,
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    Serialize,
+    Deserialize,
+    SerializedSize,
+)]
 #[repr(C)]
-pub struct SensorId(pub usize);
+pub struct SensorId(pub u32);
 
-impl From<usize> for SensorId {
-    fn from(id: usize) -> Self {
+impl From<u32> for SensorId {
+    fn from(id: u32) -> Self {
         SensorId(id)
     }
 }
 
-impl From<SensorId> for usize {
+impl From<SensorId> for u32 {
     fn from(id: SensorId) -> Self {
         id.0
     }
 }
 
-#[derive(Copy, Clone, Debug)]
-pub enum Reading {
-    Absent,
-    Value(f32),
-    NoData(NoData),
+#[derive(Copy, Clone, Debug, SerializedSize, Serialize, Deserialize)]
+pub struct Reading {
+    pub timestamp: u64,
+    pub value: f32,
+}
+
+impl Reading {
+    pub fn new(value: f32, timestamp: u64) -> Self {
+        Self { timestamp, value }
+    }
 }
 
 //
