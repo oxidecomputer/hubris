6b70241b7554502d1ec256003446831ae4c91ac5
Matt Keeter <matt@oxide.computer>
2023-01-02 09:57:47 -0500

    Fix incorrect comment

--- task/power-api/src/lib.rs
+++ task/power-api/src/lib.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! API crate for the Gimlet Host Flash server.
+//! API crate for the Gimlet `power` server.
 
 #![no_std]
 
