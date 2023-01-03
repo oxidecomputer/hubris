4f1b707f731bbc03bfea8d4037707e79b4672ad1
Matt Keeter <matt@oxide.computer>
2023-01-03 13:11:44 -0500

    Update task/power-api/src/lib.rs
    Co-authored-by: John Gallagher <john@oxidecomputer.com>

--- task/power-api/src/lib.rs
+++ task/power-api/src/lib.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! API crate for the Gimlet `power` server.
+//! API crate for the `power` server.
 
 #![no_std]
 
