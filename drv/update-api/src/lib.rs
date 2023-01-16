8cd6b06f674653c6266968257ef6cb555212a53e
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2023-01-16 16:53:21 -0500

    Add RoT update support to the control-plane-agent

--- drv/update-api/src/lib.rs
+++ drv/update-api/src/lib.rs
@@ -101,4 +101,18 @@ pub mod stm32h7 {
     pub const BLOCK_SIZE_BYTES: usize = FLASH_WORD_BYTES * 32;
 }
 
+pub mod lpc55 {
+    // This value is currently set to `lpc55_romapi::FLASH_PAGE_SIZE`
+    //
+    // We hardcode it for simplicity, and because we cannot,and should not,
+    // directly include the `lpc55_romapi` crate. While we could transfer
+    // arbitrary amounts of data over spi and have the update server on
+    // the RoT split it up, this makes the code more complicated than
+    // necessary and is only an optimization. For now, especially since we
+    // only have 1 RoT and we must currently define a constant for use the
+    // `control_plane_agent::ComponentUpdater` trait, we do the simple thing
+    // and hardcode according to hardware requirements.
+    pub const BLOCK_SIZE_BYTES: usize = 512;
+}
+
 include!(concat!(env!("OUT_DIR"), "/client_stub.rs"));
