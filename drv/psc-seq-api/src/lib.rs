17a2d8c5dd30bd7321a8015797a3f19631f6350e
John Gallagher <john@oxidecomputer.com>
2023-03-24 11:20:52 -0400

    copy/paste comment fixes

--- drv/psc-seq-api/src/lib.rs
+++ drv/psc-seq-api/src/lib.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! API crate for the Gimlet Sequencer server.
+//! API crate for the PSC Sequencer server.
 
 #![no_std]
 
