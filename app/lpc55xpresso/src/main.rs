7ac789ff60ab3459d0411c0c5885858e0bc0eacb
Cliff L. Biffle <cliff@oxide.computer>
2023-03-31 12:33:44 -0700

    Switch RoT builds to require RFD374 stage0.
    - Removes the stage0/ program and its build configs, including CI
    
    - Moves the DICE logic from stage0 into an `lpc55-rot-startup` library,
      which is in turn called by the RoT main function(s) before starting
      the kernel. This performs the same logic as the previous code,
      including reading a header out of the binary; we can improve this
      later by making more stuff happen at build time.
    
    - Revs the header magic number, ensuring that the original stage0 will
      not attempt to boot one of these images (since it would potentially do
      so in nonsecure-mode, which wouldn't work). This also has the
      potential to allow in-place upgrades, though this is not currently
      being tested.

--- app/lpc55xpresso/src/main.rs
+++ app/lpc55xpresso/src/main.rs
@@ -6,42 +6,17 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-
-// When we're secure we don't have access to read the CMPA/NMPA where the
-// official setting is stored, emulate what the clock driver does instead
-fn get_clock_speed() -> (u32, u8) {
-    // We need to set the clock speed for flash programming to work
-    // properly. Reading it out of syscon is less error prone than
-    // trying to compile it in at build time
-
-    let syscon = unsafe { &*lpc55_pac::SYSCON::ptr() };
-
-    let a = syscon.mainclksela.read().bits();
-    let b = syscon.mainclkselb.read().bits();
-    let div = syscon.ahbclkdiv.read().bits();
-
-    // corresponds to FRO 96 MHz, see 4.5.34 in user manual
-    const EXPECTED_MAINCLKSELA: u32 = 3;
-    // corresponds to Main Clock A, see 4.5.45 in user manual
-    const EXPECTED_MAINCLKSELB: u32 = 0;
-
-    // We expect the 96MHz clock to be used based on the ROM.
-    // If it's not there are probably more (bad) surprises coming
-    // and panicking is reasonable
-    if a != EXPECTED_MAINCLKSELA || b != EXPECTED_MAINCLKSELB {
-        panic!();
-    }
-
-    if div == 0 {
-        (96, div as u8)
-    } else {
-        (48, div as u8)
-    }
-}
+use lpc55_rot_startup::{get_clock_speed, startup};
+use unwrap_lite::UnwrapLite;
 
 #[entry]
 fn main() -> ! {
-    let (cycles_per_ms, _div) = get_clock_speed();
+    let core_peripherals = cortex_m::Peripherals::take().unwrap_lite();
+    let peripherals = lpc55_pac::Peripherals::take().unwrap_lite();
+
+    let (cycles_per_ms, div) = get_clock_speed(&peripherals);
+
+    startup(&core_peripherals, &peripherals);
 
     unsafe { kern::startup::start_kernel(cycles_per_ms * 1_000) }
 }
