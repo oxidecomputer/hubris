3ddc8faaec6fbb28a3e657fff86cea602002025d
John Gallagher <john@oxidecomputer.com>
2023-04-03 10:12:46 -0400

    typo

--- drv/gimlet-seq-api/src/lib.rs
+++ drv/gimlet-seq-api/src/lib.rs
@@ -25,7 +25,7 @@ pub enum SeqError {
 }
 
 // On Gimlet, we have two banks of up to 8 DIMMs apiece. Export the "two banks"
-// bit of knowledge here so it can be used both gimlet-seq-server, spd, and
+// bit of knowledge here so it can be used by gimlet-seq-server, spd, and
 // packrat, all of which want to know at compile-time how many banks there are.
 pub const NUM_SPD_BANKS: usize = 2;
 
