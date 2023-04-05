3da947bf269f9367dd632e9275071edae6bd53bd
Matt Keeter <matt@oxide.computer>
2023-04-05 16:43:25 -0400

    Add UDP server to dump agent

---
+++ task/dump-agent/src/udp.rs
@@ -0,0 +1,126 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+static_assertions::const_assert_eq!(
+    DUMP_READ_SIZE,
+    humpty::udp::DUMP_READ_SIZE
+);
+
+use crate::{ServerImpl, DUMP_READ_SIZE};
+use ringbuf::*;
+use task_net_api::*;
+
+#[derive(Copy, Clone, PartialEq)]
+enum Trace {
+    None,
+    DeserializeError(hubpack::Error),
+    DeserializeHeaderError(hubpack::Error),
+    SendError(SendError),
+    WrongVersion(u8),
+}
+
+ringbuf!(Trace, 16, Trace::None);
+
+impl ServerImpl {
+    pub fn check_net(
+        &mut self,
+        rx_data_buf: &mut [u8],
+        tx_data_buf: &mut [u8],
+    ) {
+        match self.net.recv_packet(
+            SocketName::dump_agent,
+            LargePayloadBehavior::Discard,
+            rx_data_buf,
+        ) {
+            Ok(meta) => self.handle_packet(meta, rx_data_buf, tx_data_buf),
+            Err(RecvError::QueueEmpty | RecvError::ServerRestarted) => {
+                // Our incoming queue is empty or `net` restarted. Wait for more
+                // packets in dispatch_n, back in the main loop.
+            }
+            Err(RecvError::NotYours | RecvError::Other) => panic!(),
+        }
+    }
+
+    fn handle_packet(
+        &mut self,
+        mut meta: UdpMetadata,
+        rx_data_buf: &[u8],
+        tx_data_buf: &mut [u8],
+    ) {
+        let out_len =
+            match hubpack::deserialize(&rx_data_buf[0..meta.size as usize]) {
+                Ok((header, msg)) => {
+                    let r = self.handle_message(header, msg);
+                    let reply = (header, r);
+                    Some(hubpack::serialize(tx_data_buf, &reply).unwrap())
+                }
+                Err(e) => {
+                    // We couldn't deserialize the header; give up
+                    ringbuf_entry!(Trace::DeserializeHeaderError(e));
+                    None
+                }
+            };
+
+        if let Some(out_len) = out_len {
+            meta.size = out_len as u32;
+            if let Err(e) = self.net.send_packet(
+                SocketName::dump_agent,
+                meta,
+                &tx_data_buf[..meta.size as usize],
+            ) {
+                // We'll drop packets if the outgoing queue is full;
+                // the host is responsible for retrying.
+                //
+                // Other errors are unexpected and panic.
+                //
+                // This includes ServerRestarted, because the server should only
+                // restart if the watchdog times out, and the watchdog should
+                // not be timing out, because we're literally replying to a
+                // packet here.
+                ringbuf_entry!(Trace::SendError(e));
+                match e {
+                    SendError::QueueFull => (),
+                    SendError::Other
+                    | SendError::ServerRestarted
+                    | SendError::NotYours
+                    | SendError::InvalidVLan => panic!(),
+                }
+            }
+        }
+    }
+
+    /// Handles a single message
+    fn handle_message(
+        &mut self,
+        header: humpty::udp::Header,
+        data: &[u8],
+    ) -> Result<humpty::udp::Response, humpty::udp::Error> {
+        use humpty::udp::{Request, Response};
+        let r = match hubpack::deserialize::<Request>(data) {
+            Ok((msg, _data)) => match msg {
+                Request::ReadDump { index, offset } => {
+                    self.read_dump(index, offset).map(Response::ReadDump)?
+                }
+                Request::GetDumpArea { index } => {
+                    self.dump_area(index).map(Response::GetDumpArea)?
+                }
+                Request::InitializeDump => {
+                    self.initialize().map(|()| Response::InitializeDump)?
+                }
+                Request::AddDumpSegment { address, length } => self
+                    .add_dump_segment(address, length)
+                    .map(|()| Response::AddDumpSegment)?,
+                Request::TakeDump => {
+                    self.take_dump().map(|()| Response::TakeDump)?
+                }
+            },
+            Err(e) => {
+                ringbuf_entry!(Trace::DeserializeError(e));
+                return Err(humpty::udp::Error::DeserializeError);
+                // TODO: reply something?
+            }
+        };
+        Ok(r)
+    }
+}
