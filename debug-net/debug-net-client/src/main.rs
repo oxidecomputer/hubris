1afd2d0848e4a58f306cbae68bd2d0710fa572e6
Matt Keeter <matt.j.keeter@gmail.com>
2023-07-20 21:03:59 +0000

    fooooo

--- debug-net/debug-net-client/src/main.rs
+++ debug-net/debug-net-client/src/main.rs
@@ -18,31 +18,48 @@ async fn main() {
 
     let sock = UdpSocket::bind("[::]:0").await.unwrap();
     let mut consecutive_failures = 0;
+    let mut pad = 0;
     loop {
-        let okay = run_one(&sock, addr).await;
+        let okay = run_one(&sock, addr, pad + 32).await;
         if okay {
             consecutive_failures = 0;
+            pad = (pad + 1) % 32;
         } else {
             consecutive_failures += 1;
-            if consecutive_failures > 10 {
+            if consecutive_failures > 4 {
                 info!("too many consecutive failures; exiting\x07");
                 break;
             }
         }
     }
 
-    info!("finished in {:?}", Instant::now() - start_time);
+    println!(
+        "finished in {:?} with pad {pad}",
+        Instant::now() - start_time
+    );
 }
 
-async fn run_one(sock: &UdpSocket, addr: SocketAddrV6) -> bool {
-    info!("sending packet to trigger high priority busy loop");
+async fn run_one(sock: &UdpSocket, addr: SocketAddrV6, pad: usize) -> bool {
+    info!("sending packet to trigger high priority busy loop, pad {pad}");
     sock.send_to(b"1", addr).await.unwrap();
+    // Wait for the SP to enter its busy-sleep
+    tokio::time::sleep(Duration::from_millis(50)).await;
 
-    const PACKET_COUNT: usize = 10;
+    const PACKET_COUNT: usize = 6;
+
+    let mut packets = vec![];
     for i in 1..PACKET_COUNT {
+        let mut d = format!("data-{i}").as_bytes().to_owned();
+        for i in 0..pad {
+            d.push(b'0' + (i % 10) as u8);
+        }
+        packets.push(d);
+    }
+
+    for (i, d) in packets.iter().enumerate() {
         info!("sending followup packet {i}");
         loop {
-            match sock.send_to(format!("data-{i}").as_bytes(), addr).await {
+            match sock.send_to(&d, addr).await {
                 Ok(_) => break,
                 Err(err) => {
                     error!("failed to send: {err}");
@@ -50,7 +67,6 @@ async fn run_one(sock: &UdpSocket, addr: SocketAddrV6) -> bool {
                 }
             }
         }
-        tokio::time::sleep(Duration::from_millis(30)).await;
     }
 
     let mut recvs = 0;
@@ -75,9 +91,9 @@ async fn run_one(sock: &UdpSocket, addr: SocketAddrV6) -> bool {
             Err(_) => {
                 let elapsed = start.elapsed();
                 info!("no response after {elapsed:?}");
-                if recvs >= 3 {
+                if recvs > 0 {
                     return true;
-                } else if elapsed > Duration::from_secs(1) {
+                } else if elapsed > Duration::from_secs(2) {
                     return false;
                 }
             }
