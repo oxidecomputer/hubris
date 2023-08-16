cc131b791b41b364c52f9e538dc9bc00b6257506
Philip Tricca <flihp@oxide.computer>
2023-08-15 19:40:17 -0700

    lpc55-rot-startup: Use a better method to generate an FWID.
    The current implementation captures all of the RoT software. It does not
    however include other pages in the RoT image flash range. Expanding our
    definition for the RoT FWID as not only the expected image, but also any
    other programmed flash pages in the range of possible pages mitigates a
    potential persistence mechanism for an attacker.
    
    This commit:
    - adds a new environment variable HUBRIS_FLASH_OUTPUT that holds all
      config::Output structures for the flash regions assigned to the image
      being built
    - generates a Range for each flash region in HUBRIS_FLASH_OUTPUTS for
      use by the lpc55-rot-startup crate
    - uses the A & B Ranges to walk through all possible flash pages for the
      given image including each page that's been programmed in the FWID

--- lib/lpc55-rot-startup/build.rs
+++ lib/lpc55-rot-startup/build.rs
@@ -2,33 +2,37 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Write;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out = build_util::out_dir();
+    gen_image_flash_range(&out)?;
+
     #[cfg(feature = "dice-mfg")]
     {
-        let out = build_util::out_dir();
         gen_memory_range(&out)?;
     }
 
     Ok(())
 }
 
+#[derive(Deserialize, Debug)]
+struct Region {
+    pub name: String,
+    pub address: u32,
+    pub size: u32,
+}
+
 #[cfg(feature = "dice-mfg")]
 fn gen_memory_range(
     out_dir: &std::path::PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    use serde::Deserialize;
-    use std::fs::File;
-    use std::io::Write;
-
-    #[derive(Deserialize, Debug)]
-    struct DiceMfgRegion {
-        pub address: u32,
-        pub size: u32,
-    }
-
     let toml = build_util::env_var("HUBRIS_DICE_MFG")?;
-    let region: DiceMfgRegion = toml::from_str(&toml)?;
+    let region: Region = toml::from_str(&toml)?;
     let mut dice_mfg = File::create(out_dir.join("dice-mfg.rs")).unwrap();
+
     writeln!(
         dice_mfg,
         "use core::ops::Range;\n\n\
@@ -36,6 +40,28 @@ fn gen_memory_range(
         region.address,
         region.address + region.size
     )?;
+
+    Ok(())
+}
+
+fn gen_image_flash_range(
+    out_dir: &std::path::PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let outputs = build_util::env_var("HUBRIS_FLASH_OUTPUTS")?;
+    let outputs: Vec<Region> = ron::de::from_str(&outputs)?;
+
+    let mut cfg = File::create(out_dir.join("config.rs")).unwrap();
+
+    writeln!(cfg, "use core::ops::Range;\n")?;
+    for region in outputs {
+        writeln!(
+            cfg,
+            "#[allow(dead_code)]\npub const FLASH_{}: Range<u32> = {:#x}..{:#x};",
+            region.name.replace("-", "_").to_uppercase(),
+            region.address,
+            region.address + region.size
+        )?;
+    }
 
     Ok(())
 }
