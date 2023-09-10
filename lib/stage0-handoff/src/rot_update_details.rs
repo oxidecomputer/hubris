aa8712503044f6d79756d9b6743d898a1a775db9
Ben Stoltz <ben.stoltz@oxidecomputer.com>
2023-09-10 12:38:24 -0700

    added stage0 and stage0next flash slots to RotBootState

--- lib/stage0-handoff/src/rot_update_details.rs
+++ lib/stage0-handoff/src/rot_update_details.rs
@@ -13,6 +13,40 @@ unsafe impl HandoffData for RotBootState {
     const MEM_RANGE: Range<usize> = UPDATE_RANGE;
 }
 
+#[derive(
+    Debug, Copy, Clone, PartialEq, Eq, Deserialize, Serialize, SerializedSize,
+)]
+pub enum ImageError {
+    /// Image has not been sanity checked (internal use)
+    Unchecked = 1,
+    /// First page of image is erased.
+    FirstPageErased,
+    /// Some pages in the image are erased.
+    PartiallyProgrammed,
+    /// The NXP image offset + length caused a wrapping add.
+    InvalidLength,
+    /// The header flash page is erased.
+    HeaderNotProgrammed,
+    /// An image not requiring an ImageHeader is too short.
+    Short,
+    /// A required ImageHeader is missing.
+    BadMagic,
+    /// The image size in ImageHeader is unreasonable.
+    HeaderImageSize,
+    /// total_image_length in ImageHeader is not properly aligned.
+    UnalignedLength,
+    /// Some NXP image types are not supported.
+    UnsupportedType,
+    /// Wrong format reset vector.
+    ResetVectorNotThumb2,
+    /// Reset vector points outside of image execution range.
+    ResetVector,
+    /// A CRC validated image has an invalid CRC.
+    Crc,
+    /// Signature check on image failed.
+    Signature,
+}
+
 /// Top-level type describing images loaded into flash on the RoT.
 ///
 /// This data is injected into RAM at `UPDATE_RANGE` by stage0.
@@ -23,12 +57,14 @@ unsafe impl HandoffData for RotBootState {
 )]
 pub struct RotBootState {
     pub active: RotSlot,
-    pub a: Option<RotImageDetails>,
-    pub b: Option<RotImageDetails>,
+    pub a: RotImageDetails,
+    pub b: RotImageDetails,
+    pub stage0: RotImageDetails,
+    pub stage0next: RotImageDetails,
 }
 
 impl RotBootState {
-    pub fn active_image(&self) -> Option<RotImageDetails> {
+    pub fn active_image(&self) -> RotImageDetails {
         match self.active {
             RotSlot::A => self.a.clone(),
             RotSlot::B => self.b.clone(),
@@ -42,8 +78,11 @@ fits_in_ram!(RotBootState);
     Debug, Clone, PartialEq, Eq, Deserialize, Serialize, SerializedSize,
 )]
 pub struct RotImageDetails {
+    // The SHA3-256 measurement of all programmed pages in the flash slot.
     pub digest: [u8; 32],
     pub version: ImageVersion,
+    // Image sanity check and signature validation.
+    pub status: Result<(), ImageError>,
 }
 
 #[derive(
