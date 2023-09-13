1d1aae51183f681644ec1bb0312f4d636c05fc3f
Ben Stoltz <ben.stoltz@oxidecomputer.com>
2023-09-13 12:23:57 -0700

    Remove support for CRC validated images

--- lib/lpc55-rot-startup/src/images.rs
+++ lib/lpc55-rot-startup/src/images.rs
@@ -147,16 +147,7 @@ impl Image {
             false
         }
     }
-}
 
-pub fn image_details(img: Image) -> RotImageDetails {
-    RotImageDetails {
-        digest: img.fwid,
-        version: img.get_image_version(),
-    }
-}
-
-impl Image {
     fn get_img_start(&self) -> u32 {
         self.flash.start
     }
@@ -199,71 +190,242 @@ impl Image {
         (self.get_img_start() + vector_size) as *const ImageHeader
     }
 
-    /// Make sure all of the image flash is programmed
-    fn validate(&self) -> bool {
-        let img_start = self.get_img_start();
+    fn is_imageheader_present(
+        &self,
+        header_required: bool,
+    ) -> Result<bool, ImageError> {
+        // Check Hubris header.
+        // Note that bootloaders without Hubris headers have been released.
+        let header_ptr = self.get_header_ptr();
 
-        // Start by making sure we can access the page where the vectors live
-        if !self.is_span_programmed(self.flash.start, PAGE_SIZE) {
-            return false;
-        }
-
-        let header_ptr = self.get_header();
-
-        // Next validate the header location is programmed
-        if !self.is_span_programmed(header_ptr as u32, PAGE_SIZE) {
-            return false;
+        // Even headerless bootloaders are long enough that
+        // it is an error if this area is not programmed.
+        if !self.is_span_programmed(
+            header_ptr as u32,
+            core::mem::size_of::<ImageHeader>() as u32,
+        ) {
+            if header_required {
+                return Err(ImageError::HeaderNotProgrammed);
+            } else {
+                return Err(ImageError::Short);
+            }
         }
 
         // SAFETY: We've validated the header location is programmed so this
-        // will not trigger a fault. This is generated from our build scripts
-        // which we trust.
+        // will not trigger a fault.
+        // The values used are all link-time constants.
         let header = unsafe { &*header_ptr };
-
-        // Does this look like a header?
         if header.magic != abi::HEADER_MAGIC {
-            return false;
+            if header_required {
+                return Err(ImageError::BadMagic);
+            }
+            Ok(false)
+        } else {
+            Ok(true)
+        }
+    }
+
+    fn get_imageheader_total_image_len(&self) -> u32 {
+        let header_ptr = self.get_header_ptr();
+        // SAFETY: The caller must have established that a header is present.
+        let header = unsafe { &*header_ptr };
+        header.total_image_len
+    }
+
+    /// Test an image for viability.
+    fn validate(&self, header_required: bool) -> Result<(), ImageError> {
+        // Start by making sure we can access the page where the vectors live
+        if !self.is_span_programmed(self.flash.start, PAGE_SIZE) {
+            return Err(ImageError::FirstPageErased);
         }
 
-        let total_len = match header.total_image_len.checked_add(PAGE_SIZE - 1)
+        // Check that the entire image from flash.start to the end of
+        // the signature block has been programmed.
+        let image_length = self.get_nxp_image_length();
+        let end = if let Some(end) = self.flash.start.checked_add(image_length)
         {
-            Some(s) => s & !(PAGE_SIZE - 1),
-            None => return false,
+            // The programmed image is rounded to a page boundary with padding
+            // of 0xff.
+            // Later checks ensure that interesting offsets fall within
+            // the image proper.
+            // Check rounded limit here and save image limit.
+            match end
+                .checked_add(PAGE_SIZE - 1)
+                .map(|sum| sum & !(PAGE_SIZE - 1))
+            {
+                Some(rounded_end) => {
+                    if !self.programmed.contains(&rounded_end) {
+                        return Err(ImageError::PartiallyProgrammed);
+                    }
+                } // TODO: Check that padding is 0xff.
+                None => return Err(ImageError::InvalidLength),
+            }
+            end
+        } else {
+            // A offset was found that lies outside the flash slot.
+            return Err(ImageError::InvalidLength);
         };
 
-        // Next make sure the marked image length is programmed
-        if !self.is_span_programmed(img_start, total_len) {
-            return false;
+        let image_span = Range::<u32> {
+            start: self.flash.start,
+            end,
+        };
+
+        // The signature validation routine could be called now.
+        // Any additional checks should be moot based on the signing
+        // procedure only signing "good" images.
+        //
+        // However, the price of flashing a bad bootloader is high and
+        // the criteria for signing images evolves over time. So, as
+        // long as we can afford the space and time, perform extra checks
+        // to aid diagnosis of bad images and to protect the system.
+        //
+        // There is also the concern that the ROM signature check routine
+        // might not fully protect itself from bad input.
+        //
+        // Consider deleting any of the following tests once there
+        // is high confidence that non-conforming signed images are
+        // no longer a threat.
+
+        // Bootloaders without Hubris headers have been released.
+        // So, check ImageHeader carefully.
+        if self.is_imageheader_present(header_required)? {
+            let len = self.get_imageheader_total_image_len();
+            if (len % U32_SIZE) != 0 {
+                return Err(ImageError::UnalignedLength);
+            }
+            match self.flash.start.checked_add(len) {
+                Some(end) => {
+                    if !image_span.contains(&end) {
+                        return Err(ImageError::HeaderImageSize);
+                    }
+                }
+                None => return Err(ImageError::HeaderImageSize),
+            };
         }
 
-        return true;
+        // Because of our past experience with the implementation quality of the
+        // ROM, let's do some basic checks before handing it a blob to inspect,
+        // shall we?
+
+        const MASK_WITHOUT_28: u32 = !(1 << 28);
+        let reset_vector = MASK_WITHOUT_28 & self.get_reset_vector();
+
+        // Verify that the reset vector is a valid Thumb-2 function pointer.
+        if reset_vector & 1 == 0 {
+            // This'll cause an immediate usage fault. Reject it.
+            return Err(ImageError::ResetVectorNotThumb2);
+        }
+
+        // Ensure that the reset vector is within the runtime address range.
+        if !(self.get_executable_image_range()?.contains(&reset_vector)) {
+            return Err(ImageError::ResetVector);
+        }
+
+        self.check_signature()
     }
 
-    pub fn get_image_version(&self) -> ImageVersion {
-        // SAFETY: We checked this previously
-        let header = unsafe { &*self.get_header() };
+    // Any vector address must fall within the executable
+    // code portion of an image. At this point, we don't know
+    // what is code vs data, but we can exclude the vector
+    // table, the ImageHeader, the Caboose, and the signature block.
+    // Note that this range is different from the flash range for
+    // `stage0next`.
+    fn get_executable_image_range(&self) -> Result<Range<u32>, ImageError> {
+        let end_offset = match self.get_image_type() {
+            2 | 5 => self.get_nxp_image_length(),
+            1 | 4 | 0x8001 => self.get_type_specific_header(),
+            _ => return Err(ImageError::UnsupportedType),
+        };
 
+        // TODO: Find the caboose if present and reduce end_offset.
+
+        // SAFETY: Link time constants known to be safe.
+        let mut begin_offset =
+            unsafe { core::ptr::addr_of!(__vector_size) as u32 };
+        if self.is_imageheader_present(false).is_ok() {
+            // SAFETY: Link time constants known to be safe.
+            begin_offset += core::mem::size_of::<ImageHeader>() as u32;
+        }
+        if let Some(end) = self.run.end.checked_add(end_offset) {
+            // SAFETY: Link time constants known to be safe.
+            Ok(self.run.start + begin_offset..end)
+        } else {
+            Err(ImageError::InvalidLength)
+        }
+    }
+
+    // Assuming a well-formed image, get the result of the ROM signature check
+    // routine.
+    fn check_signature(&self) -> Result<(), ImageError> {
+        // The following code is adapted to fit here from bootleby.
+
+        // The image type is at offset 0x24. We only allow type 4.
+        //   - 0x0000 Normal image for unsecure boot
+        //   - 0x0001 Plain signed Image
+        //   - 0x0002 Plain CRC Image, CRC at offset 0x28
+        //   - 0x0004 Plain signed XIP Image
+        //   - 0x0005 Plain CRC XIP Image, CRC at offset 0x28
+        //   - 0x8001 Signed plain Image with KeyStore Included
+        if self.get_image_type() != 4 {
+            return Err(ImageError::UnsupportedType);
+        }
+
+        let syscon = unsafe { &*lpc55_pac::SYSCON::ptr() };
+        // Time to check the signatures!
+
+        const HASHAES: u32 = 32 + 32 + 18;
+        const PMASK: u32 = 1 << (HASHAES % 32);
+        const REG_NUM: u32 = HASHAES / 32; // XXX must be 0, 1, or 2
+        match REG_NUM {
+            0 => clear_bit!(syscon.presetctrl0, PMASK),
+            1 => clear_bit!(syscon.presetctrl1, PMASK),
+            2 => clear_bit!(syscon.presetctrl2, PMASK),
+            _ => panic!(),
+        }
+
+        let authorized = unsafe {
+            lpc55_romapi::authenticate_image(self.flash.start).is_ok()
+        };
+        // let authorized = true;
+        // enter reset
+        match REG_NUM {
+            0 => set_bit!(syscon.presetctrl0, PMASK),
+            1 => set_bit!(syscon.presetctrl1, PMASK),
+            2 => set_bit!(syscon.presetctrl2, PMASK),
+            _ => panic!(),
+        }
+
+        if authorized {
+            Ok(())
+        } else {
+            Err(ImageError::Signature)
+        }
+    }
+
+    // Get the epoch and version from a flash slot or (0,0) if there
+    // is no valid image.
+    fn get_image_version(&self) -> ImageVersion {
+        let header_ptr = self.get_header_ptr();
+        if self.is_span_programmed(
+            header_ptr as u32,
+            core::mem::size_of::<ImageHeader>() as u32,
+        ) {
+            // SAFETY: header page is available.
+            let header = unsafe { &*header_ptr };
+
+            if header.magic == abi::HEADER_MAGIC {
+                return ImageVersion {
+                    epoch: header.epoch,
+                    version: header.version,
+                };
+            }
+        }
+        // Default for erased slots and images without ImageHeaders is 0,0
         ImageVersion {
-            epoch: header.epoch,
-            version: header.version,
+            epoch: 0,
+            version: 0,
         }
-    }
-
-    fn pointer_range(&self) -> core::ops::Range<*const u8> {
-        let img_ptr = self.get_img_start() as *const u8;
-        // The MPU requires 32 byte alignment and so the compiler pads the
-        // image accordingly. The length field from the image header does not
-        // (and should not) account for this padding so we must do that here.
-        let img_size = self.get_img_size().unwrap_lite() + 31 & !31;
-
-        // Safety: this is unsafe because the pointer addition could overflow.
-        // If that happens, we'll produce an empty range or crash with a panic.
-        // We do not dereference these here pointers.
-        img_ptr..unsafe { img_ptr.add(img_size) }
-    }
-
-    pub fn contains(&self, address: *const u8) -> bool {
-        self.pointer_range().contains(&address)
     }
 }
 
