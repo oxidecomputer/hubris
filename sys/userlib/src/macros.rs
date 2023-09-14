8462ca534e8664573759dc9601b1b3133ef4bd00
Laura Abbott <laura@oxide.computer>
2023-09-14 10:53:17 -0400

    Completely remove ITM and semihosting
    We no longer have any need for these features and they are mostly
    just sitting around as some copy/paste. Remove them.

--- sys/userlib/src/macros.rs
+++ sys/userlib/src/macros.rs
@@ -5,68 +5,6 @@
 pub use bstringify;
 pub use paste;
 
-cfg_if::cfg_if! {
-    if #[cfg(feature = "log-itm")] {
-        #[macro_export]
-        macro_rules! sys_log {
-            ($s:expr) => {
-                unsafe {
-                    let stim = &mut (*cortex_m::peripheral::ITM::PTR).stim[1];
-                    cortex_m::iprintln!(stim, $s);
-                }
-            };
-            ($s:expr, $($tt:tt)*) => {
-                unsafe {
-                    let stim = &mut (*cortex_m::peripheral::ITM::PTR).stim[1];
-                    cortex_m::iprintln!(stim, $s, $($tt)*);
-                }
-            };
-        }
-    } else if #[cfg(feature = "log-semihosting")] {
-        #[macro_export]
-        macro_rules! sys_log {
-            ($s:expr) => {
-                { let _ = cortex_m_semihosting::hprintln!($s); }
-            };
-            ($s:expr, $($tt:tt)*) => {
-                { let _ = cortex_m_semihosting::hprintln!($s, $($tt)*); }
-            };
-        }
-    } else if #[cfg(feature = "log-null")] {
-        #[macro_export]
-        macro_rules! sys_log {
-            ($s:expr) => {};
-            ($s:expr, $($x:expr),*$(,)?) => {
-                {
-                    $(
-                        let _ = &$x;
-                    )*
-                }
-            };
-        }
-    } else {
-        // Note: we provide macros that contain compile_error, instead of just
-        // using compile_error here, to allow programs to omit these features
-        // if they don't use logging.
-
-        #[macro_export]
-        macro_rules! sys_log {
-            ($s:expr) => {
-                compile_error!(concat!(
-                        "to use sys_log! must enable either ",
-                        "'log-semihosting' or 'log-itm' feature"
-                ))
-            };
-            ($s:expr, $($tt:tt)*) => {
-                compile_error!(concat!(
-                        "to use sys_log! must enable either ",
-                        "'log-semihosting' or 'log-itm' feature"
-                ))
-            };
-        }
-    }
-}
-
 #[macro_export]
 macro_rules! task_slot {
     ($vis:vis $var:ident, $task_name:ident) => {
