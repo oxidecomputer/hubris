89cc3fb962e6ffb3a1112797260604e57f54ce31
Eliza Weisman <eliza@elizas.website>
2024-03-07 14:12:13 -0800

    remove server-side counters from gimlet_seq

--- drv/gimlet-seq-server/build.rs
+++ drv/gimlet-seq-server/build.rs
@@ -67,13 +67,11 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         u32::from_le_bytes(result[..4].try_into().unwrap())
     )?;
 
-    idol::Generator::new()
-        .with_counters(idol::CounterSettings::default())
-        .build_server_support(
-            "../../idl/gimlet-seq.idol",
-            "server_stub.rs",
-            idol::server::ServerStyle::InOrder,
-        )?;
+    idol::Generator::new().build_server_support(
+        "../../idl/gimlet-seq.idol",
+        "server_stub.rs",
+        idol::server::ServerStyle::InOrder,
+    )?;
 
     Ok(())
 }
