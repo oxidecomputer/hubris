5f47ebf4922d0fe2e593edb3e89ee75fcc85abf4
unknown <aaron@oxide.computer>
2024-04-18 12:36:29 -0500

    Add a front-io-server to service the front-io board

--- drv/sidecar-seq-api/src/lib.rs
+++ drv/sidecar-seq-api/src/lib.rs
@@ -8,6 +8,7 @@
 
 use derive_idol_err::IdolError;
 use drv_fpga_api::FpgaError;
+use drv_front_io_api::FrontIOError;
 pub use drv_sidecar_mainboard_controller::{
     fan_modules::{FanModuleStatus, NUM_FAN_MODULES},
     tofino2::{
@@ -33,9 +34,10 @@ pub enum SeqError {
     SequencerTimeout,
     InvalidTofinoVid,
     SetVddCoreVoutFailed,
-    NoFrontIOBoard,
-    FrontIOBoardPowerFault,
-    FrontIOPowerNotGood,
+    NoFrontIOBoard, // TODO: implement this in the front IO server
+    FrontIOBoardPowerFault, // TODO: implement this in the front IO server
+    FrontIOPowerNotGood, // TODO: implement this in the front IO server
+    FrontIOError,
 
     #[idol(server_death)]
     ServerRestarted,
@@ -44,6 +46,12 @@ pub enum SeqError {
 impl From<FpgaError> for SeqError {
     fn from(_: FpgaError) -> Self {
         Self::FpgaError
+    }
+}
+
+impl From<FrontIOError> for SeqError {
+    fn from(_: FrontIOError) -> Self {
+        Self::FrontIOError
     }
 }
 
