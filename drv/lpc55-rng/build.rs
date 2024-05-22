67828cf5b4938be9f221fd2600a0c253c409d0bd
Philip Tricca <flihp@twobit.org>
2024-05-22 13:05:18 -0700

    lpc55-rng: Include DICE derived seed in initial PRNG seed.
    Previously we constructed the initial seed as 32 bytes from the hardware
    RNG:
    
    ```
    SEED_0 = HRNG(32)
    ```
    
    This commit includes a seed value constructed by the measured boot
    implementation from the DICE CDI. This is passed through to the RNG task
    using the `stage0-handoff` mechanism. This 32 byte value is now
    extracted and mixed with 32 bytes from the HRNG to construct SEED_0:
    
    ```
    SEED_0 = sha3_256(DICE_SEED | HRNG(32))
    ```
    
    Use of this feature is gated by the `dice-seed` feature.

--- drv/lpc55-rng/build.rs
+++ drv/lpc55-rng/build.rs
@@ -2,15 +2,62 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+use anyhow::{anyhow, Result};
+use idol::{server::ServerStyle, CounterSettings};
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "dice-seed")] {
+        mod config {
+            include!("src/config.rs");
+        }
+        use anyhow::Context;
+        use config::DataRegion;
+        use std::{fs::File, io::Write};
+
+        const CFG_SRC: &str = "rng-config.rs";
+    }
+}
+
+#[cfg(feature = "dice-seed")]
+fn extern_regions_to_cfg(path: &str) -> Result<()> {
+    let out_dir = build_util::out_dir();
+    let dest_path = out_dir.join(path);
+    let mut out =
+        File::create(dest_path).context(format!("creating {}", path))?;
+
+    let data_regions = build_util::task_extern_regions::<DataRegion>()?;
+    if data_regions.is_empty() {
+        return Err(anyhow!("no data regions found"));
+    }
+
+    writeln!(out, "use crate::config::DataRegion;\n\n")?;
+
+    let region = data_regions
+        .get("dice_rng")
+        .ok_or_else(|| anyhow::anyhow!("dice_certs data region not found"))?;
+
+    Ok(writeln!(
+        out,
+        r##"pub const DICE_RNG: DataRegion = DataRegion {{
+    address: {:#x},
+    size: {:#x},
+}};"##,
+        region.address, region.size
+    )?)
+}
+
+fn main() -> Result<()> {
     idol::Generator::new()
-        .with_counters(
-            idol::CounterSettings::default().with_server_counters(false),
-        )
+        .with_counters(CounterSettings::default().with_server_counters(false))
         .build_server_support(
             "../../idl/rng.idol",
             "server_stub.rs",
-            idol::server::ServerStyle::InOrder,
-        )?;
+            ServerStyle::InOrder,
+        )
+        .map_err(|e| anyhow!(e))?;
+
+    #[cfg(feature = "dice-seed")]
+    extern_regions_to_cfg(CFG_SRC)?;
+
     Ok(())
 }
