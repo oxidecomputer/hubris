b6e80c55998576e41d1e596539b75319d1cd52d7
Matt Keeter <matt@oxide.computer>
2024-08-20 09:58:42 -0400

    Add license
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- drv/cosmo-hf/src/hf.rs
+++ drv/cosmo-hf/src/hf.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use drv_hash_api::SHA256_SZ;
 use drv_hf_api::{
     HfDevSelect, HfError, HfMuxState, HfPersistentData, HfProtectMode,
