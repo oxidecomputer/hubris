41da8a53cffd680737a7533c0a746d658d2648e1
Laura Abbott <laura@oxide.computer>
2024-10-07 18:10:45 -0400

    Try this

---
+++ .github/buildomat/permslip-setup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt-get update
+sudo apt-get install -y postgresql gcc pkgconf openssl libssl-dev
+
+sudo -u postgres createuser yourname
+sudo -u postgres createdb permslip
+sudo -u postgres psql << EOF
+\x
+alter user yourname with encrypted password 'password';
+EOF
+
+sudo -u postgres psql << EOF
+\x
+grant all privileges on database permslip to yourname;
+EOF
+sudo -u postgres psql -d permslip << EOF
+grant all on schema public to yourname;
+EOF
+
+export PERMSLIP_DIR=/work/permslip
+BART_KEY=`pwd`/support/fake_certs/fake_private_key.pem
+
+mkdir -p $PERMSLIP_DIR
+git clone https://github.com/oxidecomputer/permission-slip.git $PERMSLIP_DIR
+pushd $PERMSLIP_DIR
+cargo build --release
+export POSTGRES_HOST=localhost
+export POSTGRES_PORT=5432
+export POSTGRES_USER=yourname
+export POSTGRES_PASSWORD=password
+
+ssh-keygen -t ecdsa -b 256 -f /tmp/id_p256 -N '' -C ''
+eval $(ssh-agent -s)
+ssh-add /tmp/id_p256
+export PERMSLIP_SSH_KEY=`ssh-keygen -lf /tmp/id_p256.pub | cut -d ' ' -f 2`
+
+$PERMSLIP_DIR/target/release/permslip-server import-ssh-key /tmp/id_p256.pub
+$PERMSLIP_DIR/target/release/permslip-server import-private-key "UNTRUSTED bart" rsa $BART_KEY
+$PERMSLIP_DIR/target/release/permslip-server start-server &
+
+sleep 5
+
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 list-keys
+
+# Gimlet
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-key "UNTRUSTED gimlet" rsa
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-csr "UNTRUSTED gimlet" > gimlet.csr
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 sign "UNTRUSTED gimlet" --kind csr gimlet.csr > gimlet.cert
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 set-key-context "UNTRUSTED gimlet" --kind hubris --cert gimlet.cert --root gimlet.cert
+
+# sidecar
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-key "UNTRUSTED sidecar" rsa
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-csr "UNTRUSTED sidecar" > sidecar.csr
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 sign "UNTRUSTED sidecar" --kind csr sidecar.csr > sidecar.cert
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 set-key-context "UNTRUSTED sidecar" --kind hubris --cert sidecar.cert --root sidecar.cert
+
+# psc
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-key "UNTRUSTED psc" rsa
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-csr "UNTRUSTED psc" > psc.csr
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 sign "UNTRUSTED psc" --kind csr psc.csr > psc.cert
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 set-key-context "UNTRUSTED psc" --kind hubris --cert psc.cert --root psc.cert
+
+# SP
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-key "UNTRUSTED SP" rsa
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-csr "UNTRUSTED SP" > SP.csr
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 sign "UNTRUSTED SP" --kind csr SP.csr > SP.cert
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 set-key-context "UNTRUSTED SP" --kind hubris --cert SP.cert --root SP.cert
+
+# Bart
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 generate-csr "UNTRUSTED bart" > bart.csr
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 sign "UNTRUSTED bart" --kind csr bart.csr > bart.cert
+$PERMSLIP_DIR/target/release/permslip --url=http://localhost:41340 set-key-context "UNTRUSTED bart" --kind hubris --cert bart.cert --root bart.cert
+
+popd
