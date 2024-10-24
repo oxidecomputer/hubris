42f9a9ceaa7ec20f0c9c33c994cd28b41c677abf
Joshua M. Clulow <jmc@oxide.computer>
2024-10-24 16:08:27 -0700

    XXX try

--- .github/buildomat/jobs/try.sh
+++ .github/buildomat/jobs/try.sh
@@ -17,4 +17,13 @@ export HUMILITY_PROBE='1fc9:0143:CFSDRHMTTXVSJ'
 
 humility probe
 
+#
+# Here is one we prepared earlier:
+#
+curl -sSf -o sp.zip 'https://pkg.oxide.computer/tmp/build-gimletlet-image-default.zip'
+
+export HUMILITY_ARCHIVE=sp.zip
+
+humility flash
+humility tasks
 
