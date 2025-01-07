ac59ce1f5361b2d4197a8448758cbe44a44f4334
Matt Keeter <matt@oxide.computer>
2025-01-07 14:43:27 -0500

    Cosmo implementation work

--- drv/cosmo-hf/build.rs
+++ drv/cosmo-hf/build.rs
@@ -17,20 +17,31 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let out_dir = build_util::out_dir();
     let out_file = out_dir.join("fmc_periph.rs");
     let mut file = std::fs::File::create(out_file)?;
-    let node = Path::new("../spartan7-loader/grapefruit/spi_nor_reg_map.json");
-    let top = Path::new("../spartan7-loader/grapefruit/gfruit_top_map.json");
+
+    // Pick out the right JSON files for our FPGA image
+    let board = build_util::target_board().expect("could not get target board");
+    let (f, short) = match board.as_str() {
+        "grapefruit" => ("grapefruit", "gfruit"),
+        "cosmo-a" => ("cosmo", "cosmo"),
+        _ => panic!("unknown board '{board}'"),
+    };
+    let folder = format!("../spartan7-loader/{f}");
+    let base_path = Path::new(&folder);
+
+    let node = base_path.join("spi_nor_reg_map.json");
+    let top = base_path.join(format!("{short}_top_map.json"));
     let token = "drv_spartan7_loader_api::Spartan7Token";
     write!(
         &mut file,
         "{}",
-        build_fpga_regmap::fpga_peripheral(node, top, 0x60000000, token)?
+        build_fpga_regmap::fpga_peripheral(&node, &top, 0x60000000, token)?
     )?;
 
-    let node = Path::new("../spartan7-loader/grapefruit/base_reg_map.json");
+    let node = base_path.join("base_reg_map.json");
     write!(
         &mut file,
         "{}",
-        build_fpga_regmap::fpga_peripheral(node, top, 0x60000000, token)?
+        build_fpga_regmap::fpga_peripheral(&node, &top, 0x60000000, token)?
     )?;
 
     Ok(())
