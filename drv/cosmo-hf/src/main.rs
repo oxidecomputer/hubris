ad45f9acc0aa5ca408b26ad4b1d7617d662d8c27
Matt Keeter <matt@oxide.computer>
2025-03-05 08:45:24 -0800

    Don't get FPGA ID in cosmo-hf

--- drv/cosmo-hf/src/main.rs
+++ drv/cosmo-hf/src/main.rs
@@ -66,12 +66,6 @@ fn main() -> ! {
     let seq =
         drv_spartan7_loader_api::Spartan7Loader::from(LOADER.get_task_id());
 
-    let base = fmc_periph::Base::new(seq.get_token());
-    let id = base.id.data();
-    if id != 0x1de {
-        fail(drv_hf_api::HfError::FpgaNotConfigured);
-    }
-
     let drv = FlashDriver {
         drv: fmc_periph::SpiNor::new(seq.get_token()),
     };
