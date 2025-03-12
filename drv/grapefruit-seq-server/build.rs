3324a9cfaa7fb1bcd15d93512a0b73b7aea9ab79
Matt Keeter <matt@oxide.computer>
2025-03-12 09:57:37 -0400

    Grapefruit update and peripheral codegen improvements (#2028)
    The top-level FPGA peripheral map now has more regular structure:
    
    - `orig_type_name` is guaranteed to be the same as the JSON file for the
      individual peripheral registers
    - `inst_name` is a per-peripheral instance name (which will be relevant
      if we have > 1 copy of the same peripheral)
    
    This means that we don't need to guess the peripheral register file name
    from the `inst_name`, which was requiring more and more special cases.
    
    In addition, we now define the top-level MMIO map in the manifest, so
    individual tasks shouldn't also specify it.
    `build_fpga_regmap::fpga_peripheral` now just takes the peripheral's
    `inst_name`, and loads the peripheral map + base address from
    environment variables set by the build script.
    
    ---------
    
    Co-authored-by: Nathanael Huffman <nathanael@oxidecomputer.com>

--- drv/grapefruit-seq-server/build.rs
+++ drv/grapefruit-seq-server/build.rs
@@ -1,7 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
-use std::{io::Write, path::Path};
+use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     build_util::expose_target_board();
@@ -14,13 +14,13 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let out_dir = build_util::out_dir();
     let out_file = out_dir.join("fmc_sgpio.rs");
     let mut file = std::fs::File::create(out_file)?;
-    let node = Path::new("../spartan7-loader/grapefruit/sgpio_reg_map.json");
-    let top = Path::new("../spartan7-loader/grapefruit/gfruit_top_map.json");
-    let token = "drv_spartan7_loader_api::Spartan7Token";
     write!(
         &mut file,
         "{}",
-        build_fpga_regmap::fpga_peripheral(node, top, 0x60000000, token)?
+        build_fpga_regmap::fpga_peripheral(
+            "sgpio",
+            "drv_spartan7_loader_api::Spartan7Token"
+        )?
     )?;
 
     Ok(())
--- drv/spartan7-loader/grapefruit/README.md
+++ drv/spartan7-loader/grapefruit/README.md
@@ -1,2 +1,3 @@
-generated from [this commit](https://github.com/oxidecomputer/quartz/commit/e31772323d13f15802554ae3daa5061f9c9290cc)
-([this CI run](https://github.com/oxidecomputer/quartz/actions/runs/13228105732/job/36921564266))
+FPGA images and collateral are generated from:
+[this sha](https://github.com/oxidecomputer/quartz/commit/6cf9b3ff8a74e631723586d4f380835fbba457e6)
+[release](https://api.github.com/repos/oxidecomputer/quartz/releases/204448780)
