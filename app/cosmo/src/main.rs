05ce5fcb1b46b6c6502fedfbba2fbf435f271d59
Matt Keeter <matt@oxide.computer>
2025-04-07 14:24:08 -0500

    Bring up FMC peripheral

--- app/cosmo/src/main.rs
+++ app/cosmo/src/main.rs
@@ -115,7 +115,7 @@ fn system_init() {
     assert_eq!(rev, expected_rev);
 
     // Do most of the setup with the common implementation.
-    drv_stm32h7_startup::system_init_custom(
+    let p = drv_stm32h7_startup::system_init_custom(
         cp,
         p,
         ClockConfig {
@@ -159,4 +159,299 @@ fn system_init() {
             flash_write_delay: 2,
         },
     );
+
+    // Now that our clock tree is configured appropriately, we need to set up
+    // the external bus to the FPGA. This ensures that we're not relying on any
+    // particular task to do it, so we can direct-map its peripherals into other
+    // tasks. If those tasks try to use the peripheral before the FPGA is
+    // initialized, the peripherals won't work, but the result should not be
+    // _fatal._
+
+    // Pin mapping:
+    // PB7      ADV_L (this is PB2 on the schematic, manually jumpered)
+    //
+    // PD0      DA2
+    // PD1      DA3
+    // PD2      -
+    // PD3      CLK
+    // PD4      OE_L
+    // PD5      WE_L
+    // PD6      WAIT_L  <-- pulled up in hardware
+    // PD7      CS1_L
+    // PD8      DA13
+    // PD9      DA14
+    // PD10     DA15
+    // PD11     A16
+    // PD12     A17
+    // PD13     A18
+    // PD14     DA0
+    // PD15     DA1
+    //
+    // PE0      BL0_L
+    // PE1      BL1_L
+    // PE2      A23     <-- new
+    // PE3      A19
+    // PE4      A20     <-- new
+    // PE5      A21     <-- new
+    // PE6      A22     <-- new
+    // PE7      DA4
+    // PE8      DA5
+    // PE9      DA6
+    // PE10     DA7
+    // PE11     DA8
+    // PE12     DA9
+    // PE13     DA10
+    // PE14     DA11
+    // PE15     DA12
+    //
+    // Our goal is to put all of these into the appropriate AF setting (which,
+    // conveniently, is AF12 across all ports) and prepare the memory controller.
+
+    // Ensure clock is enabled to both the GPIO ports we touch, and the FMC
+    // itself.
+    p.RCC.ahb3enr.modify(|_, w| w.fmcen().set_bit());
+    p.RCC.ahb4enr.modify(|_, w| {
+        w.gpioben().set_bit();
+        w.gpioden().set_bit();
+        w.gpioeen().set_bit();
+        w
+    });
+    cortex_m::asm::dsb();
+
+    // Expose all the pins _first._ This seems to work best.
+
+    // GPIOB
+    p.GPIOB.afrl.modify(|_, w| {
+        w.afr7().af12();
+        w
+    });
+    p.GPIOB.ospeedr.modify(|_, w| {
+        w.ospeedr7().very_high_speed();
+        w
+    });
+    p.GPIOB.moder.modify(|_, w| {
+        w.moder7().alternate();
+        w
+    });
+
+    // GPIOD
+    p.GPIOD.afrl.modify(|_, w| {
+        w.afr0().af12();
+        w.afr1().af12();
+        // pin 2 used for something else
+        w.afr3().af12();
+        w.afr4().af12();
+        w.afr5().af12();
+        w.afr6().af12();
+        w.afr7().af12();
+        w
+    });
+    p.GPIOD.afrh.modify(|_, w| {
+        w.afr8().af12();
+        w.afr9().af12();
+        w.afr10().af12();
+        w.afr11().af12();
+        w.afr12().af12();
+        w.afr13().af12();
+        w.afr14().af12();
+        w.afr15().af12();
+        w
+    });
+    p.GPIOD.ospeedr.modify(|_, w| {
+        w.ospeedr0().very_high_speed();
+        w.ospeedr1().very_high_speed();
+        // pin 2 used elsewhere
+        w.ospeedr3().very_high_speed();
+        w.ospeedr4().very_high_speed();
+        w.ospeedr5().very_high_speed();
+        w.ospeedr6().very_high_speed();
+        w.ospeedr7().very_high_speed();
+
+        w.ospeedr8().very_high_speed();
+        w.ospeedr9().very_high_speed();
+        w.ospeedr10().very_high_speed();
+        w.ospeedr11().very_high_speed();
+        w.ospeedr12().very_high_speed();
+        w.ospeedr13().very_high_speed();
+        w.ospeedr14().very_high_speed();
+        w.ospeedr15().very_high_speed();
+        w
+    });
+    p.GPIOD.moder.modify(|_, w| {
+        w.moder0().alternate();
+        w.moder1().alternate();
+        // pin 2 used elsewhere
+        w.moder3().alternate();
+        w.moder4().alternate();
+        w.moder5().alternate();
+        w.moder6().alternate();
+        w.moder7().alternate();
+
+        w.moder8().alternate();
+        w.moder9().alternate();
+        w.moder10().alternate();
+        w.moder11().alternate();
+        w.moder12().alternate();
+        w.moder13().alternate();
+        w.moder14().alternate();
+        w.moder15().alternate();
+        w
+    });
+
+    // GPIOE
+    p.GPIOE.afrl.modify(|_, w| {
+        w.afr0().af12();
+        w.afr1().af12();
+        // w.afr2().af12();
+        w.afr3().af12();
+        // w.afr4().af12();
+        // w.afr5().af12();
+        // w.afr6().af12();
+        w.afr7().af12();
+        w
+    });
+    p.GPIOE.afrh.modify(|_, w| {
+        w.afr8().af12();
+        w.afr9().af12();
+        w.afr10().af12();
+        w.afr11().af12();
+        w.afr12().af12();
+        w.afr13().af12();
+        w.afr14().af12();
+        w.afr15().af12();
+        w
+    });
+    p.GPIOE.ospeedr.modify(|_, w| {
+        w.ospeedr0().very_high_speed();
+        w.ospeedr1().very_high_speed();
+        // w.ospeedr2().very_high_speed();
+        w.ospeedr3().very_high_speed();
+        // w.ospeedr4().very_high_speed();
+        // w.ospeedr5().very_high_speed();
+        // w.ospeedr6().very_high_speed();
+        w.ospeedr7().very_high_speed();
+
+        w.ospeedr8().very_high_speed();
+        w.ospeedr9().very_high_speed();
+        w.ospeedr10().very_high_speed();
+        w.ospeedr11().very_high_speed();
+        w.ospeedr12().very_high_speed();
+        w.ospeedr13().very_high_speed();
+        w.ospeedr14().very_high_speed();
+        w.ospeedr15().very_high_speed();
+        w
+    });
+    p.GPIOE.moder.modify(|_, w| {
+        w.moder0().alternate();
+        w.moder1().alternate();
+        // w.moder2().alternate();
+        w.moder3().alternate();
+        // w.moder4().alternate();
+        // w.moder5().alternate();
+        // w.moder6().alternate();
+        w.moder7().alternate();
+
+        w.moder8().alternate();
+        w.moder9().alternate();
+        w.moder10().alternate();
+        w.moder11().alternate();
+        w.moder12().alternate();
+        w.moder13().alternate();
+        w.moder14().alternate();
+        w.moder15().alternate();
+        w
+    });
+
+    // Basic memory controller setup:
+    p.FMC.bcr1.write(|w| {
+        // Emit clock signal continuously, the FPGA likes that.
+        w.cclken().set_bit();
+
+        // Use synchronous bursts for both writes and reads.
+        w.bursten().set_bit();
+        w.cburstrw().set_bit();
+
+        // Enable wait states.
+        w.waiten().set_bit();
+        // Expect the wait state to be active _during_ a wait, not one cycle
+        // early.
+        w.waitcfg().set_bit();
+
+        // Enable writes through the controller.
+        w.wren().set_bit();
+
+        // Disable NOR flash memory access (may not be necessary?)
+        w.faccen().clear_bit();
+
+        // Configure the memory as "PSRAM" since that's the closest to the
+        // behavior we want.
+        //
+        // Safety: this enum value is not modeled in the PAC, but is defined in
+        // the reference manual, so this has no implications for safety.
+        unsafe {
+            w.mtyp().bits(0b01);
+        }
+
+        // Turn on the bank (note that we have not turned on the _controller_
+        // still).
+        w.mbken().set_bit();
+
+        // The following fields are being deliberately left in their reset
+        // states:
+        // - FMCEN is being left off
+        // - BMAP default (no remapping) is retained
+        // - Write FIFO is being left on (TODO is this correct?)
+        // - CPSIZE is being left with no special behavior on page-crossing
+        // - ASYNCWAIT is being left off since we're synchronous
+        // - EXTMOD is being left off, since it seems to only affect async
+        // - WAITPOL is treating NWAIT as active low (could change if desired)
+        // - MWID is being left at a 16 bit data bus.
+        // - MUXEN is being left with a multiplexed A/D bus.
+
+        w
+    });
+
+    // Now for the timings.
+    //
+    // Synchronous access write/read latency, minus 2. That is, 0 means 2 cycle
+    // latency. Max value: 15 (for 17 cycles). NWAIT is not sampled until this
+    // period has elapsed, so if you're handshaking with a device using NWAIT,
+    // you almost certainly want this to be 0.
+    const DATLAT: u8 = 0;
+    // FMC_CLK division ratio relative to input (AHB3) clock, minus 1. Range:
+    // 1..=15.
+    //
+    // Note from the clock config earlier in this function that AHB3 is running
+    // at 200 MHz.
+    const CLKDIV: u8 = 3; // /4, for 50 MHz -- field is divisor minus 1
+
+    // Bus turnaround time in FMC_CLK cycles, 0..=15
+    const BUSTURN: u8 = 0;
+
+    p.FMC.btr1.write(|w| {
+        unsafe {
+            w.datlat().bits(DATLAT);
+        }
+        unsafe {
+            w.clkdiv().bits(CLKDIV);
+        }
+        unsafe {
+            w.busturn().bits(BUSTURN);
+        }
+
+        // Deliberately left in reset state and/or ignored:
+        // - ACCMOD: only applies when EXTMOD is set in BCR above; also probably
+        //   async only
+        // - DATAST: async only
+        // - ADDHLD: async only
+        // - ADDSET: async only
+        //
+        w
+    });
+
+    // BWTR1 register is irrelevant if we're not using EXTMOD, which we're not,
+    // currently.
+
+    // Turn on the controller.
+    p.FMC.bcr1.modify(|_, w| w.fmcen().set_bit());
 }
