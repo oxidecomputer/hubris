4e589c1bc5789e3c651abf94feb552f3a43a8bcf
Matt Keeter <matt@oxide.computer>
2025-05-15 13:25:27 -0400

    Make clippy happier

--- task/thermal/src/control.rs
+++ task/thermal/src/control.rs
@@ -854,7 +854,7 @@ impl<'a> ThermalControl<'a> {
                 }
             } else {
                 // Invalidate fan speed readings in the sensors task
-                let sensor_id = self.bsp.fan_sensor_id(index.into());
+                let sensor_id = self.bsp.fan_sensor_id(index);
                 self.sensor_api.nodata_now(
                     sensor_id,
                     task_sensor_api::NoData::DeviceNotPresent,
