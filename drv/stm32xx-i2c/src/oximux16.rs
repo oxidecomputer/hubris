9e90ccfec8985c95ce4de8d92d6f447a25823070
Matt Keeter <matt@oxide.computer>
2025-07-15 16:45:19 -0400

    Use 16-channel virtual mux on Cosmo

--- drv/stm32xx-i2c/src/oximux16.rs
+++ drv/stm32xx-i2c/src/oximux16.rs
@@ -2,17 +2,28 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//! Driver for the PCA9548 I2C mux
+//! Driver for a virtual 16-channel mux implemented in an FGPA
+//!
+//! This is based on the PCA9545, but is controlled by a single 16-bit register
+//! (instead of an 8-bit register).
 
 use crate::*;
 use bitfield::bitfield;
 use drv_i2c_api::{ResponseCode, Segment};
 
-pub struct Pca9548;
+pub struct Oximux16;
 
 bitfield! {
     #[derive(Copy, Clone, Eq, PartialEq)]
-    pub struct ControlRegister(u8);
+    pub struct ControlRegister(u16);
+    channel15_enabled, set_channel15_enabled: 15;
+    channel14_enabled, set_channel14_enabled: 14;
+    channel13_enabled, set_channel13_enabled: 13;
+    channel12_enabled, set_channel12_enabled: 12;
+    channel11_enabled, set_channel11_enabled: 11;
+    channel10_enabled, set_channel10_enabled: 10;
+    channel9_enabled, set_channel9_enabled: 9;
+    channel8_enabled, set_channel8_enabled: 8;
     channel7_enabled, set_channel7_enabled: 7;
     channel6_enabled, set_channel6_enabled: 6;
     channel5_enabled, set_channel5_enabled: 5;
@@ -23,7 +34,7 @@ bitfield! {
     channel0_enabled, set_channel0_enabled: 0;
 }
 
-impl I2cMuxDriver for Pca9548 {
+impl I2cMuxDriver for Oximux16 {
     fn configure(
         &self,
         mux: &I2cMux<'_>,
@@ -69,8 +80,29 @@ impl I2cMuxDriver for Pca9548 {
                 Segment::S8 => {
                     reg.set_channel7_enabled(true);
                 }
-                _ => {
-                    return Err(ResponseCode::SegmentNotFound);
+                Segment::S9 => {
+                    reg.set_channel8_enabled(true);
+                }
+                Segment::S10 => {
+                    reg.set_channel9_enabled(true);
+                }
+                Segment::S11 => {
+                    reg.set_channel10_enabled(true);
+                }
+                Segment::S12 => {
+                    reg.set_channel11_enabled(true);
+                }
+                Segment::S13 => {
+                    reg.set_channel12_enabled(true);
+                }
+                Segment::S14 => {
+                    reg.set_channel13_enabled(true);
+                }
+                Segment::S15 => {
+                    reg.set_channel14_enabled(true);
+                }
+                Segment::S16 => {
+                    reg.set_channel15_enabled(true);
                 }
             }
         }
@@ -81,14 +113,20 @@ impl I2cMuxDriver for Pca9548 {
         //
         match controller.write_read(
             mux.address,
-            1,
-            |_| Some(reg.0),
+            2,
+            |i| Some(reg.0.to_le_bytes()[i]),
             ReadLength::Fixed(0),
             |_, _| Some(()),
             ctrl,
         ) {
             Err(code) => Err(mux.error_code(code)),
-            _ => Ok(()),
+            _ => {
+                // The mux takes ~100µs to electrically switch over; we could
+                // optimize this later by using a dedicated timer with a shorter
+                // delay.
+                userlib::hl::sleep_for(1);
+                Ok(())
+            }
         }
     }
 
