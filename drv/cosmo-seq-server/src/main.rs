74759a9a7687636e0e65c11ce84a7cc72769a3ec
Matt Keeter <matt@oxide.computer>
2025-07-21 15:03:48 -0400

    Fixes from Dan's review

--- drv/cosmo-seq-server/src/main.rs
+++ drv/cosmo-seq-server/src/main.rs
@@ -426,9 +426,15 @@ impl ServerImpl {
         use fmc_periph::A0Sm;
         match (self.get_state_impl(), state) {
             (PowerState::A2, PowerState::A0) => {
+                // Reset edge counters in the sequencer
+                self.seq.amd_reset_fedges.set_counts(0);
+                self.seq.amd_pwrok_fedges.set_counts(0);
+
+                // Tell the sequencer to go to A0
                 self.seq.power_ctrl.modify(|m| m.set_a0_en(true));
-                let mut okay = false;
+
                 // Wait 2 seconds for power-up
+                let mut okay = false;
                 for _ in 0..200 {
                     let state = self.log_state_registers();
                     match state.seq {
@@ -675,14 +681,20 @@ impl NotificationHandler for ServerImpl {
             _ => (),
         }
 
-        // If we're powered up, detect powering down
+        // If Hubris thinks we're powered up, check that the FPGA has not logged
+        // any reset conditions from the CPU.
         if matches!(self.state, PowerState::A0 | PowerState::A0PlusHP) {
             // TODO this reads the register 3x
             let thermtrip = self.seq.ifr.thermtrip();
             let pwrok_fedge = self.seq.ifr.amd_pwrok_fedge();
             let rstn_fedge = self.seq.ifr.amd_rstn_fedge();
 
-            if pwrok_fedge || rstn_fedge {
+            if thermtrip {
+                self.seq.ifr.modify(|h| h.set_thermtrip(false));
+                ringbuf_entry!(Trace::Thermtrip);
+                self.set_state_internal(PowerState::A0Thermtrip)
+                // this is a terminal state (for now)
+            } else if pwrok_fedge || rstn_fedge {
                 let rstn = self.seq.amd_reset_fedges.counts();
                 let pwrokn = self.seq.amd_pwrok_fedges.counts();
                 ringbuf_entry!(Trace::ResetCounts { rstn, pwrokn });
@@ -690,17 +702,11 @@ impl NotificationHandler for ServerImpl {
                     h.set_amd_pwrok_fedge(false);
                     h.set_amd_rstn_fedge(false);
                 });
-                self.seq.amd_reset_fedges.set_counts(0);
-                self.seq.amd_pwrok_fedges.set_counts(0);
+                // counters are cleared in the A2 -> A0 transition
                 self.set_state_internal(PowerState::A0Reset);
-                // host_sp_comms detects this change and reboots us
-            }
-
-            if thermtrip {
-                self.seq.ifr.modify(|h| h.set_thermtrip(false));
-                ringbuf_entry!(Trace::Thermtrip);
-                self.set_state_internal(PowerState::A0Thermtrip)
-                // this is a terminal state (for now)
+                // host_sp_comms will be notified of this change and will call
+                // back into this task to reboot the system (going to A2 then
+                // back into A0)
             }
         }
 
