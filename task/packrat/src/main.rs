75538b75f51718d08d9b06b8c8c91d90c5e8204b
Eliza Weisman <eliza@elizas.website>
2026-01-06 11:49:49 -0800

    clippy fix when ereports are turned off

--- task/packrat/src/main.rs
+++ task/packrat/src/main.rs
@@ -579,7 +579,7 @@ impl NotificationHandler for ServerImpl {
         0
     }
 
-    fn handle_notification(&mut self, bits: userlib::NotificationBits) {
+    fn handle_notification(&mut self, _bits: userlib::NotificationBits) {
         unreachable!()
     }
 }
