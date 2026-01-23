0c96d99faa2d51b5c113f137fac88280cfd78e7c
Adam Suczewski <adamlouis@users.noreply.github.com>
2026-01-23 11:55:37 -0800

    fixes for PR comments - filename, return code, buffer size

--- task/control-plane-agent/src/main.rs
+++ task/control-plane-agent/src/main.rs
@@ -265,8 +265,8 @@ impl NotificationHandler for ServerImpl {
             | notifications::USART_IRQ_MASK
             | notifications::TIMER_MASK;
 
-        // Minibar only communicates over the network, not USART, so it doesn't
-        // need USART_IRQ_MASK.
+        // Minibar does not configure USART for serial console, so omit it
+        // from the mask.
         #[cfg(feature = "minibar")]
         let mask = notifications::SOCKET_MASK | notifications::TIMER_MASK;
 
