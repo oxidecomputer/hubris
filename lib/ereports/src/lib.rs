2534fd814081fc19365ca7cb6e067955cadd85fe
Eliza Weisman <eliza@elizas.website>
2026-02-27 11:38:40 -0800

    wip cool idea

--- lib/ereports/src/lib.rs
+++ lib/ereports/src/lib.rs
@@ -8,3 +8,19 @@
 
 pub mod cpu;
 pub mod pwr;
+
+#[cfg(feature = "ereporter-macro")]
+#[macro_export]
+macro_rules! declare_ereporter {
+    ($($v:vis)? struct $Ereporter:ident<$Trait:ident> { $($EreportTy:ty),+ $(,)? }) => {
+        $($v)? struct $Ereporter {
+
+        }
+    };
+}
+
+#[cfg(feature = "ereporter-macro")]
+#[doc(hidden)]
+pub mod __macro_support {
+    pub use paste::paste;
+}
