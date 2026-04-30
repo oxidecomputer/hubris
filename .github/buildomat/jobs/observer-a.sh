d5b79ff25347afcea1d75ee0988366fd35c36f0d
Evan <evan@oxidecomputer.com>
2026-04-30 13:48:04 -0400

    Add buildomat job for observer-a

---
+++ .github/buildomat/jobs/observer-a.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#:
+#: name = "build observer-a"
+#: variety = "basic"
+#: target = "ubuntu-22.04"
+#: rust_toolchain = true
+#: output_rules = [
+#:  "=/work/*.zip",
+#:  "=/work/this_is_not_signed.txt",
+#: ]
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+exec .github/buildomat/build-one.sh observer-a app/observer/rev-a.toml default
