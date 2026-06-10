c4290325bf9e1e4b0b2553085cfe9f3f57822c6e
evan-oxide <evan@oxidecomputer.com>
2026-06-10 15:04:35 -0400

    fix doc link

--- drv/i2c-devices/src/lib.rs
+++ drv/i2c-devices/src/lib.rs
@@ -23,8 +23,7 @@
 //! - [`max6634`]: MAX6634 temperature sensor
 //! - [`max31790`]: MAX31790 fan controller
 //! - [`mcp9808`]: MCP9808 temperature sensor
-//! - [`mwocp67`]: Murata power shelf
-//! - [`mwocp68`]: Murata power shelf
+//! - [`mwocp6x`]: Murata power supply units
 //! - [`nvme_bmc`]: NVMe basic management control
 //! - [`pca9538`]: PCA9538 GPIO expander
 //! - [`pca9956b`]: PCA9956B LED driver
