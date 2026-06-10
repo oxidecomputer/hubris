f9a65c0d43dbf3fe5aaf0a199172489560862d00
evan-oxide <evan@oxidecomputer.com>
2026-06-10 14:53:28 -0400

    add missing license

--- drv/psc-seq-server/src/bsp/observer_a.rs
+++ drv/psc-seq-server/src/bsp/observer_a.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use drv_i2c_api::I2cDevice;
 use drv_stm32xx_sys_api as sys_api;
 use userlib::*;
