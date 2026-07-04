e087d34402e3c3c44c903009c6f2456b47259944
James Munns <james.munns@oxidecomputer.com>
2026-07-04 23:52:18 +0200

    Start extracting portions of `get_max_stack`
    No functional changes expected.

--- build/stack/src/lib.rs
+++ build/stack/src/lib.rs
@@ -12,6 +12,7 @@ use capstone::{
     Capstone, InsnGroupId, InsnGroupType,
     arch::{ArchOperand, BuildsCapstone, BuildsCapstoneExtraMode, arm},
 };
+use goblin::elf::{Elf, SectionHeader, Sym};
 
 // We'll be packing everything into this data structure
 #[derive(Debug)]
@@ -20,6 +21,132 @@ struct FunctionData {
     short_name: String,
     frame_size: Option<u64>,
     calls: BTreeSet<u32>,
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Pulling data from the elf file
+////////////////////////////////////////////////////////////////////////////////
+
+/// Read the .stack_sizes section, which is an array of
+/// `(address: u32, stack size: unsigned leb128)` tuples.
+fn extract_stack_sizes_section(
+    raw_elf: &[u8],
+    parsed_elf: &Elf<'_>,
+) -> Result<BTreeMap<u32, u64>> {
+    let sizes = elf::get_section_by_name(parsed_elf, ".stack_sizes")
+        .context("could not get .stack_sizes")?;
+    let mut sizes =
+        &raw_elf[sizes.sh_offset as usize..][..sizes.sh_size as usize];
+    let mut addr_to_frame_size = BTreeMap::new();
+    while !sizes.is_empty() {
+        let (addr, rest) = sizes.split_at(4);
+        let addr = u32::from_le_bytes(addr.try_into().unwrap());
+        sizes = rest;
+        let size = leb128::read::unsigned(&mut sizes)?;
+        addr_to_frame_size.insert(addr, size);
+    }
+
+    Ok(addr_to_frame_size)
+}
+
+// There are `$t` and `$d` symbols which indicate the beginning of text
+// versus data in the `.text` region.  We collect them into a `BTreeMap`
+// here so that we can avoid trying to decode inline data words.
+fn extract_text_regions(
+    parsed_elf: &Elf<'_>,
+    task_name: &str,
+) -> Result<BTreeMap<u32, bool>> {
+    let mut text_regions = BTreeMap::new();
+    for sym in parsed_elf.syms.iter() {
+        if sym.st_name == 0
+            || sym.st_size != 0
+            || sym.st_type() != goblin::elf::sym::STT_NOTYPE
+        {
+            continue;
+        }
+
+        let addr = sym.st_value as u32;
+        let is_text = match parsed_elf.strtab.get_at(sym.st_name) {
+            Some("$t") => true,
+            Some("$d") => false,
+            Some(_) => continue,
+            None => {
+                bail!("bad symbol in {task_name}: {}", sym.st_name);
+            }
+        };
+        text_regions.insert(addr, is_text);
+    }
+    Ok(text_regions)
+}
+
+struct SymbolItem<'a> {
+    sym: Sym,
+    name: &'a str,
+    base_addr: u64,
+    text_region: &'a [u8],
+}
+
+struct ChunkItem {
+    code: Vec<u8>,
+    addr: u64,
+}
+
+impl SymbolItem<'_> {
+    // TODO: return `Vec<(u32, &[u8])>?
+    fn extract_instruction_chunks<F>(&self, is_code: F) -> Vec<ChunkItem>
+    where
+        F: Fn(u32) -> bool,
+    {
+        // Split the text region into instruction-only chunks
+        let mut chunks = vec![];
+        let mut chunk = None;
+        for (i, b) in self.text_region.iter().enumerate() {
+            let addr = self.base_addr + i as u64;
+            if is_code(addr as u32) {
+                chunk
+                    .get_or_insert(ChunkItem { addr, code: vec![] })
+                    .code
+                    .push(*b);
+            } else if let Some(c) = chunk.take() {
+                chunks.push(c);
+            }
+        }
+        chunks.extend(chunk); // don't forget the trailing chunk!
+        chunks
+    }
+}
+
+fn fn_symbol_iter<'a>(
+    parsed_elf: &Elf<'a>,
+    text_section: &SectionHeader,
+    raw_elf: &'a [u8],
+) -> impl Iterator<Item = SymbolItem<'a>> {
+    parsed_elf
+        .syms
+        .iter()
+        // We only care about named function symbols here
+        .filter(|s| s.st_name != 0)
+        .filter(|s| s.is_function())
+        .filter(|s| s.st_size != 0)
+        // TODO: assert?
+        .filter_map(|s| {
+            // Clear the lowest bit, which indicates that the function contains
+            // thumb instructions (always true for our systems!)
+            let base_addr = s.st_value & !1;
+
+            // Get the text region for this function
+            let offset = (base_addr - text_section.sh_addr
+                + text_section.sh_offset) as usize;
+            let text_region = &raw_elf[offset..][..s.st_size as usize];
+
+            // Bake into a handy collected symbol item
+            Some(SymbolItem {
+                sym: s,
+                name: parsed_elf.strtab.get_at(s.st_name)?,
+                base_addr,
+                text_region,
+            })
+        })
 }
 
 /// Estimates the maximum stack size for the given task
@@ -37,43 +164,10 @@ pub fn get_max_stack(
     let data = std::fs::read(elf).context("could not open ELF file")?;
     let elf = goblin::elf::Elf::parse(&data)?;
 
-    // Read the .stack_sizes section, which is an array of
-    // `(address: u32, stack size: unsigned leb128)` tuples
-    let sizes = elf::get_section_by_name(&elf, ".stack_sizes")
-        .context("could not get .stack_sizes")?;
-    let mut sizes = &data[sizes.sh_offset as usize..][..sizes.sh_size as usize];
-    let mut addr_to_frame_size = BTreeMap::new();
-    while !sizes.is_empty() {
-        let (addr, rest) = sizes.split_at(4);
-        let addr = u32::from_le_bytes(addr.try_into().unwrap());
-        sizes = rest;
-        let size = leb128::read::unsigned(&mut sizes)?;
-        addr_to_frame_size.insert(addr, size);
-    }
+    // Get sizes of stack frames by addr from the elf
+    let addr_to_frame_size = extract_stack_sizes_section(&data, &elf)?;
 
-    // There are `$t` and `$d` symbols which indicate the beginning of text
-    // versus data in the `.text` region.  We collect them into a `BTreeMap`
-    // here so that we can avoid trying to decode inline data words.
-    let mut text_regions = BTreeMap::new();
-    for sym in elf.syms.iter() {
-        if sym.st_name == 0
-            || sym.st_size != 0
-            || sym.st_type() != goblin::elf::sym::STT_NOTYPE
-        {
-            continue;
-        }
-
-        let addr = sym.st_value as u32;
-        let is_text = match elf.strtab.get_at(sym.st_name) {
-            Some("$t") => true,
-            Some("$d") => false,
-            Some(_) => continue,
-            None => {
-                bail!("bad symbol in {task_name}: {}", sym.st_name);
-            }
-        };
-        text_regions.insert(addr, is_text);
-    }
+    let text_regions = extract_text_regions(&elf, task_name)?;
     let is_code = |addr| {
         let mut iter = text_regions.range(..=addr);
         *iter.next_back().unwrap().1
@@ -92,43 +186,20 @@ pub fn get_max_stack(
 
     // Disassemble each function, building a map of its call sites
     let mut fns = BTreeMap::new();
-    for sym in elf.syms.iter() {
-        // We only care about named function symbols here
-        if sym.st_name == 0 || !sym.is_function() || sym.st_size == 0 {
-            continue;
-        }
-
-        let Some(name) = elf.strtab.get_at(sym.st_name) else {
-            bail!("bad symbol in {task_name}: {}", sym.st_name);
-        };
-
-        // Clear the lowest bit, which indicates that the function contains
-        // thumb instructions (always true for our systems!)
-        let val = sym.st_value & !1;
-        let base_addr = val as u32;
-
-        // Get the text region for this function
-        let offset = (val - text.sh_addr + text.sh_offset) as usize;
-        let text = &data[offset..][..sym.st_size as usize];
+    for sym_item in fn_symbol_iter(&elf, text, &data) {
+        // TODO
+        let sym = sym_item.sym;
+        let base_addr = sym_item.base_addr as u32;
+        let name = sym_item.name;
 
         // Split the text region into instruction-only chunks
-        let mut chunks = vec![];
-        let mut chunk = None;
-        for (i, b) in text.iter().enumerate() {
-            let addr = base_addr + i as u32;
-            if is_code(addr) {
-                chunk.get_or_insert((addr, vec![])).1.push(*b);
-            } else {
-                chunks.extend(chunk.take());
-            }
-        }
-        chunks.extend(chunk); // don't forget the trailing chunk!
+        let chunks = sym_item.extract_instruction_chunks(is_code);
 
         let frame_size = addr_to_frame_size.get(&base_addr).copied();
         let mut calls = BTreeSet::new();
-        for (addr, chunk) in chunks {
+        for chunk_item in chunks {
             let instrs = cs
-                .disasm_all(&chunk, addr.into())
+                .disasm_all(&chunk_item.code, chunk_item.addr)
                 .map_err(|e| anyhow!("disassembly failed: {e:?}"))?;
             for (i, instr) in instrs.iter().enumerate() {
                 let detail = cs.insn_detail(instr).map_err(|e| {
