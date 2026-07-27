265893630a414f859ac1b018193a8948adfc3cbc
James Munns <james.munns@oxidecomputer.com>
2026-07-27 18:49:44 +0200

    Fix grapefruit

--- task/thermal/src/bsp/common/emc2305.rs
+++ task/thermal/src/bsp/common/emc2305.rs
@@ -11,7 +11,7 @@ use task_thermal_api::{SensorReadError, ThermalError};
 
 use crate::{
     __RINGBUF, Trace,
-    control::{Fan, FanStatus},
+    control::{Fan, FanReading, FanStatus},
 };
 
 /// Tracks whether a Emc2305 fan controller has been initialized, and
@@ -75,6 +75,11 @@ impl Emc2305State {
         // TODO: Maybe there's a way to make this a method on Fan that we can
         // call, kind of like InputStatus?
         fans.iter_mut().map(move |f| {
+            let sensor_id = f.rpm_sensor_id;
+            if f.last_reading.is_none() {
+                return FanStatus::NotPresent { sensor_id };
+            }
+
             // If initialization failed, then we short circuit to return that
             // original error, copied for each fan we're going to report.
             let fctrl = fctrl.as_mut().map_err(|e| *e);
@@ -86,18 +91,12 @@ impl Emc2305State {
             });
             match res {
                 Ok(rpm) => {
-                    f.last_reading = Some(rpm);
-                    FanStatus::PresentSuccess {
-                        rpm,
-                        sensor_id: f.rpm_sensor_id,
-                    }
+                    f.last_reading = Some(FanReading::Valid);
+                    FanStatus::PresentSuccess { rpm, sensor_id }
                 }
                 Err(error) => {
-                    f.last_reading = None;
-                    FanStatus::PresentError {
-                        error,
-                        sensor_id: f.rpm_sensor_id,
-                    }
+                    f.last_reading = Some(FanReading::Invalid);
+                    FanStatus::PresentError { error, sensor_id }
                 }
             }
         })
